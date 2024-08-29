e706e503e8c266560dab5e9f9579eb7007ccdb76
chiyoung jung <jcy0011@yahoo.com>
2024-08-30 06:30:16 +0900

    make layout for sidebar, footer, header

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const assetMap = {
+    "banner" : "asset/png/d72d05561bfdb209c688de2957f78ef1.jpeg",
+    "sad" : "asset/png/b19da762c846d560f8bfcde81d4efdc5.png",
+    "surprised" : "asset/png/a0d14d42505d44a355217f4a6333160a.png",
+    "happy" : "asset/png/394c18eb9129e4d3f22f03a59519cfdc.png",
+    "other" : "asset/png/47d90f65f868897bceebf4a1969de84b.png",
+    "angry" : "asset/png/4ae621ed2ead07f5b36fb83001247dba.png"
+}
+window.onload = () => 
+{
+    
+}
