69215eb699386412bd368cd9209b896513cb1396
류지승 <rjs8833@gmail.com>
2024-08-30 21:24:44 +0900

    [240830] complete assignment

--- utils/onClickDailyInfor.js
+++ utils/onClickDailyInfor.js
@@ -1,4 +1,5 @@
 function onClickDailyInfor(event) {
+    const dailyArray = JSON.parse(localStorage.getItem("dailyArray"));
     const detailInfor = dailyArray[event.target.id];
     alert(
         `제목은 "${detailInfor.title}"이고 내용은 "${detailInfor.content}"이고 나는 "${detailInfor.mood}". 오늘 날짜는 "${detailInfor.date}"입니다.`
