5b121cc840b4e066dea30a50bf8002de5684815b
h0ngsik <hongsik.choi.dev@gmail.com>
2024-08-30 20:41:41 +0900

    [240830] Section 02 - forEach 로 체크된 기분 라디오 버튼 추출 구현

--- 01-diary/index.js
+++ 01-diary/index.js
@@ -8,31 +8,30 @@ const validateInputText = () => {
     console.log("입력된 텍스트를 검증합니다.")
     inputTitle = document.getElementById("input_diary_title").value
     inputText = document.getElementById("input_diary_text").value
-    moodChecked = checkedInputMood()
-    activateWriteButton(moodChecked)
+    checkedInputMood()
+    activateWriteButton()
 }
 
 const checkedInputMood = () => {
     const moodList = document.getElementsByName("mood_radio")
-    for (let i = 0; i < moodList.length; i = i + 1) {
-        if (moodList[i].checked) {
-            selectedMood = moodList[i].value
+
+    moodList.forEach(el => {
+        if (el.checked) {
+            selectedMood = el.value
             console.log(`선택된 기분은 "${selectedMood}" 입니다.`)
-            return true
-        } 
-    }
-    return false
+        }
+    })
 }
 
-const activateWriteButton = (moodChecked) => {
-    if (inputTitle !== "" && inputText !== "" && moodChecked === true) {
+const activateWriteButton = () => {
+    if (inputTitle !== "" && inputText !== "" && selectedMood !== "") {
         console.log("등록 버튼이 활성화 됩니다.")
         document.getElementById("writeButton").disabled = false
         document.getElementById("writeButton").style = "background-color: black; color: white"
         return
     }
 
-    if (inputTitle === "" || inputText === "" || moodChecked === false) {
+    if (inputTitle === "" || inputText === "" || selectedMood === "") {
         console.log("등록 버튼이 비활성화 됩니다.")
         document.getElementById("writeButton").disabled = true
         document.getElementById("writeButton").style = "background-color: #C7C7C7; color: #F2F2F2"  
