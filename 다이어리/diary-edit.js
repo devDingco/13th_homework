e973695c2ee1c47e8778615892e6584844c83ccc
yheseul <kangyheseul@naver.com>
2024-09-01 21:20:19 +0900

    feat: 상세페이지에서 다이어리 수정 기능 구현

--- 다이어리/diary-edit.js
+++ 다이어리/diary-edit.js
@@ -1,12 +1,54 @@
+const editBtn = document.getElementById("edit_button");
 const queryString = location.search;
 const part = new URLSearchParams(queryString);
 const id = part.get("id");
 const diaryDetailContentList = JSON.parse(localStorage.getItem("diaryList"));
-const diaryDetailContent = diaryDetailContentList.filter((e) => e.id == id)[0];
 
-const check = document.getElementById(`check_${diaryDetailContent.imageName}`);
-const title = document.getElementsByClassName("title_edit_input")[0];
-const content = document.getElementsByClassName("content_edit_input")[0];
+let [obj, index] = [];
+
+diaryDetailContentList.map((e, i) => {
+  if (e.id == id) [obj, index] = [e, i];
+});
+
+const check = document.getElementById(`check_${obj.imageName}`);
+const title = document.getElementById("title_edit_input");
+const content = document.getElementById("content_edit_input");
 check.checked = true;
-title.value = diaryDetailContent.title;
-content.value = diaryDetailContent.content;
+title.value = obj.title;
+content.value = obj.content;
+
+const cancel = document.getElementById("cancel");
+const edit = document.getElementById("edit");
+
+cancel.href = `./diary-detail.html?id=${id}`;
+edit.href = `./diary-detail.html?id=${id}`;
+
+const handleSaveChanges = () => {
+  let updatedMood;
+  let updatedMoodId;
+
+  const getUpdatedMood = document.getElementsByName("mood");
+
+  getUpdatedMood.forEach((mood) => {
+    if (mood.checked) {
+      updatedMood = mood.nextElementSibling.innerText;
+      updatedMoodId = mood.id;
+    }
+  });
+  const updateImageName = updatedMoodId.split(`_`)[1];
+  const updateColor = `${updateImageName}_emotion_font_color`;
+  const updatedTitle = document.getElementById("title_edit_input");
+  const updatedContent = document.getElementById("content_edit_input");
+
+  obj.title = updatedTitle.value;
+  obj.content = updatedContent.value;
+  obj.mood = updatedMood;
+  obj.imageName = updateImageName;
+  obj.color = updateColor;
+
+  diaryDetailContentList.splice(index, 1, obj);
+  localStorage.removeItem("diaryList");
+  localStorage.setItem("diaryList", JSON.stringify(diaryDetailContentList));
+};
+
+editBtn.addEventListener("click", handleSaveChanges);
