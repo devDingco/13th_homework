dffb799727222d11588b7e4740c8c942450fac8d
Dain <danggin.min@gmail.com>
2024-09-03 19:11:50 +0900

    ✨ feat: 스크롤 될 때 header에 blur 추가

--- section03/javascript/common.js
+++ section03/javascript/common.js
@@ -1,3 +1,7 @@
 const scrollToTop = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+window.addEventListener('scroll', () => {
+  document.querySelector('.header').style = 'backdrop-filter: blur(50px)';
+});
