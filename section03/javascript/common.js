618248e0c9d863e50a546abc154b2be212e2cc03
Dain <danggin.min@gmail.com>
2024-09-03 21:16:24 +0900

    ✨ feat: 푸터 영역까지 스크롤 시 플로틴 버튼 위치 바뀌는 기능 추가

--- section03/javascript/common.js
+++ section03/javascript/common.js
@@ -3,5 +3,22 @@ const scrollToTop = () => {
 };
 
 window.addEventListener('scroll', () => {
+  const windowToFooter = document.querySelector('.footer')?.getBoundingClientRect().top;
+  const windowheight = window.innerHeight;
+
+  if (windowheight >= windowToFooter) {
+    document.querySelector('.scroll-up').style = `
+      position: fixed;
+      right: 10px;
+      bottom: 150px;
+      `;
+  } else {
+    document.querySelector('.scroll-up').style = `
+      position: fixed;
+      right: 10px;
+      bottom: 32px;
+      `;
+  }
+
   document.querySelector('.header').style = 'backdrop-filter: blur(50px)';
 });
