160f27cae05c878ef10f21a579863a7d09b7c2fe
Dain <danggin.min@gmail.com>
2024-09-03 16:50:03 +0900

    ♻️ refactor: 공통으로 사용하는 코드만 common.js 로 분기

--- section03/javascript/common.js
+++ section03/javascript/common.js
@@ -1,29 +1,3 @@
-let initialScroll = 0;
-
 const scrollToTop = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 };
-
-window.addEventListener('scroll', () => {
-  const filterElem = document.querySelector('.filter-emotion');
-
-  filterElem.style = `
-        background-color: var(--gray-b);
-        color: var(--gray-w);
-    `;
-
-  filterElem.classList.add('scrolled');
-
-  const currentScroll = window.scrollY;
-
-  if (currentScroll === 0 || initialScroll > currentScroll) {
-    filterElem.style = `
-        background-color: var(--gray-w);
-        color: var(--gray-b);
-    `;
-
-    filterElem.classList.remove('scrolled');
-  }
-
-  initialScroll = currentScroll;
-});
--- section03/javascript/index.js
+++ section03/javascript/index.js
@@ -144,3 +144,29 @@ const filterEmotion = (event) => {
 
   document.querySelector('.panel-list').innerHTML = filteredListHTML.join('');
 };
+
+let initialScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const filterElem = document.querySelector('.filter-emotion');
+
+  filterElem.style = `
+        background-color: var(--gray-b);
+        color: var(--gray-w);
+    `;
+
+  filterElem.classList.add('scrolled');
+
+  const currentScroll = window.scrollY;
+
+  if (currentScroll === 0 || initialScroll > currentScroll) {
+    filterElem.style = `
+        background-color: var(--gray-w);
+        color: var(--gray-b);
+    `;
+
+    filterElem.classList.remove('scrolled');
+  }
+
+  initialScroll = currentScroll;
+});
