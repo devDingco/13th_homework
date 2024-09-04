e68a804649d93a90d39613d27ad1933f3ac0bdd6
Dain <danggin.min@gmail.com>
2024-09-04 16:23:47 +0900

    ♻️ refactor: 공통인 코드만 common.js에 남도록 수정

--- section03/javascript/index.js
+++ section03/javascript/index.js
@@ -11,25 +11,6 @@ window.addEventListener('scroll', () => {
     filterElem.classList.add('scrolled');
   }
   initialScroll = currentScroll;
-
-  const windowToFooter = document.querySelector('.footer')?.getBoundingClientRect().top;
-  const windowheight = window.innerHeight;
-
-  if (windowheight >= windowToFooter) {
-    document.querySelector('.scroll-up').style = `
-      position: fixed;
-      right: 10px;
-      bottom: 150px;
-      `;
-  } else {
-    document.querySelector('.scroll-up').style = `
-      position: fixed;
-      right: 10px;
-      bottom: 32px;
-      `;
-  }
-
-  document.querySelector('.header').style = 'backdrop-filter: blur(50px)';
 });
 
 window.onload = () => {
