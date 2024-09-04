b9bc9fc26f8c7758d6591693ec6db5b30bba1219
류지승 <rjs8833@gmail.com>
2024-09-05 08:47:06 +0900

    [240905] assignment complete

--- utils/onClickDeletePost.js
+++ utils/onClickDeletePost.js
@@ -2,11 +2,11 @@ function onClickDeletePost() {
     const dailyArray = JSON.parse(localStorage.getItem("dailyArray"));
     const queryString = location.search;
     const queryId = new URLSearchParams(queryString).get("id");
-    console.log(queryId);
 
     const deleteArray = dailyArray.filter(
         (daily) => daily.id != Number(queryId)
     );
     localStorage.setItem("dailyArray", JSON.stringify(deleteArray));
+    alert("삭제 되었습니다.");
     history.back();
 }
