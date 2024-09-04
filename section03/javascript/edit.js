0fc55f856a6733a7edd64839d97f736708dfd1f4
Dain <danggin.min@gmail.com>
2024-09-04 16:24:42 +0900

    ✨ feat: 일기 수정 페이지 기존 데이터 보여주기, 수정 기능 추가

--- section03/javascript/edit.js
+++ section03/javascript/edit.js
@@ -1,3 +1,18 @@
+const queryString = window.location.search;
+const parameters = new URLSearchParams(queryString);
+const itemNumber = parameters.get('number');
+
+window.onload = () => {
+  const diaryListString = localStorage.getItem('myDiaryList') !== null ? localStorage.getItem('myDiaryList') : '[]';
+  const diaryList = JSON.parse(diaryListString);
+
+  document.querySelector('#input-title').value = diaryList[itemNumber].title;
+  document.querySelector('#input-content').value = diaryList[itemNumber].content;
+  window.document.getElementsByName('emotion').forEach((el) => {
+    if (el.id === diaryList[itemNumber].emotion) el.checked = true;
+  });
+};
+
 const skew = (elem, number) => {
   elem.style = `transform: skew(${number}deg)`;
 };
@@ -7,5 +22,40 @@ const clickHandler = () => {
   const diaryList = JSON.parse(diaryListString);
   const buttonEdit = document.querySelector('.button-edit');
 
+  const newTitle = document.querySelector('#input-title').value;
+  const newContent = document.querySelector('#input-content').value;
+  const newEmotion =
+    document.querySelector('#happy').checked === true
+      ? 'happy'
+      : document.querySelector('#sad').checked === true
+      ? 'sad'
+      : document.querySelector('#angry').checked === true
+      ? 'angry'
+      : document.querySelector('#surprising').checked === true
+      ? 'surprising'
+      : document.querySelector('#etc').checked === true
+      ? 'etc'
+      : '';
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDay()).padStart(2, '0');
+  const newDate = `${year}. ${month}. ${day}`;
+
+  diaryList[itemNumber] = {
+    title: newTitle,
+    content: newContent,
+    emotion: newEmotion,
+    createdAt: newDate,
+  };
+
+  window.localStorage.setItem('myDiaryList', JSON.stringify(diaryList));
+
   skew(buttonEdit, 5);
+
+  location.replace(`./detail.html?number=${itemNumber}`);
+};
+
+const goToBack = () => {
+  window.location.replace(`./detail.html?number=${itemNumber}`);
 };
