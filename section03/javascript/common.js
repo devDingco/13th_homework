66215ac683e43275278b030c4fc21190ebf8f85a
Dain <danggin.min@gmail.com>
2024-09-04 13:01:38 +0900

    ♻️ refactor: 공통인 코드만 common.js에 남도록 수정

--- section03/javascript/common.js
+++ section03/javascript/common.js
@@ -1,24 +1,3 @@
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
 window.addEventListener('scroll', () => {
-  const windowToFooter = document.querySelector('.footer')?.getBoundingClientRect().top;
-  const windowheight = window.innerHeight;
-
-  if (windowheight >= windowToFooter) {
-    document.querySelector('.scroll-up').style = `
-      position: fixed;
-      right: 10px;
-      bottom: 150px;
-      `;
-  } else {
-    document.querySelector('.scroll-up').style = `
-      position: fixed;
-      right: 10px;
-      bottom: 32px;
-      `;
-  }
-
   document.querySelector('.header').style = 'backdrop-filter: blur(50px)';
 });
--- section03/javascript/index.js
+++ section03/javascript/index.js
@@ -11,10 +11,33 @@ window.addEventListener('scroll', () => {
     filterElem.classList.add('scrolled');
   }
   initialScroll = currentScroll;
+
+  const windowToFooter = document.querySelector('.footer')?.getBoundingClientRect().top;
+  const windowheight = window.innerHeight;
+
+  if (windowheight >= windowToFooter) {
+    document.querySelector('.scroll-up').style = `
+      position: fixed;
+      right: 10px;
+      bottom: 150px;
+      `;
+  } else {
+    document.querySelector('.scroll-up').style = `
+      position: fixed;
+      right: 10px;
+      bottom: 32px;
+      `;
+  }
+
+  document.querySelector('.header').style = 'backdrop-filter: blur(50px)';
 });
 
 window.onload = () => {
   showDiary();
+};
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 const renderData = (array) => {
