9bd8208e33f681d1e1c16dc6ac3355b2bff0ecab
Dain <danggin.min@gmail.com>
2024-09-06 09:39:44 +0900

    ✨ feat: 모달 열었을 때 배경 스크롤 위로 이동 및 스크롤 안되게 설정

--- section05/javascript/common.js
+++ section05/javascript/common.js
@@ -25,7 +25,10 @@ const scrollToTop = () => {
 
 const showModal = (target) => {
   document.querySelector(target).style = 'display: block;';
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  document.querySelector('.app').style = 'overflow-y: hidden; max-height: 100vh;';
 };
 const closeModal = (target) => {
   document.querySelector(target).style = 'display: none;';
+  document.querySelector('.app').style = 'overflow-y: auto; max-height: unset;';
 };
