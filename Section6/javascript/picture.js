38497e5aa76c48bead7a5b56f9b2437a2f9611cf
안상현 <tedan96@naver.com>
2024-09-07 19:15:12 +0900

    [240907] Section6 과제

---
+++ Section6/javascript/picture.js
@@ -0,0 +1,21 @@
+// const JS_필터링기능 = (event) => {
+//     const 필터선택 = event.target.value;
+//     let 유형선택;
+//     switch(필터선택) {
+//         // case "가로형": {
+//         //     유형선택 = 
+//         // }
+//     }
+// }
+
+// const JS_강아지사진 = () => {
+//     fetch("https://dog.ceo/api/breeds/image/random/4").then((받아온결과) => {
+//         받아온결과.json().then((객체로변한결과) => {
+//             console.log("결과:", 객체로변경한결과)
+//             const 상자 = 객체로변한결과.message
+//             document.getElementById("HTML_이미지").innerHTML = `
+//             <img src="${상자}" width:"30px" />
+//             `
+//         })
+//     })
+// }
