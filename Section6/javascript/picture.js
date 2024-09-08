c7db8e25b4fe18c542b9023490992b147473f970
안상현 <tedan96@naver.com>
2024-09-08 19:02:35 +0900

    강아지 사진 10개 띄우기, 필터 작업 진행중

--- Section6/javascript/picture.js
+++ Section6/javascript/picture.js
@@ -1,21 +1,21 @@
-// const JS_필터링기능 = (event) => {
-//     const 필터선택 = event.target.value;
-//     let 유형선택;
-//     switch(필터선택) {
-//         // case "가로형": {
-//         //     유형선택 = 
-//         // }
-//     }
-// }
+const JS_필터링기능 = (event) => {
+    const 필터선택 = event.target.value;
+    const 이미지 = document.querySelectorAll("HTML_이미지");
+    이미지.forEach((image) => {
+        switch (필터선택) {
+            case "기본형" : {
+                image.setAttribute("style", "aspect-ratio: 1/1;");
+                break;
+            }
+            case "가로형" : {
+                image.setAttribute("style", "aspect-ration: 4 / 3;");
+                break;
+            }
+            case "세로형" : {
+                image.setAttribute("style", "aspect-ration: 3 / 4;");
+                break;
+            }
+          }
+    })
 
-// const JS_강아지사진 = () => {
-//     fetch("https://dog.ceo/api/breeds/image/random/4").then((받아온결과) => {
-//         받아온결과.json().then((객체로변한결과) => {
-//             console.log("결과:", 객체로변경한결과)
-//             const 상자 = 객체로변한결과.message
-//             document.getElementById("HTML_이미지").innerHTML = `
-//             <img src="${상자}" width:"30px" />
-//             `
-//         })
-//     })
-// }
+}
