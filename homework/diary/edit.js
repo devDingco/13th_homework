d835315653122ef3b1541bfcce95a999bfffd675
janghwahyun <1117jhh@gmail.com>
2024-09-11 16:18:44 +0900

    [240911] 수정페이지 JS_회고 보여주기기능 구현함

--- homework/diary/edit.js
+++ homework/diary/edit.js
@@ -17,6 +17,8 @@ window.onload = () => {
     window.document.getElementsByName('감정').forEach((el) => {
         if (el.value === 일기담는통.기분) el.checked = true;
     });
+
+    JS_회고그리기기능();
 };
 
 const JS_수정기능 = () => {
@@ -59,4 +61,42 @@ const JS_수정취소하기기능 = () => {
     const 일기번호 = 잘게나누어담은통.get('number');
 
     window.location.replace(`./detail.html?number=${일기번호}`);
+};
+
+const JS_회고그리기기능 = () => {
+    // 주소에서 일기번호 가져오기
+    const 쿼리스트링 = window.location.search;
+    const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+    const 일기번호 = 잘게나누어담은통.get('number');
+
+    // 스토리지에 저장된 일기목록 가져오기
+    const 로컬스토리지저장된일기목록 =
+        localStorage.getItem('민지의일기목록') ?? '[]';
+    const 일기목록 = JSON.parse(로컬스토리지저장된일기목록);
+
+    const 일기담는통 = 일기목록[일기번호];
+    // console.log(일기담는통);
+
+    // 현재일기에서 회고목록만 뽑아내기
+    // 위에 일기담는통에는 회고목록이라는 key값이 없음
+    // 따라서 일기담는통.회고목록 = undefined이 됨
+    // undefinedrk 되면 새로운 배열을 넣어줌
+    const 회고목록 = 일기담는통.회고목록 ?? [];
+
+    // 회고목록 화면에 새롭게 전체 그리기
+    let HTML_새로운회고목록 = '';
+    회고목록.forEach((element, index) => {
+        const 마지막요소 = index === 회고목록.length - 1;
+
+        HTML_새로운회고목록 =
+            HTML_새로운회고목록 +
+            `
+        <div class="CSS_회고담는상자">
+            <div class="CSS_회고내용">${element.회고내용}</div>
+            <div class="CSS_회고작성일">[${element.작성일}]</div>
+        </div>
+        `;
+    });
+    document.getElementById('id_회고목록보여주기').innerHTML =
+        HTML_새로운회고목록;
 };
