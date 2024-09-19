61bfdcf3667fae988fd51ca40ebb155a4c4747e2
h0ngsik <hongsik.choi.dev@gmail.com>
2024-09-19 17:58:06 +0900

    [240919]: Day16 과제 구현

---
+++ 02-main_camp/index.js
@@ -0,0 +1,128 @@
+const PostEditor = () => {
+    const writerDescription = "작성자명을 입력해 주세요."
+    const passwordDescription = "비밀번호를 입력해 주세요."
+    const titleDescription = "제목을 입력해 주세요."
+    const contentsDescription = "내용을 입력해 주세요."
+    const youtubeLinkDescription = "링크를 입력해 주세요."
+
+    return (
+        <div className="rootContainer">
+            <header>게시물 등록</header>
+            <main>
+                <div className="inputUserForm">
+                    <InputForm isRequired={true} title="작성자" placeholder={writerDescription}/>
+                    <InputForm isRequired={true} title="비밀번호" placeholder={passwordDescription}/>
+                </div>
+                <Divider />
+                <InputForm isRequired={true} title="제목" placeholder={titleDescription}/>
+                <Divider />
+                <ContentsInputForm title="내용" placeholder={contentsDescription}/>
+                <AddressInputForm />
+                <Divider />
+                <InputForm isRequired={false} title="유튜브 링크" placeholder={youtubeLinkDescription}/>
+                <Divider />
+                <PhotoUploadForm />
+            </main>
+            <footer>
+                <RegisterForm />
+            </footer>
+        </div>
+    );
+}
+
+const InputForm = (props) => {
+    if (props.isRequired) {
+        return (
+            <div id="PostInputForm" className="inputForm">
+                <div className="inputTitle">{props.title}<span class="requiredMark">*</span></div>
+                <TextInput placeholder={props.placeholder}/>
+            </div>
+            
+        );        
+    } else {
+        return (
+            <div id="PostInputForm" className="inputForm">
+                <div className="inputTitle">{props.title}</div>
+                <TextInput placeholder={props.placeholder}/>
+            </div>
+        );
+    }
+}
+
+const ContentsInputForm = (props) => {
+    return (
+        <div className="inputForm">
+            <div className="inputTitle">{props.title}<span class="requiredMark">*</span></div>
+            <textarea className="inputTextArea" placeholder={props.placeholder}></textarea>
+        </div>
+    );
+}
+
+const AddressInputForm = () => {
+    return (
+        <div className="inputForm">
+            주소
+            <div class="ZipCodeContainer">
+                <TextInput placeholder="01234"/>
+                <ZipCodeSearchButton />
+            </div>
+            <TextInput placeholder="주소를 입력해 주세요." />
+            <TextInput placeholder="상세 주소" />
+        </div>
+    );
+}
+
+const PhotoUploadForm = () => {
+    return (
+        <div className="inputForm">
+            사진 첨부
+            <div class="uploadButtonContainer">
+                <UploadButton />
+                <UploadButton />
+                <UploadButton />
+            </div>
+        </div>
+        
+    );
+}
+
+const TextInput = (props) => {
+    return <input className="inputText" type="text" placeholder={props.placeholder} />;
+}
+
+const Divider = () => {
+    return <div class="divider"></div>;
+}
+
+const ZipCodeSearchButton = () => {
+    return (
+        <button class="zipCodeSearchButton">
+            우편번호 검색
+        </button>
+    );
+}
+
+const UploadButton = () => {
+    return (
+        <div class="uploadImageContainer">
+            <input type="file" id="uploadImage" />
+            <div class="uploadDescription">
+                <label for="uploadImage" id="uploadLabel">
+                    <img src="./assets/add.png" />
+                    클릭해서 사진 업로드
+                </label>
+            </div>
+            
+        </div>
+    );
+}
+
+const RegisterForm = () => {
+    return (
+        <>
+            <button class="cancelButton">취소</button>
+            <button class="submitButton">등록하기</button>
+        </>
+    );
+}
+
