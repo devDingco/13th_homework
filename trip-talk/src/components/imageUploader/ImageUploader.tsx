846493d8596a6f31b6df254db1068214a11736db
yheseul <kangyheseul@naver.com>
2024-09-20 03:02:57 +0900

    style: imageUploader 컴포넌트 퍼블리싱 및 CSS 파일 추가

---
+++ trip-talk/src/components/imageUploader/ImageUploader.tsx
@@ -0,0 +1,13 @@
+import add from '../../assets/svgs/add.svg'
+import styles from "./imageUploader.module.css";
+
+export default function ImageUploader() {
+  return (
+    <>
+      <div className={styles.image_uploader_wrapper}>
+        <img src={add} />
+        <div className={styles.upload_prompt}>클릭해서 사진 업로드</div>
+      </div>
+    </>
+  );
+}
