295530a1f8efbe2ab5b1ccacfc35b072afc94b2e
안상현 <tedan96@naver.com>
2024-09-20 12:36:46 +0900

    [240920] 업로드1

--- React/button.js
+++
@@ -1,7 +0,0 @@
-const Search = ({name, placeholder}) => {
-    return (
-        <div>
-            <button name={id} placeholder={placeholder} className={`css_${name}button`} />
-        </div>
-    );
-};
