a364bb9eb05e93a3d1e27c2ad55b8aa10dd832e7
류지승 <rjs8833@gmail.com>
2024-09-20 09:58:45 +0900

    test

--- utils/onClickCancelDaily.js
+++
@@ -1,5 +0,0 @@
-function onClickCancelDaily() {
-    const modalElement = document.getElementById("modal_fail_container");
-
-    modalElement.style.display = "none";
-}
--- utils/onClickDetailModal.js
+++
@@ -1,5 +0,0 @@
-function onClickDetailModal() {
-    const modalElement = document.getElementById("modal_fail_container");
-
-    modalElement.style.display = "flex";
-}
