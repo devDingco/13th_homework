969d9fbb3b9a83792e3ec7e5fb271d700ba4b970
seolmiseon <budaxige@gmail.com>
2024-09-21 14:44:32 +0900

    [240921]Day17 과제제출 설미선

---
+++ react1/youtuber.js
@@ -0,0 +1,11 @@
+const YoutubeLink = () => {
+    return (
+        <>
+            <div className="form-content">
+            <label htmlFor="youtubeUrl">유투브 링크</label>
+            
+                <input id="youtubeUrl" type="url" placeholder="링크를 입력해주세요"/>
+            </div>
+        </>
+    )
+}
