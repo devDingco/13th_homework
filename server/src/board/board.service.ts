fbf3a67577141eb5b29672190d0df22dd19ee23f
류지승 <rjs8833@gmail.com>
2024-09-22 14:26:51 +0900

    [240922] complete create except autoincrement

--- server/src/board/board.service.ts
+++ server/src/board/board.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
 import { BoardRepository } from './repositories/boardRepository';
 import { BoardIdCounterRepository } from './repositories/boardIdCounterRepository';
@@ -11,24 +10,22 @@ import { BoardIdCounterRepository } from './repositories/boardIdCounterRepositor
 @Injectable()
 export class BoardService {
     constructor(
-        @InjectRepository(Board)
-        private boardRepository: BoardRepository,
-        private boardIdCounterRepository: BoardIdCounterRepository,
+        private readonly boardRepository: BoardRepository,
+        private readonly boardIdCounterRepository: BoardIdCounterRepository,
     ) {}
     async create(createBoardDto: CreateBoardDto): Promise<Board> {
         const board = this.boardRepository.createBoard(createBoardDto);
-        const boardId =
-            await this.boardIdCounterRepository.incrementBoardId('Boards');
-
-        return await this.boardRepository.save({ board, boardId });
+        // const boardId =
+        //     await this.boardIdCounterRepository.incrementBoardId('Boards');
+        return await this.boardRepository.saveBoard(board);
     }
 
     async findAll(): Promise<Board[]> {
-        return await this.boardRepository.find();
+        return await this.boardRepository.findAllBoard();
     }
 
     async findOne(id: number) {
-        return await this.boardRepository.findOne({ where: { id } });
+        return await this.boardRepository.findBoard(id);
     }
 
     update(id: number, updateBoardDto: UpdateBoardDto) {
--- server/src/board/repositories/boardRepository.ts
+++ server/src/board/repositories/boardRepository.ts
@@ -2,17 +2,35 @@ import { MongoRepository } from 'typeorm';
 import { Board } from '../entities/board.entity';
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from '../dto/create-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class BoardRepository extends MongoRepository<Board> {
+export class BoardRepository {
+    constructor(
+        @InjectRepository(Board)
+        private readonly boardRepository: MongoRepository<Board>,
+    ) {}
+
     createBoard(createBoardDto: CreateBoardDto): Board {
         const { author, title, content, imageUrl, youtubeUrl } = createBoardDto;
-        return this.create({
+        return this.boardRepository.create({
             author,
             title,
             content,
             ...(imageUrl?.length > 0 && { imageUrl }),
             ...(youtubeUrl && { youtubeUrl }),
         });
+    }
+
+    async saveBoard(board: Board): Promise<Board> {
+        return await this.boardRepository.save(board);
+    }
+
+    async findAllBoard(): Promise<Board[]> {
+        return await this.boardRepository.find();
+    }
+
+    async findBoard(id: number): Promise<Board[]> {
+        return await this.boardRepository.find({ where: { id } });
     }
 }
