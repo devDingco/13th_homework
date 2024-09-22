c30f00b119f906ce9bb37bdd7f93c48fc636d47a
jomira0220 <whalfk220@gmail.com>
2024-09-22 21:02:44 +0900

    메인 스타일 수정

--- react 과제/src/layout/main.tsx
+++ react 과제/src/layout/main.tsx
@@ -4,7 +4,7 @@ const Main = (props: any) => {
   return (
     <main>
       <Header />
-      <div className="max-w-7xl m-auto py-10 max-md:max-w-full max-md:mx-5 flex flex-col gap-10">
+      <div className="max-w-7xl m-auto py-10 max-md:max-w-full max-lg:mx-5 flex flex-col gap-10">
         {props.children}
       </div>
     </main>
