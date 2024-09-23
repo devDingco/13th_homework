bfce18f47f3c417cbfa2f20744bf7d05d00f7018
salwwkd <hettzze@gmail.com>
2024-09-23 17:57:42 +0900

    [240923] day17 과제제출

---
+++ myproject/src/App.js
@@ -0,0 +1,221 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // const [errorMessage, setErrorMessage] = useState("");
+
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [contentError, setContentError] = useState("");
+
+  const onChangeName = (event) => {
+    const value = event.target.value;
+    setName(value);
+    if (value === "") {
+      setNameError("필수입력 사항입니다");
+    } else {
+      setNameError("");
+    }
+    // setErrorMessage("");
+    // if (name && password && title && content) return setErrorMessage(true);
+    // setErrorMessage(false);
+  };
+
+  const onChangePassword = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+    if (value === "") {
+      setPasswordError("필수입력 사항입니다");
+    } else {
+      setPasswordError("");
+    }
+    // setErrorMessage("");
+    // if (name && password && title && content) return setErrorMessage(true);
+    // setErrorMessage(false);
+  };
+
+  const onChangeTitle = (event) => {
+    const value = event.target.value;
+    setTitle(value);
+    if (value === "") {
+      setTitleError("필수입력 사항입니다");
+    } else {
+      setTitleError("");
+    }
+    // setErrorMessage("");
+    // if (name && password && title && content) return setErrorMessage(true);
+    // setErrorMessage(false);
+  };
+
+  const onChangeContent = (event) => {
+    const value = event.target.value;
+    setContent(value);
+    if (value === "") {
+      setContentError("필수입력 사항입니다");
+    } else {
+      setContentError("");
+    }
+    // setErrorMessage("");
+    // if (name && password && title && content) return setErrorMessage(true);
+    // setErrorMessage(false);
+  };
+
+  const onClickSignup = (event) => {
+    console.log("버튼클릭확인");
+    // if (
+    //   name.length === 0 ||
+    //   password.length === 0 ||
+    //   title.length === 0 ||
+    //   content.length === 0
+    // ) {
+    //   setErrorMessage("필수입력 사항입니다");
+    // } else {
+    //   alert("게시글 등록이 가능한 상태입니다");
+    // }
+    // console.log("name:", name);
+    // console.log("password:", password);
+    // console.log("title:", title);
+    // console.log("content:", content);
+
+    let message = false;
+    if (name === "") {
+      setNameError("필수입력 사항입니다");
+      message = true;
+    }
+    if (password === "") {
+      setPasswordError("필수입력 사항입니다");
+      message = true;
+    }
+    if (title === "") {
+      setTitleError("필수입력 사항입니다");
+      message = true;
+    }
+    if (content === "") {
+      setContentError("필수입력 사항입니다");
+      message = true;
+    }
+    if (!message) {
+      alert("게시글 등록이 가능합니다");
+    }
+  };
+
+  return (
+    <div className="전체창">
+      <header>게시물 등록</header>
+      <main>
+        <div className="메인첫번째박스">
+          <div className="작성자박스">
+            <label htmlFor="작성자인풋">
+              작성자<span>*</span>
+            </label>
+            <input
+              type="text"
+              onChange={onChangeName}
+              id="작성자인풋"
+              placeholder="작성자 명을 입력해 주세요."
+            />
+            <div className="에러메시지">{nameError}</div>
+          </div>
+          <div className="비밀번호박스">
+            <label htmlFor="비밀번호인풋">
+              비밀번호<span>*</span>
+            </label>
+            <input
+              type="password"
+              onChange={onChangePassword}
+              id="비밀번호인풋"
+              placeholder="비밀번호를 입력해 주세요."
+            />
+            <div className="에러메시지">{passwordError}</div>
+          </div>
+        </div>
+        <div className="제목박스">
+          <label htmlFor="제목인풋">
+            제목<span>*</span>
+          </label>
+          <input
+            type="text"
+            onChange={onChangeTitle}
+            id="제목인풋"
+            placeholder="제목을 입력해 주세요."
+          />
+          <div className="에러메시지">{titleError}</div>
+        </div>
+        <div className="내용박스">
+          <label htmlFor="내용인풋">
+            내용<span>*</span>
+          </label>
+          <textarea
+            type="text"
+            onChange={onChangeContent}
+            id="내용인풋"
+            placeholder="내용을 입력해 주세요."
+          ></textarea>
+          <div className="에러메시지">{contentError}</div>
+        </div>
+        <div className="주소박스">
+          <div className="우편번호전체박스">
+            <label htmlFor="우편번호인풋">주소</label>
+
+            <div className="우편번호창">
+              <input type="text" id="우편번호인풋" placeholder="01234" />
+              <div className="우편번호검색">우편번호 검색</div>
+            </div>
+          </div>
+          <input
+            className="주소입력인풋"
+            type="text"
+            placeholder="주소를 입력해 주세요."
+          ></input>
+          <input
+            className="상세주소입력인풋"
+            type="text"
+            placeholder="상세주소"
+          ></input>
+        </div>
+        <div className="유튜브링크박스">
+          <label htmlFor="유튜브링크인풋">유튜브링크</label>
+          <input
+            type="text"
+            id="유튜브링크인풋"
+            placeholder="링크를 입력해 주세요."
+          />
+        </div>
+        <div className="사진첨부여섯번째박스">
+          <label htmlFor="">사진 첨부</label>
+          <div id="사진업로드전체박스">
+            <div className="사진박스">
+              <img src="./add.png" alt="" />
+              <div>클릭해서 사진 업로드</div>
+            </div>
+            <div className="사진박스">
+              <img src="./add.png" alt="" />
+              <div>클릭해서 사진 업로드</div>
+            </div>
+            <div className="사진박스">
+              <img src="./add.png" alt="" />
+              <div>클릭해서 사진 업로드</div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <div className="푸터전체박스">
+          <div className="푸터취소">취소</div>
+          <div className="푸터등록하기" onClick={onClickSignup}>
+            등록하기
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
