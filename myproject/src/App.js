330854b555a487732f8f27f6b88432a80f843602
moonhuicheol <moonhuicheol@gmail.com>
2024-09-23 17:56:08 +0900

    [240923]Day18과제제출

---
+++ myproject/src/App.js
@@ -0,0 +1,173 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [errorMessage, setErrorMessage] = useState({
+    writer: "",
+    password: "",
+    title: "",
+    content: "",
+  });
+
+  const [validation, setValidation] = useState({
+    writer: "",
+    password: "",
+    title: "",
+    content: "",
+  });
+
+  const onChange = (e) => {
+    setValidation({
+      ...validation,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = () => {
+    let errors = {
+      writer: "",
+      password: "",
+      title: "",
+      content: "",
+    };
+
+    // 빈 값 검증
+    if (!validation.writer) errors.writer = "필수 입력 사항입니다.";
+    if (!validation.password) errors.password = "필수 입력 사항입니다.";
+    if (!validation.title) errors.title = "필수 입력 사항입니다.";
+    if (!validation.content) errors.content = "필수 입력 사항입니다.";
+
+    setErrorMessage(errors);
+
+    if (
+      validation.writer &&
+      validation.password &&
+      validation.title &&
+      validation.content
+    ) {
+      alert("게시글 등록이 가능한 상태입니다!");
+    }
+  };
+
+  return (
+    <div className="root">
+      <header>게시물 등록</header>
+      <main>
+        <div className="writer_password_box">
+          <div className="inputBox writer">
+            <label htmlFor="writerInput">
+              작성자<span>*</span>
+            </label>
+            <input
+              type="text"
+              id="writerInput"
+              placeholder="작성자 명을 입력해 주세요"
+              name="writer"
+              onChange={onChange}
+            ></input>
+            <div className="errorText writerError">{errorMessage.writer}</div>
+          </div>
+          <div className="inputBox password">
+            <label htmlFor="passwordInput">
+              비밀번호<span>*</span>
+            </label>
+            <input
+              type="text"
+              id="passwordInput"
+              placeholder="비밀번호를 입력해 주세요."
+              name="password"
+              onChange={onChange}
+            ></input>
+            <div className="errorText passwordError">
+              {errorMessage.password}
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="inputBox title">
+          <label htmlFor="titleInput">
+            제목<span>*</span>
+          </label>
+          <input
+            type="text"
+            id="titleInput"
+            placeholder="제목을 입력해 주세요."
+            name="title"
+            onChange={onChange}
+          ></input>
+          <div className="errorText titleError">{errorMessage.title}</div>
+        </div>
+        <hr />
+        <div className="inputBox content">
+          <label htmlFor="contentTextarea">
+            내용<span>*</span>
+          </label>
+          <textarea
+            id="contentTextarea"
+            placeholder="내용을 입력해주세요."
+            name="content"
+            onChange={onChange}
+          ></textarea>
+          <div className="errorText contentError">{errorMessage.content}</div>
+        </div>
+        <hr />
+        <div className="address_box">
+          <div className="inputBox addressNumber">
+            <label htmlFor="addressNumberInput">주소</label>
+            <div className="addressNumberSearchBox">
+              <input
+                type="text"
+                id="addressNumberInput"
+                placeholder="01234"
+              ></input>
+              <div className="addressNumberButton">우편번호 검색</div>
+            </div>
+          </div>
+          <input type="text" placeholder="주소를 입력해 주세요."></input>
+          <input type="text" placeholder="상세주소"></input>
+        </div>
+        <hr />
+        <div className="inputBox youtube">
+          <label htmlFor="youtubeInput">유튜브 링크</label>
+          <input
+            type="text"
+            id="youtubeInput"
+            placeholder="링크를 입력해 주세요."
+          ></input>
+        </div>
+        <hr />
+        <div className="inputBox photo">
+          <label>사진첨부</label>
+          <div className="photoCardBox">
+            <div className="photoBox">
+              <div className="photoCard">
+                <img src="/img/add.png" />
+                <div className="photo_text">클릭해서 사진 업로드</div>
+              </div>
+            </div>
+            <div className="photoBox">
+              <div className="photoCard">
+                <img src="/img/add.png" />
+                <div className="photo_text">클릭해서 사진 업로드</div>
+              </div>
+            </div>
+            <div className="photoBox">
+              <div className="photoCard">
+                <img src="/img/add.png" />
+                <div className="photo_text">클릭해서 사진 업로드</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <button className="button cancel">취소</button>
+        <button className="button register" onClick={onSubmit}>
+          등록하기
+        </button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ myproject/src/routes/broads/new/BoardsDetail.js
@@ -0,0 +1,91 @@
+import "./BoardsDetail.css";
+
+const BoardsDetail = () => {
+  const text = `
+살겠노라 살겠노라. 청산에 살겠노라.
+머루랑 다래를 먹고 청산에 살겠노라.
+얄리얄리 얄랑셩 얄라리 얄라
+
+우는구나 우는구나 새야. 자고 일어나 우는구나 새야.
+너보다 시름 많은 나도 자고 일어나 우노라.
+얄리얄리 얄라셩 얄라리 얄라
+
+갈던 밭(사래) 갈던 밭 보았느냐. 물 아래(근처) 갈던 밭 보았느냐
+이끼 묻은 쟁기를 가지고 물 아래 갈던 밭 보았느냐.
+얄리얄리 얄라셩 얄라리 얄라
+
+이럭저럭 하여 낮일랑 지내 왔건만
+올 이도 갈 이도 없는 밤일랑 또 어찌 할 것인가.
+얄리얄리 얄라셩 얄라리 얄라
+
+어디다 던지는 돌인가 누구를 맞히려던 돌인가.
+미워할 이도 사랑할 이도 없이 맞아서 우노라.
+얄리얄리 얄라셩 얄라리 얄라
+
+살겠노라 살겠노라. 바다에 살겠노라.
+나문재, 굴, 조개를 먹고 바다에 살겠노라.
+얄리얄리 얄라셩 얄라리 얄라
+
+가다가 가다가 듣노라. 에정지(미상) 가다가 듣노라.
+사슴(탈 쓴 광대)이 솟대에 올라서 해금을 켜는 것을 듣노라.
+얄리얄리 얄라셩 얄라리 얄라
+
+가다 보니 배불룩한 술독에 독한 술을 빚는구나.
+조롱박꽃 모양 누룩이 매워 (나를) 붙잡으니 내 어찌 하리이까.[1]
+얄리얄리 얄라셩 얄라리 얄라
+    `;
+  return (
+    <main>
+      <div className="title_box">
+        살어리 살어리랏다 쳥산(靑山)애 살어리랏다멀위랑 ᄃᆞ래랑 먹고
+        쳥산(靑山)애 살어리랏다얄리얄리 얄랑셩 얄라리 얄라
+      </div>
+      <div className="details_box">
+        <div className="writer_date_box">
+          <div className="writer_box">
+            <img src="/img/profile.png" />
+            <div className="writer">홍길동</div>
+          </div>
+          <div className="date_box">2024.11.11</div>
+        </div>
+        <hr />
+        <div className="icon_box">
+          <img src="/img/link.png" />
+          <img src="/img/location.png" />
+        </div>
+      </div>
+      <img
+        className="content_box_img"
+        src="/img/Tranquil Beachside Serenity 1.png"
+      />
+      <div className="content_box">
+        <pre>{text}</pre>
+      </div>
+      <div className="video_box">
+        <img src="/img/video.png" />
+      </div>
+      <div className="bad_good_box">
+        <div className="heart_box bad">
+          <img src="/img/bad.png" />
+          <div className="bad_count">24</div>
+        </div>
+        <div className="heart_box good">
+          <img src="/img/good.png" />
+          <div className="good_count">24</div>
+        </div>
+      </div>
+      <div className="list_edit_box">
+        <div className="button_box">
+          <img src="/img/menu.png" />
+          <div className="button_text">목록으로</div>
+        </div>
+        <div className="button_box">
+          <img src="/img/edit.png" />
+          <div className="button_text">수정하기</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default BoardsDetail;
