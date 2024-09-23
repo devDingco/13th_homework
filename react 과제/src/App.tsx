829edb3431df1c160f2e0050cdcc3c3e7d8bbe95
jomira0220 <whalfk220@gmail.com>
2024-09-24 08:55:55 +0900

    app 내용 수정

--- react 과제/src/App.tsx
+++ react 과제/src/App.tsx
@@ -1,15 +1,10 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 import Main from "./layout/main";
 function App() {
   return (
     <Main>
-      <h1 className="text-3xl text-blue-500">Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-        possimus architecto quidem ut officia quod maxime deleniti minus eum
-        numquam at dolorum animi debitis, quo quaerat molestiae quas velit
-        nobis?
-      </p>
+      <div>홈입니다.</div>
     </Main>
   );
 }
