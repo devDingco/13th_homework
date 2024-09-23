93080b9f35331b8f8b0ac68485c53f4ecaf63569
jomira0220 <whalfk220@gmail.com>
2024-09-24 08:59:11 +0900

    스타일 수정

--- react 과제/src/layout/main.tsx
+++ react 과제/src/layout/main.tsx
@@ -1,10 +1,10 @@
-import Header from "../components/header";
+import Header from "components/header";
 
 const Main = (props: any) => {
   return (
     <main>
       <Header />
-      <div className="max-w-7xl m-auto py-10 max-md:max-w-full max-lg:mx-5 flex flex-col gap-10">
+      <div className="max-w-7xl m-auto py-10 max-xl:max-w-full max-xl:mx-5 flex flex-col gap-10">
         {props.children}
       </div>
     </main>
