fa62947e4e61faaa7364a6709e5c7e6d44a2b7cc
김세연 <kimsaeyeon@gimseyeon-ui-MacBookAir.local>
2024-09-23 18:05:18 +0900

    라우터기능, 디테일페이지 퍼블리싱

--- cra-homework/src/index.js
+++ cra-homework/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import BoardsDetail from "./routes/boards/new/BoardsDetail";
+import BoardsNew from "./routes/boards/new/BoardsNew";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/boards/new", element: <BoardsNew /> },
+  { path: "/boards/detail", element: <BoardsDetail /> },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
