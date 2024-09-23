81f248069218996552283704395b2c66df88d5fa
yheseul <kangyheseul@naver.com>
2024-09-23 16:20:50 +0900

    feat: React-Router로 /boards/new 및 /boards/detail 경로 연결

--- trip-talk/src/main.tsx
+++ trip-talk/src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BoardsNew from "./routes/boards/new/BoardsNew.tsx";
+import BoardsDetail from "./routes/boards/new/BoardsDetail.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/boards/new", element: <BoardsNew />},
+  {path: "/boards/detail", element: <BoardsDetail />},
+]);
+
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
---
+++ trip-talk/src/routes/boards/new/BoardsDetail.tsx
@@ -0,0 +1,7 @@
+export default function BoardsDetail () {
+  return (
+    <>
+      This is detail page.
+    </>
+  )
+}
