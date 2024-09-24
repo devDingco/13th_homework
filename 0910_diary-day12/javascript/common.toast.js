04c92ab2b6e4fe0e11fe7e64ebba448fd4347d09
(suef606) <(suef3005@gmail.com)>
2024-09-24 12:17:43 +0900

    [240924]메인캠프_과제 다시 업로드

--- 0910_diary-day12/javascript/common.toast.js
+++
@@ -1,9 +0,0 @@
-const JS_복사토스트메시지실행기능 = () => {
-  window.document.getElementById("HTML_복사토스트메시지").style =
-    "display: block";
-
-  setTimeout(() => {
-    window.document.getElementById("HTML_복사토스트메시지").style =
-      "display: none";
-  }, 1000);
-};
