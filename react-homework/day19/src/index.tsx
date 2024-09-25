4b8313fd16649301bfa9f7b3f2a64b341516c9ed
MingseoYa <alstj3758@naver.com>
2024-09-25 23:22:00 +0900

    [240925] next 마이그레이션

--- react-homework/day19/src/index.tsx
+++ react-homework/day19/src/index.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import BoardsNew from './routes/boards/new/BoardsNew';
-import BoardsDetail from './routes/boards/detail/BoardsDetail';
-import './routes/boards/new/BoardsDetail.css';
-import './routes/boards/new/BoardsNew.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import BoardsNew from "./routes/boards/new/BoardsNew";
+import BoardsDetail from "./routes/boards/detail/BoardsDetail";
+import "./routes/boards/detail/BoardsDetail.css";
+import "./routes/boards/new/BoardsNew.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const page = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/boards/new', element: <BoardsNew /> },
-  { path: '/boards/detail', element: <BoardsDetail /> },
-])
+  { path: "/", element: <App /> },
+  { path: "/boards/new", element: <BoardsNew /> },
+  { path: "/boards/detail", element: <BoardsDetail /> },
+]);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root') as ReactDOM.Container);
-root.render(
-  <RouterProvider router={page} />
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as ReactDOM.Container,
 );
-
+root.render(<RouterProvider router={page} />);
---
+++ react-homework/day20/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(props) {
+  return (
+    <html lang="en">
+      <body>{props.children}</body>
+    </html>
+  );
+}
