3dc2a31127d71beaf4b2faad7a6bfbeaa8b5f405
jomira0220 <whalfk220@gmail.com>
2024-09-28 22:28:39 +0900

    redux 주석처리 및 메인 레이아웃 컴포넌트들 추가

--- next_과제/src/app/page.tsx
+++ next_과제/src/app/page.tsx
@@ -1,29 +1,91 @@
 "use client";
-import { useAppDispatch, useAppSelector } from "@/libs/redux/config/hook";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from "@/libs/redux/modules/slices/counter";
-import { useGetPokemonByNameQuery } from "@/services/pokemon";
+// import { useAppDispatch, useAppSelector } from "@/libs/redux/config/hook";
+// import {
+//   decrement,
+//   increment,
+//   incrementByAmount,
+// } from "@/libs/redux/modules/slices/counter";
+// import { useGetPokemonByNameQuery } from "@/services/pokemon";
 // import Image from "next/image";
 
-export default function Home() {
-  const counter = useAppSelector((state) => state.counter.value); // Return Root State Slices
-  const dispatch = useAppDispatch(); // Action Dispatcher
+import CardBox from "@/components/cardBox";
+import MainSlide from "@/components/mainSlide";
+import SearchBox from "@/components/searchBox";
+import Icon from "@/components/iconFactory";
+import BoardList from "@/app/board/list/page";
 
-  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+export default function Home() {
+  // const counter = useAppSelector((state) => state.counter.value); // Return Root State Slices
+  // const dispatch = useAppDispatch(); // Action Dispatcher
+
+  // const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+
+  const cardData = [
+    {
+      img: "/images/img-0.png",
+      title: "제목입니다.",
+      writer: "작성자이름",
+      writerimg: "/images/img-0.png",
+      goodcount: 0,
+      writedate: "2024.02.20",
+    },
+    {
+      img: "/images/img-0.png",
+      title: "제목입니다.",
+      writer: "작성자이름",
+      writerimg: "/images/img-0.png",
+      goodcount: 0,
+      writedate: "2024.02.20",
+    },
+    {
+      img: "/images/img-0.png",
+      title: "제목입니다.",
+      writer: "작성자이름",
+      writerimg: "/images/img-0.png",
+      goodcount: 0,
+      writedate: "2024.02.20",
+    },
+    {
+      img: "/images/img-0.png",
+      title: "제목입니다.",
+      writer: "작성자이름",
+      writerimg: "/images/img-0.png",
+      goodcount: 0,
+      writedate: "2024.02.20",
+    },
+  ];
 
   return (
     <div>
-      <img
-        src={`${process.env.NEXT_PUBLIC_URL}/images/beach.jpg`}
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
-      <div className="App">
-        {error ? (
+      <MainSlide />
+
+      <div className="max-w-7xl m-auto py-10 max-sm:pl-6 max-sm:pt-6">
+        <div className="flex flex-col gap-6">
+          <h3 className="font-bold text-xl">오늘 핫한 트립토크</h3>
+          <div className="grid grid-cols-4 gap-8 max-sm:flex max-sm:gap-4 max-sm:overflow-x-auto overflow-hidden max-sm:mr-[-100px]">
+            {cardData.map((data, idx) => {
+              return <CardBox key={idx} data={data} />;
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="mainContent">
+        <div className="flex flex-col gap-6">
+          <h3 className="font-bold text-xl">트립토크 게시판</h3>
+          <div className="flex gap-4 justify-between flex-wrap">
+            <SearchBox />
+            <button className="btn btn-info text-white max-sm:fixedBtn">
+              <span className="w-6 h-6 fill-white">
+                <Icon icon="rwite" />
+              </span>
+              트립토크 등록
+            </button>
+          </div>
+          <BoardList />
+        </div>
+      </div>
+      {/* {error ? (
           <>Oh no, there was an error</>
         ) : isLoading ? (
           <>Loading...</>
@@ -37,9 +99,9 @@ export default function Home() {
               height={100}
             />
           </>
-        ) : null}
-      </div>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        ) : null} */}
+
+      {/* <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <div className="text-center mb-12">Count is {counter}</div>
 
         <div className="flex">
@@ -69,7 +131,7 @@ export default function Home() {
             Incement By 10
           </button>
         </div>
-      </main>
+      </main> */}
     </div>
   );
 }
