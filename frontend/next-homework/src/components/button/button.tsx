6dad74a6ad9397817d66cb585f2ad0d23de9557f
main <0ksuyeon0@gmail.com>
2024-10-01 01:15:28 +0900

    feat: 동적 라우팅

--- frontend/next-homework/src/components/button/button.tsx
+++ frontend/next-homework/src/components/button/button.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import Image from "next/image";
+import styles from "./styles.module.css";
 import add from "../../../public/icons/add.svg";
 
 // 게시글 등록 페이지 버튼
 export const addressSearchButton = () => {
-  return <button className="addressSearch">우편번호 검색</button>;
+  return <button className={styles.addressSearch}>우편번호 검색</button>;
 };
 
 export const postCancelButton = () => {
-  return <button className="checkButton postCancelButton">취소</button>;
+  return <button className={`${styles.check} ${styles.cancle}`}>취소</button>;
 };
 
 export const postSubmitButton = ({ onClick }, buttonActiveStyle: boolean) => {
   return (
     <button
       id="postSubmitButton"
-      className="checkButton postSubmitButton"
+      className={`${styles.check} ${styles.submit}`}
       onClick={onClick}
       style={{ backgroundColor: buttonActiveStyle ? "var(--n-main, #2974e5)" : "var(--gray-300, #c7c7c7)" }}
     >
@@ -27,8 +28,8 @@ export const postSubmitButton = ({ onClick }, buttonActiveStyle: boolean) => {
 
 export const addImageButton = () => {
   return (
-    <button className="postUploadImageButton">
-      <Image src={add} alt="add" />
+    <button className={styles.uploadImg}>
+      <Image src={add} alt="add" width={0} height={0} />
       <p>사진 업로드</p>
     </button>
   );
