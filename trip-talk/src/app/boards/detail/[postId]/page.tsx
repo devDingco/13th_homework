32b1e0c3fb3b9f88b515642cf7d782f0b9ef6136
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-09-30 17:53:50 +0900

    [240930] 윤준수 DAY24 과제 완료

--- trip-talk/src/app/boards/detail/[postId]/page.tsx
+++ trip-talk/src/app/boards/detail/[postId]/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import React from 'react';
 
 import Image from 'next/image';
-import LinkIcon from '../../../../public/icons/link_icon.svg';
+import LinkIcon from '@/../public/icons/link_icon.svg';
 import LocationIcon from '@/../public/icons/location_icon.svg';
 import LikeIcon from '@/../public/icons/like_icon.svg';
 import UnLikeIcon from '@/../public/icons/unlike_icon.svg';
@@ -11,21 +13,51 @@ import MockImg1 from '@/../public/images/mockImg1.png';
 import MockImg2 from '@/../public/images/mockImg2.svg';
 import Button from '@/app/_component/form/Button';
 import s from './PostDetail.module.css';
-// import Button from '_component/form/Button';
+import dateFormatter from '@/app/_commons/formatter/dateFormat';
+import { useParams } from 'next/navigation';
+import { gql, useQuery } from '@apollo/client';
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      writer
+      title
+      contents
+      createdAt
+    }
+  }
+`;
+
 const PostDetail = () => {
+  const { postId: params } = useParams();
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { boardId: params },
+  });
+
   return (
     <>
       <div>
         <div className="border-b-[#E4E4E4] border-b border-solid">
-          <h3 className={s.H3}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          <h3
+            className={`${s.H3} ${data ? '' : 'bg-[#ccc] w-[100px] h-[20px]'}`}>
+            {data?.fetchBoard.title}
           </h3>
           <div className={`${s.flexbox} gap-1`}>
             <div className={`${s.flexbox} gap-1`}>
               <p className={s.userImg} />
-              <p className={s.userName}>윤준수</p>
+              <p
+                className={`${s.userName} ${
+                  data ? '' : 'bg-[#ccc] w-[100px] h-[20px]'
+                }`}>
+                {data?.fetchBoard.writer}
+              </p>
             </div>
-            <span className={s.dateFormat}>2024.09.23</span>
+            <span
+              className={`${s.dateFormat} ${
+                data ? '' : 'bg-[#ccc] w-[100px] h-[20px]'
+              }`}>
+              {data && dateFormatter(data?.fetchBoard.createdAt)}
+            </span>
           </div>
         </div>
         <section className="w-full">
@@ -38,31 +70,8 @@ const PostDetail = () => {
             </button>
           </div>
           <Image src={MockImg1} alt="" width={0} height={0} />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque quia
-            cupiditate labore quos voluptate, sit illo assumenda. Corporis
-            doloremque consectetur ipsam commodi vitae debitis accusantium, illo
-            dignissimos dolores quibusdam quas autem ab vel repudiandae
-            veritatis mollitia. Culpa unde repudiandae rem maxime perspiciatis
-            omnis quis voluptatibus eligendi. Iusto natus optio, velit dolorum
-            consectetur voluptates placeat tempora, iure, quam pariatur esse
-            fugiat magni quas necessitatibus sunt veniam? Quod ullam ea
-            accusantium ipsa voluptas esse, harum repellat labore. Commodi dicta
-            fuga et aliquid! Magni alias debitis consequatur voluptatum quae.
-            Accusamus culpa illum dicta, atque beatae ducimus eum, at facere
-            alias, sunt repudiandae repellendus. Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Officia dolore, fugit dicta minima,
-            omnis corrupti expedita vel quisquam, consectetur magnam eum sed
-            nostrum numquam sequi tenetur repellat! Repudiandae asperiores
-            tempore labore necessitatibus, totam est laborum excepturi quae, ex
-            nam placeat dolorum vero eveniet aliquid dignissimos ea earum
-            voluptate sapiente, assumenda itaque delectus? Alias repudiandae
-            error delectus quasi quos consequuntur distinctio repellendus quo
-            doloribus neque dolorem eveniet doloremque, debitis, ipsam magni
-            quibusdam? Quam dignissimos deleniti adipisci, facilis error illo
-            assumenda. Ipsa nobis consequatur tenetur nostrum nemo harum
-            praesentium id dolor illo sequi, deleniti quis quo doloribus placeat
-            obcaecati. Mollitia, dolorem repudiandae.
+          <p className={`${data ? '' : 'bg-[#ccc] w-[100px] h-[20px]'}`}>
+            {data?.fetchBoard.contents}
           </p>
           <div className={`${s.videoBox}`}>
             <Image src={MockImg2} alt="" width={0} height={0} />
