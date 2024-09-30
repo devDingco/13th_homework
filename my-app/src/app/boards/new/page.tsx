a61f6db76a11616cac40e9f9ad1bedff8c487c3b
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2024-09-30 09:41:47 +0900

    [240930] 이찬우 Day 22 과제제출

--- my-app/src/app/boards/new/page.tsx
+++ my-app/src/app/boards/new/page.tsx
@@ -1,7 +1,41 @@
-import "../css/boardNew.css"; // boardNew.css 파일 경로
-import { Link } from "react-router-dom";
+"use client";
+
+// import "../css/boardNew.css"; // boardNew.css 파일 경로
+import styles from "./styles.module.css";
+import Image from "next/image";
+// import { Link } from "react-router-dom";
+import Link from "next/link";
+
 import React, { ChangeEvent, useState } from "react";
-function BoardNew() {
+
+import { useMutation, gql } from "@apollo/client";
+
+const CREATE_BOARD = gql`
+  # 타입적는곳
+  mutation createBoard(
+    $writer: String
+    $password: String
+    $title: String!
+    $contents: String!
+  ) {
+    #전달할 변수 적는곳
+    createBoard(
+      createBoardInput: {
+        writer: $writer
+        password: $password
+        title: $title
+        contents: $contents
+      }
+    ) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function BoardNew() {
   // 작성자인풋, 작성자인풋에러
   const [name, setName] = React.useState("");
   const [nameError, setNameError] = React.useState("");
@@ -79,7 +113,21 @@ function BoardNew() {
     }
   };
 
-  const onClickSignup = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const [나의함수] = useMutation(CREATE_BOARD);
+  // const onClickSubmit = async () => {
+  //   // 그래프ql 요청함
+  //   const result = await 나의함수({
+  //     variables: {
+  //       writer: name,
+  //       password: password,
+  //       title: title,
+  //       contents: content,
+  //     },
+  //   });
+  //   console.log(result);
+  // };
+
+  const onClickSignup = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
 
     console.log("작성자 이름은:", name);
@@ -127,141 +175,183 @@ function BoardNew() {
     // 제출 전 모든 부분이 만족해서 true인지 확인하고 alert을 띄울지 정하는 곳
     if (isValid) {
       alert("회원가입 완료됨");
+
+      const result = await 나의함수({
+        variables: {
+          writer: name,
+          password: password,
+          title: title,
+          contents: content,
+        },
+      });
+      console.log(result);
     }
   };
 
   return (
     <>
-      <main className="게시물등록섹션">
-        <Link to="/BoardsDetail">상세 페이지 이동</Link>
-        <section className="게시물등록">
+      <main className={styles.게시물등록섹션}>
+        <Link href="/boards/detail">상세 페이지 이동</Link>
+        {/* <button onClick={onClickSubmit}>그래프큐엘요청하는버튼</button> */}
+        <section className={styles.게시물등록}>
           <h1>게시물 등록</h1>
         </section>
 
         <form>
-          <fieldset className="이름과비번입력하는곳">
+          <fieldset className={styles.이름과비번입력하는곳}>
             <legend>사용자 정보</legend>
 
-            <div className="반쪽인풋섹션">
-              <label className="인풋이름">
-                작성자 <span className="빨간별">*</span>
+            <div className={styles.반쪽인풋섹션}>
+              <label className={styles.인풋이름}>
+                작성자 <span className={styles.빨간별}>*</span>
               </label>
               <input
-                className="반쪽인풋"
+                className={styles.반쪽인풋}
                 type="text"
                 placeholder="작성자 명을 입력해 주세요."
                 onChange={onChangeName}
               />
-              <span id="작성자경고" className="경고글">
+              <span id="작성자경고" className={styles.경고글}>
                 {nameError}
               </span>
             </div>
 
-            <div className="반쪽인풋섹션">
-              <label className="인풋이름">
-                비밀번호 <span className="빨간별">*</span>
+            <div className={styles.반쪽인풋섹션}>
+              <label className={styles.인풋이름}>
+                비밀번호 <span className={styles.빨간별}>*</span>
               </label>
               <input
-                className="반쪽인풋"
+                className={styles.반쪽인풋}
                 type="password"
                 placeholder="비밀번호를 입력해주세요."
                 onChange={onChangePassword}
               />
-              <span id="비밀번호경고" className="경고글">
+              <span id="비밀번호경고" className={styles.경고글}>
                 {passwordError}
               </span>
             </div>
           </fieldset>
 
-          <hr className="실선" />
+          <hr className={styles.실선} />
 
-          <fieldset className="제목입력하는곳">
+          <fieldset className={styles.제목입력하는곳}>
             <legend>게시물 제목</legend>
-            <label className="인풋이름">
-              제목 <span className="빨간별">*</span>
+            <label className={styles.인풋이름}>
+              제목 <span className={styles.빨간별}>*</span>
             </label>
             <input
-              className="풀인풋"
+              className={styles.풀인풋}
               type="text"
               placeholder="제목을 입력해 주세요."
               onChange={onChangeTitle}
             />
-            <span id="제목경고" className="경고글">
+            <span id="제목경고" className={styles.경고글}>
               {titleError}
             </span>
           </fieldset>
 
-          <hr className="실선" />
+          <hr className={styles.실선} />
 
-          <fieldset className="내용입력하는곳">
+          <fieldset className={styles.내용입력하는곳}>
             <legend>게시물 내용</legend>
-            <label className="인풋이름">
-              내용 <span className="빨간별">*</span>
+            <label className={styles.인풋이름}>
+              내용 <span className={styles.빨간별}>*</span>
             </label>
             <textarea
-              className="많이큰인풋"
+              className={styles.많이큰인풋}
               placeholder="내용을 입력해 주세요."
               onChange={onChangeContent}
             />
-            <span id="내용경고" className="경고글">
+            <span id="내용경고" className={styles.경고글}>
               {contentError}
             </span>
           </fieldset>
 
-          <hr className="실선" />
+          <hr className={styles.실선} />
 
-          <fieldset className="주소입력하는곳">
+          <fieldset className={styles.주소입력하는곳}>
             <legend>주소 정보</legend>
-            <label className="인풋이름">주소</label>
-            <div className="우편번호입력하는곳">
-              <input className="우편번호인풋" type="text" placeholder="01234" />
-              <button className="우편버튼">우편번호 검색</button>
+            <label className={styles.인풋이름}>주소</label>
+            <div className={styles.우편번호입력하는곳}>
+              <input
+                className={styles.우편번호인풋}
+                type="text"
+                placeholder="01234"
+              />
+              <button className={styles.우편버튼}>우편번호 검색</button>
             </div>
 
             <input
-              className="풀인풋"
+              className={styles.풀인풋}
               type="text"
               placeholder="주소를 입력해 주세요."
             />
-            <input className="풀인풋" type="text" placeholder="상세주소" />
+            <input
+              className={styles.풀인풋}
+              type="text"
+              placeholder="상세주소"
+            />
           </fieldset>
 
-          <hr className="실선" />
+          <hr className={styles.실선} />
 
-          <fieldset className="링크입력하는곳">
+          <fieldset className={styles.링크입력하는곳}>
             <legend>유튜브 링크</legend>
-            <label className="인풋이름">유튜브 링크</label>
+            <label className={styles.인풋이름}>유튜브 링크</label>
             <input
-              className="풀인풋"
+              className={styles.풀인풋}
               type="text"
               placeholder="링크를 입력해 주세요."
             />
           </fieldset>
 
-          <hr className="실선" />
+          <hr className={styles.실선} />
 
-          <fieldset className="사진첨부하는곳">
+          <fieldset className={styles.사진첨부하는곳}>
             <legend>사진 첨부</legend>
-            <div className="업로드박스그룹">
-              <div className="업로드박스">
-                <img src="./icon/add.png" alt="추가" />
+            <div className={styles.업로드박스그룹}>
+              <div className={styles.업로드박스}>
+                <Image
+                  src="/images/add.png"
+                  alt="추가"
+                  className={styles.addIcon}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                />
                 <div>클릭해서 사진 업로드</div>
               </div>
-              <div className="업로드박스">
-                <img src="./icon/add.png" alt="추가" />
+              <div className={styles.업로드박스}>
+                <Image
+                  src="/images/add.png"
+                  alt="추가"
+                  className={styles.addIcon}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                />
                 <div>클릭해서 사진 업로드</div>
               </div>
-              <div className="업로드박스">
-                <img src="./icon/add.png" alt="추가" />
+              <div className={styles.업로드박스}>
+                <Image
+                  src="/images/add.png"
+                  alt="추가"
+                  className={styles.addIcon}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                />
                 <div>클릭해서 사진 업로드</div>
               </div>
             </div>
           </fieldset>
 
-          <div className="버튼있는곳">
-            <button className="취소버튼">취소</button>
+          <div className={styles.버튼있는곳}>
+            <button className={styles.취소버튼}>취소</button>
             <button
-              className={`등록하기버튼 ${isActive ? "active" : ""}`}
+              className={`${styles.등록하기버튼} ${
+                isActive ? styles.active : ""
+              }`}
               onClick={onClickSignup}
             >
               등록하기
@@ -272,5 +362,3 @@ function BoardNew() {
     </>
   );
 }
-
-export default BoardNew;
