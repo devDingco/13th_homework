0f49bfe21c30b26b7671c44a617a24585db4afa5
jomira0220 <whalfk220@gmail.com>
2024-09-30 22:28:41 +0900

    이미지 링크 화 관련 작업중 및 edit용 input속성 값 추가

--- next_과제/src/components/input.tsx
+++ next_과제/src/components/input.tsx
@@ -1,5 +1,5 @@
 import Icon from "@/components/iconFactory";
-import { forwardRef } from "react";
+import { ChangeEvent, forwardRef } from "react";
 
 type InputProps = {
   id?: string;
@@ -10,13 +10,26 @@ type InputProps = {
   required?: boolean;
   uploadcount?: number;
   placeholder?: string;
-  value?: string;
+  defaultValue?: string;
+  readOnly?: boolean;
 };
 
 type inputRef = HTMLInputElement;
 
 const Input = forwardRef<inputRef, InputProps>((props, ref) => {
   const { title, errormessage, ...rest } = props;
+
+  const showImage = (e: ChangeEvent<HTMLInputElement>) => {
+    // const targetLable = e.target.parentNode as HTMLLabelElement;
+    const targetFile = e.target.files as FileList;
+    const targetFileArray = Array.from(targetFile);
+    const selectedFiles: string[] = targetFileArray.map((file) => {
+      return URL.createObjectURL(file);
+    });
+    console.log(selectedFiles[0]);
+    //targetLable.setAttribute("style", `url(${selectedFiles[0]})`);
+  };
+
   if (props.type === "file" && props.accept === "image/*") {
     return (
       <div className="flex flex-col gap-2">
@@ -25,14 +38,19 @@ const Input = forwardRef<inputRef, InputProps>((props, ref) => {
           {props.required && <span className="text-red-500">*</span>}
         </div>
         <div className={`flex gap-4 ${props.required ? "validStyle" : ""}`}>
-          {props.uploadcount &&
-            Array.from({ length: props.uploadcount }).map((_, index) => (
-              <label key={index} className="imgUploadBox">
-                <input ref={ref} className="blind imgFile" {...rest} />
-                <Icon icon="add" className="fill-gray-500 w-10" />
-                클릭해서 사진 업로드
-              </label>
-            ))}
+          <label className="imgUploadBox" htmlFor={props.id}>
+            <input
+              ref={ref}
+              id={props.id}
+              className="blind imgFile"
+              {...rest}
+              onChange={(e) => showImage(e)}
+            />
+            <span className="w-10 h-10">
+              <Icon icon="add" className="fill-gray-500 w-10" />
+            </span>
+            클릭해서 사진 업로드
+          </label>
         </div>
         {errormessage && <p className="toolTip">{errormessage}</p>}
       </div>
@@ -47,7 +65,9 @@ const Input = forwardRef<inputRef, InputProps>((props, ref) => {
       </div>
       <input
         ref={ref}
-        className="border border-gray-300 rounded-lg w-full py-3 px-4"
+        className={`border border-gray-300 rounded-lg w-full py-3 px-4 ${
+          props.readOnly ? "bg-gray-100" : ""
+        }`}
         {...rest}
       />
       {errormessage && <p className="toolTip">{errormessage}</p>}
