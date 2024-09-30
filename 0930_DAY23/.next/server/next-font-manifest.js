8a47ff6f652591e85deb886a29c89da793199ee3
suef606 <sufe3005@gmail.com>
2024-09-30 22:19:52 +0900

    [240930] 과제 제출

---
+++ 0930_DAY23/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/su/Desktop/13th_homework/0930_DAY23/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
