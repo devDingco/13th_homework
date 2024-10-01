3cc193b7d707c4978b78bfe9c257762595c4a6ae
jomira0220 <whalfk220@gmail.com>
2024-10-01 21:15:43 +0900

    기본 테마 색상 수정

--- next_과제/tailwind.config.ts
+++ next_과제/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import daisyui from "daisyui";
+import { inherits } from "util";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,20 +13,54 @@ const config: Config = {
         roboto: ["var(--roboto)"],
         notosanskr: ["var(--noto-sans-kr)"],
       },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#2974E5",
+          "base-100": "#fff",
+          "--base-100": "#fff",
+          "accent-content": "#000",
+          "--accent-content": "#000",
+          ".btn-accent-content": {
+            backgroundColor: "var(--accent-content)",
+            color: "var(--base-100)",
+            "&:hover": {
+              backgroundColor:
+                "color-mix(in oklab,oklch(var(--btn-color, var(--b2)) / var(--tw-border-opacity, 1)) 40%, black)",
+            },
+          },
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#2974E5",
+          "base-100": "#000",
+          "--base-100": "#000",
+          "accent-content": "#fff",
+          "--accent-content": "#fff",
+          ".btn-accent-content": {
+            backgroundColor: "var(--accent-content)",
+            color: "var(--base-100)",
+            "&:hover": {
+              backgroundColor:
+                "color-mix(in oklab,oklch(var(--btn-color, var(--b2)) / var(--tw-border-opacity, 1)) 40%, white)",
+            },
+          },
+        },
+      },
+    ],
     base: true,
     styled: true,
     utils: true,
     prefix: "",
     themeRoot: ":root",
   },
+  darkMode: ["class", '[data-theme="dark"]'],
 };
 export default config;
