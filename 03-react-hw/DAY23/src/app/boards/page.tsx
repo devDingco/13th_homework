931adbc079481cc52ecb8943cbc2e9844525dbcc
Shin-E2 <sinhyejee322@gmail.com>
2024-10-01 17:04:23 +0900

    [241001] DAY24 필수과제

--- 03-react-hw/DAY23/src/app/boards/page.tsx
+++ 03-react-hw/DAY23/src/app/boards/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import styles from "./page.module.css";
 import BoardHeader from "../components/BoardHeader";
 import BoardItem from "../components/BoardItem";
 
-// Board
 interface Board {
   _id: string;
   writer: string;
@@ -14,7 +13,6 @@ interface Board {
   createdAt: string;
 }
 
-// GraphQL 타입
 interface FetchBoardsResult {
   fetchBoards: Board[];
 }
@@ -31,12 +29,35 @@ const FETCH_BOARDS = gql`
   }
 `;
 
+const DELETE_BOARD = gql`
+  mutation deleteBoard($boardId: ID!) {
+    deleteBoard(boardId: $boardId)
+  }
+`;
+
 export default function Boardspage() {
-  const { data } = useQuery<FetchBoardsResult>(FETCH_BOARDS, {
+  const { data, refetch } = useQuery<FetchBoardsResult>(FETCH_BOARDS, {
     variables: {
       page: 10,
     },
   });
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  const onClickDelete = async (id: string) => {
+    try {
+      await deleteBoard({
+        variables: {
+          boardId: id,
+        },
+      });
+      alert("게시글이 삭제되었습니다.");
+      refetch();
+    } catch (error) {
+      alert("게시글 삭제에 실패했습니다.");
+      console.log(`게시글 삭제 오류: ${error}`);
+    }
+  };
 
   return (
     <div>
@@ -45,7 +66,12 @@ export default function Boardspage() {
           <BoardHeader />
           <div className={styles.등록된게시글상자}>
             {data?.fetchBoards.map((board, index) => (
-              <BoardItem key={board._id} board={board} index={index} />
+              <BoardItem
+                key={board._id}
+                board={board}
+                index={index}
+                onDelete={onClickDelete}
+              />
             ))}
           </div>
         </div>
