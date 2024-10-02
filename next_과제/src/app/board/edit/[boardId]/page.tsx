767e0ad7c2ee002cb5d5ccc80190f67378ff686f
jomira0220 <whalfk220@gmail.com>
2024-10-02 22:12:12 +0900

    폴더 정리(페이지 구조 정리)

--- next_과제/src/app/board/edit/[boardId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import BoardForm from "@/components/boardForm";
-
-export default function BoardEdit() {
-  return <BoardForm title="게시글 수정" formType="edit" />;
-}
--- next_과제/src/app/board/write/page.tsx
+++
@@ -1,5 +0,0 @@
-import BoardForm from "@/components/boardForm";
-
-export default function BoardWrite() {
-  return <BoardForm title="게시글 등록" formType="write" />;
-}
