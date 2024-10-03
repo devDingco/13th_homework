8baaa9a99a958417f47cf84e7679e140d1588e24
지승 <rjs8833@gmail.com>
2024-10-03 23:51:53 +0900

    [241003] add modal open body overflow hidden

--- client/src/app/(board)/boards/@modal/page.tsx
+++ client/src/app/(board)/boards/@modal/page.tsx
@@ -1,8 +1,14 @@
 /** @format */
+'use client';
 
 import { IModalProps } from '@/models/children.type';
 import ModalContainer from '../_components/ModalContainer';
 
 export default function ModalPage({ searchParams }: IModalProps) {
+	if (searchParams.boardId) {
+		document.body.classList.add('overflow-hidden');
+	} else {
+		document.body.classList.remove('overflow-hidden');
+	}
 	return searchParams.boardId && <ModalContainer boardId={searchParams.boardId} />;
 }
--- client/src/app/(board)/boards/[boardId]/edit/page.tsx
+++ client/src/app/(board)/boards/[boardId]/edit/page.tsx
@@ -1,5 +1,12 @@
 /** @format */
 
+import NewForm from '../../new/_components/NewForm';
+
 export default function editPage() {
-	return 1;
+	return (
+		<>
+			<div className="prose-b_20_28">게시물 수정</div>
+			<NewForm />
+		</>
+	);
 }
