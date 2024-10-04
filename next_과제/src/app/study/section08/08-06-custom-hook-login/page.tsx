36d66ec3d881f5f4ddf36c0837af30416b714c97
jomira0220 <whalfk220@gmail.com>
2024-10-04 15:35:47 +0900

    스터디용 파일 분리에 따른 삭제 처리

--- next_과제/src/app/study/section08/08-06-custom-hook-login/page.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-export default function CustomHookLoginPage() {
-  return <div>로그인 페이지 입니다. 로그인 해주세요.</div>;
-}
--- next_과제/src/app/study/section09/09-02-board-component-new/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import BoardForm from "@/components/studyComponent/boardForm";
-
-export default function BoardNewPage() {
-  return <BoardForm isEdit={false} />;
-}
