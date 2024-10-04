addc71f5bdba93b6514b4c3e10ecf71d348b6560
지승 <rjs8833@gmail.com>
2024-10-04 14:45:54 +0900

    client change textarea defaultValue

--- client/src/app/(board)/boards/new/_components/NewTextarea.tsx
+++ client/src/app/(board)/boards/new/_components/NewTextarea.tsx
@@ -1,8 +1,14 @@
 /** @format */
 
 import { ENewTitleProps, ITitle, RNewInputPlaceHolder } from '@/models/board.type';
+import { useParams, usePathname } from 'next/navigation';
 
-export default function NewTextarea({ title, error, edit }: ITitle) {
+import useSWR from 'swr';
+
+export default function NewTextarea({ title, error }: ITitle) {
+	const param = useParams();
+	const { data } = useSWR(`/board/${param.boardId}`, null);
+
 	return (
 		<div className="flex flex-1 flex-col gap-2">
 			<div className="flex gap-1">
@@ -14,7 +20,7 @@ export default function NewTextarea({ title, error, edit }: ITitle) {
 				name={title}
 				className="h-[336px] w-full rounded-lg border-[1px] border-gray-200 px-3 py-4 outline-none placeholder:prose-r_16_24 placeholder:text-gray-400"
 				placeholder={RNewInputPlaceHolder[title]}
-				defaultValue={edit as string}
+				defaultValue={data?.title}
 			></textarea>
 			<div className="prose-me_16_24 text-red-400">{error}</div>
 		</div>
