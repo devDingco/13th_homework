7d96b346bd55d027f8bf3b8e820bf1e015289b26
jomira0220 <whalfk220@gmail.com>
2024-10-04 15:37:21 +0900

    불필요한 임폴트 삭제

--- next_과제/tailwind.config.ts
+++ next_과제/tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import daisyui from "daisyui";
-import { inherits } from "util";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
