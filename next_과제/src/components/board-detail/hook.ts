336b13070f1b065c304ae237cff33bd3ae063f33
jomira0220 <whalfk220@gmail.com>
2024-10-07 08:50:59 +0900

    graphql 연결 수정

--- next_과제/src/components/board-detail/hook.ts
+++ next_과제/src/components/board-detail/hook.ts
@@ -1,17 +1,17 @@
 import { useQuery } from "@apollo/client";
 import { useParams } from "next/navigation";
 // import { DETAIL_FETCH_BOARD } from "@/components/board-detail/queries";
-import { FetchBoardsListDocument } from "@/commons/graphql/graphql";
+import { FetchBoardDocument } from "@/commons/graphql/graphql";
 
 export const useBoardDetail = () => {
   const params = useParams();
   // 주소에서 값을 가져온 params.id는 문자이므로 Number로 변환해주고
   // FETCH_BOARD 쿼리에 넣어준다.
-  const { data } = useQuery(FetchBoardsListDocument, {
+  const { data } = useQuery(FetchBoardDocument, {
     variables: { boardId: params.boardId },
   });
   // console.log(params.boardId, data?.fetchBoard);
-  const detailData = data?.fetchBoards;
+  const detailData = data?.fetchBoard;
 
   return { detailData, params };
 };
