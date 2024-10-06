d6bc414c095f0ae891748f60ed9506def91204de
yheseul <kangyheseul@naver.com>
2024-10-06 14:19:25 +0900

    refactor: move Apollo Client queries to the graphql directory

--- trip-talk/src/commons/graphql/backend-api.tsx
+++ trip-talk/src/commons/graphql/backend-api.tsx
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const FETCH_BOARDS = gql`
   query {
-    fetchBoards(page: 1) {
+    fetchBoards {
       _id
       writer
       title
@@ -14,5 +14,73 @@ export const FETCH_BOARDS = gql`
 export const DELETE_BOARD = gql`
   mutation deleteBoard($id: ID!) {
     deleteBoard(boardId: $id)
+  }
+`;
+
+export const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+      createdAt
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+    }
+  }
+`;
+
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      writer
+      title
+      contents
+      createdAt
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+    }
+  }
+`;
+
+export const UPDATE_BOARD = gql`
+  mutation (
+    $updateBoardInput: UpdateBoardInput!
+    $password: String
+    $boardId: ID!
+  ) {
+    updateBoard(
+      updateBoardInput: $updateBoardInput
+      password: $password
+      boardId: $boardId
+    ) {
+      _id
+      title
+      contents
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+    }
   }
 `;
