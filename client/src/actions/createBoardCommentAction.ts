3c985c4f747fff730642d78827964bc8db9ab444
지승 <rjs8833@gmail.com>
2024-10-08 08:14:11 +0900

    [241008] client complete useActionState state error:

--- client/src/actions/createBoardCommentAction.ts
+++ client/src/actions/createBoardCommentAction.ts
@@ -7,6 +7,7 @@ export default async function createBoardCommentAction(
 	prevState: { boardId: number },
 	formData: { get: (arg0: string) => string | null },
 ) {
+	const boardId: number = prevState.boardId;
 	const author: string | null = formData.get('Author');
 	const password: string | null = formData.get('Password');
 	const content: string | null = formData.get('Comment');
@@ -14,20 +15,25 @@ export default async function createBoardCommentAction(
 
 	const rating: number | null = Number(stringRating);
 
-	if (!author || !password || !content || !rating) return { error: '댓글을 정확히 입력해주세요' };
+	if (!author || !password || !content || !rating)
+		return {
+			message: 'fail',
+			boardId: boardId,
+			error: '댓글을 정확히 입력해주세요',
+		};
 
-	const result = await postComment({ author, password, content, rating }, prevState.boardId);
+	const result = await postComment({ author, password, content, rating }, boardId);
 	// 업데이트 client cache & 재 생성할 때 comment가 정상적으로 생성되지 않앗음
 	if (typeof result === 'string') {
 		return {
 			message: 'fail',
-			id: prevState.boardId,
+			boardId: boardId,
 			error: result,
 		};
 	} else if (typeof result === 'object') {
 		return {
 			message: 'success',
-			id: prevState.boardId,
+			boardId: boardId,
 			error: undefined,
 		};
 	}
--- client/src/app/(board)/boards/[boardId]/_components/BoardCommentForm.tsx
+++ client/src/app/(board)/boards/[boardId]/_components/BoardCommentForm.tsx
@@ -33,9 +33,7 @@ export default function BoardCommentForm() {
 
 	useEffect(() => {
 		const reMutation = async () => {
-			console.log(1);
 			await mutate();
-			console.log(2);
 		};
 		if (state?.message === 'success') {
 			formRef.current?.reset();
