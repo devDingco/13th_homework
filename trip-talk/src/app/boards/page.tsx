d62826a16cc8d86310d09ab8b4391004ffd30dc5
yheseul <kangyheseul@naver.com>
2024-10-08 18:27:54 +0900

    chore: rename map element from 'e' to 'board'

--- trip-talk/src/app/boards/page.tsx
+++ trip-talk/src/app/boards/page.tsx
@@ -7,29 +7,26 @@ import { FetchBoardsDocument } from "../../commons/graphql/graphql";
 
 export default function Boards() {
   const { data } = useQuery(FetchBoardsDocument);
-
   return (
-    <>
-      <div className={styles.post_contain}>
-        <div className={styles.boards_header}>
-          <div className={styles.boards_header_number}>번호</div>
-          <div className={styles.boards_header_title}>제목</div>
-          <div className={styles.boards_header_writer}>작성자</div>
-          <div className={styles.boards_header_createdAt}>날짜</div>
-        </div>
-        <ul className={styles.posts}>
-          {data?.fetchBoards.map((e, index: number) => (
-            <BoardList
-              key={e._id}
-              _id={e._id}
-              number={index + 1}
-              title={e.title}
-              writer={String(e.writer)}
-              createdAt={e.createdAt}
-            />
-          ))}
-        </ul>
+    <div className={styles.post_contain}>
+      <div className={styles.boards_header}>
+        <div className={styles.boards_header_number}>번호</div>
+        <div className={styles.boards_header_title}>제목</div>
+        <div className={styles.boards_header_writer}>작성자</div>
+        <div className={styles.boards_header_createdAt}>날짜</div>
       </div>
-    </>
+      <ul className={styles.posts}>
+        {data?.fetchBoards.map((board, index: number) => (
+          <BoardList
+            key={board._id}
+            _id={board._id}
+            number={index + 1}
+            title={board.title}
+            writer={String(board.writer)}
+            createdAt={board.createdAt}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
