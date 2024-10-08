d410282465278035f66d5d242eec3bc5f27d975f
yheseul <kangyheseul@naver.com>
2024-10-08 18:46:50 +0900

    refactor: simplify returned from useBoardsList for better efficieny

--- trip-talk/src/app/components/BoardList/BoardList.tsx
+++ trip-talk/src/app/components/BoardList/BoardList.tsx
@@ -1,24 +1,22 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { IBoardList } from "../../../types/board.type";
-import UseBoardsList from "../../../commons/hooks/UseBoardList";
+import useBoardsList from "../../../commons/hooks/UseBoardList";
 
 export default function BoardList(props: IBoardList) {
-  const {onClickBoard, onCLickDelete, number, title, writer, createdAt} = UseBoardsList(props)
+  const { onClickBoard, onCLickDelete } = useBoardsList(props.id);
 
   return (
-    <>
-      <li className={styles.board_list} onClick={onClickBoard}>
-        <span className={styles.board_number}>{number}</span>
-        <span className={styles.board_title}>{title}</span>
-        <span className={styles.board_writer}>{writer}</span>
-        <span className={styles.board_createdAt}>
-          {createdAt.slice(0, 10).replaceAll("-", ".")}
-        </span>
-        <button className={styles.deleteButton} onClick={onCLickDelete}>
-          <Image src="/svgs/delete.svg" alt="delete" width={24} height={24} />
-        </button>
-      </li>
-    </>
+    <li className={styles.board_list} onClick={onClickBoard}>
+      <span className={styles.board_number}>{props.number}</span>
+      <span className={styles.board_title}>{props.title}</span>
+      <span className={styles.board_writer}>{props.writer}</span>
+      <span className={styles.board_createdAt}>
+        {props.createdAt.slice(0, 10).replaceAll("-", ".")}
+      </span>
+      <button className={styles.deleteButton} onClick={onCLickDelete}>
+        <Image src="/svgs/delete.svg" alt="delete" width={24} height={24} />
+      </button>
+    </li>
   );
 }
