bf636c207ac5e440719778ee8eaf9900d012e0d7
지승 <rjs8833@gmail.com>
2024-10-09 10:46:48 +0900

    [241009] client apply carousel

--- client/src/app/(board)/boards/_components/BoardCarousel.tsx
+++ client/src/app/(board)/boards/_components/BoardCarousel.tsx
@@ -1,27 +1,46 @@
 /** @format */
+'use client';
 
-import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/ui/carousel';
 
+import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
 
 export default function BoardCarousel() {
 	return (
-		<Carousel>
-			<CarouselContent>
-				{[...Array(3)].map((_, index) => {
-					return (
-						<CarouselItem key={index}>
-							<Image
-								src={`/Images/banner_${index}.png`}
-								alt={`banner_${index}`}
-								width={800}
-								height={400}
-								priority
-							/>
-						</CarouselItem>
-					);
-				})}
-			</CarouselContent>
-		</Carousel>
+		<div className="mx-auto">
+			<Carousel
+				className="w-full"
+				plugins={[
+					Autoplay({
+						delay: 4000,
+					}),
+				]}
+			>
+				<CarouselContent>
+					{[...Array(3)].map((_, index) => {
+						return (
+							<CarouselItem key={index}>
+								<Image
+									src={`/Images/banner_${index}.png`}
+									alt={`banner_${index}`}
+									width={800}
+									height={400}
+									priority
+								/>
+							</CarouselItem>
+						);
+					})}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</div>
 	);
 }
