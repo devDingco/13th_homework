b79f749fa3b3032f9a24986695d6f4f88c4be000
yheseul <kangyheseul@naver.com>
2024-10-09 18:08:11 +0900

    refactor: reduce props in Input component and update useage in CommentWrite page

--- trip-talk/src/app/components/CommentWrite/CommentWrite.tsx
+++ trip-talk/src/app/components/CommentWrite/CommentWrite.tsx
@@ -5,16 +5,11 @@ import styles from "./styles.module.css";
 import Input from "../Input/Input";
 import UseCommentWrite from "../../../commons/hooks/UseCommentWrite";
 import Button from "../Button/Button";
-import { BUTTON, INPUT_CHILDREN, PLACEHOLDERS } from "../../../enums/constants";
+import TextArea from "../TextArea/TextArea";
 
 export default function CommentWrite() {
-  const {
-    handleCommentSubmit,
-    handleInputChange,
-    handleContentChange,
-    disabledButton,
-    comment,
-  } = UseCommentWrite();
+  const { handleCommentSubmit, handleInputChange, disabledButton, comment } =
+    UseCommentWrite();
 
   return (
     <>
@@ -58,42 +53,29 @@ export default function CommentWrite() {
         <div className={styles.input_layout}>
           <div className={styles.input_wrapper}>
             <Input
-              isLabel={true}
-              children={INPUT_CHILDREN.WRITER}
               id="writer"
-              isRequired={true}
-              type="text"
-              placeholder={PLACEHOLDERS.WRITER}
               onChange={handleInputChange}
               value={comment.writer}
             />
             <Input
-              isLabel={true}
-              children={INPUT_CHILDREN.PASSWORD}
               id="password"
-              isRequired={true}
-              type="password"
-              placeholder={PLACEHOLDERS.PASSWORD}
               onChange={handleInputChange}
               value={comment.password}
             />
           </div>
           <div className={styles.textarea_wrapper}>
-            <textarea
-              id="comment"
-              placeholder={PLACEHOLDERS.COMMENT}
-              onChange={handleContentChange}
+            <TextArea
+              id="contents"
+              onChange={handleInputChange}
               value={comment.contents}
             />
           </div>
           <div className={styles.button_wrapper}>
             <Button
-              type="submit"
+              id="commit_submit"
               disabled={disabledButton}
               color={disabledButton ? "gray" : "blue"}
-            >
-              {BUTTON.COMMENT_SUBMIT}
-            </Button>
+            />
           </div>
         </div>
       </form>
--- trip-talk/src/app/components/Input/Input.tsx
+++ trip-talk/src/app/components/Input/Input.tsx
@@ -1,13 +1,9 @@
+import { INPUT_CHILDREN, PLACEHOLDERS } from "../../../enums/constants";
 import { IInputProps } from "../../../types/components.type";
 import styles from "./styles.module.css";
 
 export default function Input({
-  isLabel,
-  children,
-  isRequired,
   id,
-  type,
-  placeholder,
   onChange,
   defaultValue,
   disabled,
@@ -16,18 +12,20 @@ export default function Input({
   return (
     <>
       <div className={styles.input_wrapper}>
-        {isLabel && (
+        {id !== "address" && id !== "detail_address" && (
           <div className={styles.label_wrapper}>
-            <label className={styles.label}>{children}</label>
-            {isRequired && <b className={`${styles.required_marker}`}>*</b>}
+            <label className={styles.label}>{INPUT_CHILDREN[id]}</label>
+            {id !== "zip_code" && id !== "url" && (
+              <b className={`${styles.required_marker}`}>*</b>
+            )}
           </div>
         )}
 
         <input
           className={styles.input}
           id={id}
-          type={type}
-          placeholder={placeholder}
+          type={id === "password" ? "password" : "text"}
+          placeholder={PLACEHOLDERS[id]}
           onChange={onChange}
           defaultValue={defaultValue}
           disabled={disabled}
