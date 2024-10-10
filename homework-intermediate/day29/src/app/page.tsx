05da681b66e8d8d73a7770192a2180c12b549879
jintae1954 <qkrwlsxo1954@gmail.com>
2024-10-10 14:29:04 +0900

    chore: add day29 directory

---
+++ homework-intermediate/day29/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <>여기는 루트 페이지 입니다.</>;
+}
---
+++ homework-intermediate/day29/src/components/boards-detail/detail/hooks.ts
@@ -0,0 +1,15 @@
+import { useQuery } from '@apollo/client';
+import { FetchBoardDocument } from '@/commons/graphql/graphql';
+import { useParams } from 'next/navigation';
+
+export default function useBoardsDeatil() {
+  const params = useParams();
+  console.log('detail 화면에서 boardId::::', params.boardId);
+
+  const { data } = useQuery(FetchBoardDocument, {
+    variables: { boardId: String(params.boardId) },
+  });
+  console.log('detail 화면에서 data:::', data);
+
+  return { boardId: String(params.boardId), data };
+}
