88e40c0152031b52b8cfe17043d5348d7d2dec59
jomira0220 <whalfk220@gmail.com>
2024-10-10 09:10:13 +0900

    기존 datepicker 삭제 및 antd datepicker로 수정 작업

--- next_과제/src/components/searchBox/index.tsx
+++ next_과제/src/components/searchBox/index.tsx
@@ -3,14 +3,11 @@
 import Icon from "@/components/iconFactory";
 import { useParams } from "next/navigation";
 import { gql, useQuery } from "@apollo/client";
-
 import styles from "./index.module.scss";
 
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { ko } from "date-fns/locale";
-
 import { useState } from "react";
+
+import { DatePicker, Input, Button } from "antd";
 
 const SEARCH_BOARD = gql`
   query fetchBoards(
@@ -36,19 +33,21 @@ const SEARCH_BOARD = gql`
 export default function SearchBox() {
   const params = useParams();
 
-  const [startDate, setStartDate] = useState<Date>();
-  const [endDate, setEndDate] = useState<Date>();
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
   const [search, setSearch] = useState("");
+  const { RangePicker } = DatePicker;
 
-  const toKoreanTimeString = (date?: Date, isEndDate = false) => {
+  const toKoreanTimeString = (date?: string, isEndDate = false) => {
     if (!date) return undefined;
+    const dateSet = new Date(date);
     const offset = 9 * 60; // Korea is UTC+9
-    const koreanTime = new Date(date.getTime() + offset * 60 * 1000);
+    const koreanTime = new Date(dateSet.getTime() + offset * 60 * 1000);
     if (isEndDate) {
       // 종료일은 24시로 설정
       koreanTime.setHours(24, 0, 0, 0);
     }
-    return koreanTime.toISOString().substring(0, 19);
+    return koreanTime.toISOString();
   };
 
   const { refetch } = useQuery(SEARCH_BOARD, {
@@ -73,22 +72,33 @@ export default function SearchBox() {
   return (
     <div className="flex gap-4 flex-wrap max-sm:w-full">
       <label className="input input-bordered flex flex-wrap items-center gap-2 w-[682px] max-sm:w-full">
-        <span className="w-6 h-6 inline-block">
-          <Icon icon="search" />
-        </span>
-        <input
+        <Input
+          size="large"
           type="text"
-          className="grow"
           placeholder="제목을 검색해 주세요"
           defaultValue={search}
           onChange={(e) => setSearch(e.target.value)}
+          prefix={
+            <Icon icon="search" className="w-6 h-6 fill-accent-content" />
+          }
         />
       </label>
       <div className="flex gap-4 flex-wrap max-sm:w-full">
-        <label className="flex items-center border rounded-lg px-3 overflow-hidden">
+        <RangePicker
+          size="large"
+          id={{
+            start: "startDate",
+            end: "endDate",
+          }}
+          onChange={(date, dateString) => {
+            setStartDate(new Date(dateString[0]).toISOString());
+            setEndDate(new Date(dateString[1]).toISOString());
+          }}
+        />
+        {/* <label className="flex items-center border rounded-lg px-3 overflow-hidden">
           <div className={`${styles.datePicker}`}>
-            <Icon icon="calendar" className="w-6 h-6 fill-accent-content" />
-            <DatePicker
+            <Icon icon="calendar" className="w-6 h-6 fill-accent-content" /> */}
+        {/* <DatePicker
               selected={startDate}
               locale={ko}
               onChange={(date) => setStartDate(date || new Date())}
@@ -111,15 +121,16 @@ export default function SearchBox() {
               dateFormat={"yyyy.MM.dd"}
               placeholderText="YYYY.MM.DD"
               closeOnScroll={true}
-            />
-          </div>
-        </label>
-        <button
+            /> */}
+        {/* </div>
+        </label> */}
+        <Button
+          size="large"
           className="btn btn-accent-content max-sm:w-full"
           onClick={() => searchBoard()}
         >
           검색
-        </button>
+        </Button>
       </div>
     </div>
   );
