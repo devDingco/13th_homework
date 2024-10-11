88de2e52d12b4abca267b0238d1a33a58a04b60e
지승 <rjs8833@gmail.com>
2024-10-11 14:32:54 +0900

    [241011] client commit init

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,17 +1,20 @@
 /** @format */
-'use client';
+// 'use client';
 
 import './globals.css';
 
-import { ApolloProvider } from '@apollo/client';
+// import { ApolloProvider } from '@apollo/client';
 import { IProps } from '@/models/children.type';
-import client from '@/libs/apollo';
+import RootContainer from '@/components/common/RootContainer';
+
+// import client from '@/libs/apollo';
 
 export default function RootLayout({ children }: IProps) {
 	return (
 		<html lang="ko">
-			<body className="m-0 box-border flex items-center justify-center p-0">
-				<ApolloProvider client={client}>{children}</ApolloProvider>
+			<body className="m-0 box-border p-0">
+				{/* <ApolloProvider client={client}>{children}</ApolloProvider> */}
+				<RootContainer>{children}</RootContainer>
 			</body>
 		</html>
 	);
---
+++ client/src/components/common/RootHeader.tsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function RootHeader() {
+	return 1;
+}
