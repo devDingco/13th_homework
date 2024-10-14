c1cf014bd7753062fb95f6f891137f3c93c91b63
jomira0220 <whalfk220@gmail.com>
2024-10-14 09:06:23 +0900

    로딩 확인 메서드 추가

--- next_과제/src/components/board-detail/detail/hook.ts
+++ next_과제/src/components/board-detail/detail/hook.ts
@@ -7,10 +7,10 @@ export const useBoardDetail = () => {
   const params = useParams();
   // 주소에서 값을 가져온 params.id는 문자이므로 Number로 변환해주고
   // FETCH_BOARD 쿼리에 넣어준다.
-  const { data, error } = useQuery(FetchBoardDocument, {
+  const { data, error, loading } = useQuery(FetchBoardDocument, {
     variables: { boardId: String(params.boardId) },
   });
   const detailData = data?.fetchBoard;
 
-  return { detailData, params, error };
+  return { detailData, params, error, loading };
 };
