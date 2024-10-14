4ebb5e5b9e2b77afc5bf1e08d8e2997d242a2475
김세연 <kimsaeyeon@gimseyeon-ui-MacBookAir.local>
2024-10-14 19:24:44 +0900

    layout,banner,navigation 구성 완료

--- cna-homework/src/app/boards/page.tsx
+++ cna-homework/src/app/boards/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import BoardList from "@/components/boards-list/list";
-import SwiperComponent from "@/components/boards-list/banner";
 
 const Boards = () => {
   // graphql
 
   return (
     <>
-      <SwiperComponent />
       <BoardList />
     </>
   );
