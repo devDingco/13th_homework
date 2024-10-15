c9b19d83cdd696b06c11c43812bdc16c4aba51c2
jomira0220 <whalfk220@gmail.com>
2024-10-15 20:51:46 +0900

    게시판용 레이아웃 추가에 따른 부모박스 클레스 삭제

--- next_과제/src/app/boards/page.tsx
+++ next_과제/src/app/boards/page.tsx
@@ -3,9 +3,9 @@ import BoardList from "@/components/board-list";
 
 export default function BoardsPage() {
   return (
-    <div className="mainContent">
+    <>
       <h3 className="text-2xl font-bold">게시글 리스트</h3>
       <BoardList />
-    </div>
+    </>
   );
 }
