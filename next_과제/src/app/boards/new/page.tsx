8473888a19b734391eeccce5ba52e48712d9482e
jomira0220 <whalfk220@gmail.com>
2024-10-16 09:08:40 +0900

    레이아웃 수정에 따른 수정 처리

--- next_과제/src/app/boards/new/page.tsx
+++ next_과제/src/app/boards/new/page.tsx
@@ -1,9 +1,5 @@
 import BoardWrite from "@/components/board-write";
 
 export default function BoardsNewPage() {
-  return (
-    <div className="mainContent">
-      <BoardWrite title="게시글 등록" formType="write" />
-    </div>
-  );
+  return <BoardWrite title="게시글 등록" formType="write" />;
 }
