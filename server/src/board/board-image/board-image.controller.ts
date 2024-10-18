10f9f83027a7cadf29b4424c9b35f86e8b179d69
지승 <rjs8833@gmail.com>
2024-10-18 09:06:59 +0900

    [241018] server change board Image resource

--- server/src/board/board-image/board-image.controller.ts
+++ server/src/board/board-image/board-image.controller.ts
@@ -1,45 +1,7 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Patch,
-    Param,
-    Delete,
-} from '@nestjs/common';
 import { BoardImageService } from './board-image.service';
-import { CreateBoardImageDto } from './dto/create-board-image.dto';
-import { UpdateBoardImageDto } from './dto/update-board-image.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('board-image')
 export class BoardImageController {
     constructor(private readonly boardImageService: BoardImageService) {}
-
-    @Post()
-    create(@Body() createBoardImageDto: CreateBoardImageDto) {
-        return this.boardImageService.create(createBoardImageDto);
-    }
-
-    @Get()
-    findAll() {
-        return this.boardImageService.findAll();
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.boardImageService.findOne(+id);
-    }
-
-    @Patch(':id')
-    update(
-        @Param('id') id: string,
-        @Body() updateBoardImageDto: UpdateBoardImageDto,
-    ) {
-        return this.boardImageService.update(+id, updateBoardImageDto);
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.boardImageService.remove(+id);
-    }
 }
--- server/src/board/board-image/dto/update-board-image.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateBoardImageDto } from './create-board-image.dto';
-import { PartialType } from '@nestjs/swagger';
-
-export class UpdateBoardImageDto extends PartialType(CreateBoardImageDto) {}
