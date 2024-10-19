11954876b3bd7b39fe88cbd702c21008be9eff46
jomira0220 <whalfk220@gmail.com>
2024-10-19 15:56:44 +0900

    수업내용 파일 업로드

---
+++ study/src/commons/lib/17-01-firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhf4R88xZLB3ujtj5PMLz0olnbebrMYr8",
+  authDomain: "codecamp-jomira.firebaseapp.com",
+  projectId: "codecamp-jomira",
+  storageBucket: "codecamp-jomira.appspot.com",
+  messagingSenderId: "792650395274",
+  appId: "1:792650395274:web:7234d4dcdfe2160e834773",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
