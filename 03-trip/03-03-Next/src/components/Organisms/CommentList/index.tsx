cc1f9894aa693c1820f5a2aea3d90d7f527be419
revy7289 <95402753+revy7289@users.noreply.github.com>
2024-10-21 02:43:44 +0900

    feat: comments infinite scrolling

--- 03-trip/03-03-Next/src/components/Organisms/CommentList/index.tsx
+++ 03-trip/03-03-Next/src/components/Organisms/CommentList/index.tsx
@@ -17,43 +17,80 @@ import {
 import { useQuery } from "@apollo/client";
 import { useParams } from "next/navigation";
 import { FetchBoardCommentsDocument } from "@/commons/graphql/graphql";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState } from "react";
 
 export default function CommentListUI() {
+    const [hasMore, setHasMore] = useState(true);
     const params = useParams();
 
-    const { data } = useQuery(FetchBoardCommentsDocument, {
+    const { data, fetchMore } = useQuery(FetchBoardCommentsDocument, {
         variables: { page: 1, boardId: String(params.boardId) },
     });
 
+    const onNext = () => {
+        if (data === undefined) return;
+
+        fetchMore({
+            variables: {
+                page:
+                    Math.ceil((data?.fetchBoardComments.length ?? 10) / 10) + 1,
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+                if (!fetchMoreResult.fetchBoardComments.length) {
+                    setHasMore(false);
+                    return;
+                }
+                return {
+                    fetchBoardComments: [
+                        ...prev.fetchBoardComments,
+                        ...fetchMoreResult.fetchBoardComments,
+                    ],
+                };
+            },
+        });
+    };
+
     return (
         <section style={comment_wrap}>
-            {data && data.fetchBoardComments.length > 0 ? (
-                data.fetchBoardComments.map((el) => (
-                    <div key={el._id}>
-                        <div style={comment_input}>
-                            <div style={comment_label}>
-                                <Image src={profile} alt="profile" />
-                                <div>{el.writer}</div>
-                                <Rate value={el.rating} allowHalf disabled />
-                            </div>
-                            <div style={comment_label}>
-                                <div style={comment_btn}>
-                                    <Image src={edit} alt="edit" />
+            <InfiniteScroll
+                next={onNext}
+                hasMore={hasMore}
+                loader={<div>...Loading...</div>}
+                dataLength={data?.fetchBoardComments.length ?? 0}
+            >
+                {data && data.fetchBoardComments.length > 0 ? (
+                    data.fetchBoardComments.map((el) => (
+                        <div key={el._id}>
+                            <div style={comment_input}>
+                                <div style={comment_label}>
+                                    <Image src={profile} alt="profile" />
+                                    <div>{el.writer}</div>
+                                    <Rate
+                                        value={el.rating}
+                                        allowHalf
+                                        disabled
+                                    />
                                 </div>
-                                <div style={comment_btn}>
-                                    <Image src={close} alt="close" />
+                                <div style={comment_label}>
+                                    <div style={comment_btn}>
+                                        <Image src={edit} alt="edit" />
+                                    </div>
+                                    <div style={comment_btn}>
+                                        <Image src={close} alt="close" />
+                                    </div>
                                 </div>
                             </div>
+
+                            <pre>{el.contents}</pre>
+
+                            <div>{el.createdAt.split("T")[0]}</div>
                         </div>
-
-                        <pre>{el.contents}</pre>
-
-                        <div>{el.createdAt.split("T")[0]}</div>
-                    </div>
-                ))
-            ) : (
-                <div>등록된 댓글이 없습니다.</div>
-            )}
+                    ))
+                ) : (
+                    <div>등록된 댓글이 없습니다.</div>
+                )}
+            </InfiniteScroll>
         </section>
     );
 }
