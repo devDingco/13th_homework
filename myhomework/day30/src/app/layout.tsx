f61795c6c911d04118510377ccf63019dbd90fc9
noey <yoneee909@gmail.com>
2024-10-21 18:59:45 +0900

    react-youtube 라이브러리 사용 / navigation 및 banner 구현 / carousel 색상 변경

---
+++ myhomework/day30/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import ApolloSetting from "@/commons/settings/apollo-setting";
+import LayoutComponent from "@/commons/layout";
+
+const pretendardM = localFont({
+  src: "./fonts/Pretendard-Medium.otf",
+  variable: "--pretendard-medium",
+  weight: "100 900",
+});
+const PretendardR = localFont({
+  src: "./fonts/Pretendard-Regular.otf",
+  variable: "--pretendard-regular",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Day30",
+  description: "메인캠프 과제",
+};
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: IProps) {
+  return (
+    <html lang="en">
+      <body className={`${pretendardM.variable} ${PretendardR.variable}`}>
+        <div>
+          <LayoutComponent />
+        </div>
+        <ApolloSetting>{children}</ApolloSetting>
+      </body>
+    </html>
+  );
+}
---
+++ myhomework/day30/src/components/boards-wirte/queries.ts
@@ -0,0 +1,50 @@
+"use client";
+import { gql } from "@apollo/client";
+
+export const createBoard = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+      youtubeUrl
+      likeCount
+      images
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      createdAt
+      updatedAt
+      deletedAt
+    }
+  }
+`;
+
+export const updateBoard = gql`
+  mutation updateBoard(
+    $boardId: ID!
+    $password: String
+    $updateBoardInput: UpdateBoardInput!
+  ) {
+    updateBoard(
+      boardId: $boardId
+      password: $password
+      updateBoardInput: $updateBoardInput
+    ) {
+      _id
+      writer
+      title
+      contents
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+      createdAt
+      updatedAt
+      deletedAt
+    }
+  }
+`;
