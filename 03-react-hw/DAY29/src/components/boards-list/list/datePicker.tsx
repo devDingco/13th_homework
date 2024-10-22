4bcbdff9cf764368758fe30e492ee34bf8743d61
Shin-E2 <sinhyejee322@gmail.com>
2024-10-22 22:55:46 +0900

    [241022] antd datepicker css 수정

--- 03-react-hw/DAY29/src/components/boards-list/list/datePicker.tsx
+++ 03-react-hw/DAY29/src/components/boards-list/list/datePicker.tsx
@@ -1,26 +1,46 @@
-import { DatePicker, Space } from "antd";
+import { DatePicker } from "antd";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
+import locale from "antd/locale/ko_KR"; // antd 한국어 locale 추가
+import { ConfigProvider } from "antd"; // ConfigProvider 추가
+
+import "./UserDatePicker.css";
+import Image from "next/image";
+
 dayjs.locale("ko");
 
-import customParseFormat from "dayjs/plugin/customParseFormat";
-
-dayjs.extend(customParseFormat);
-
 const { RangePicker } = DatePicker;
-
-const dateFormat = "YYYY/MM/DD";
+const dateFormat = "YYYY.MM.DD";
 
 export default function UserDatePicker() {
   return (
-    <Space direction="vertical" size={12}>
-      <RangePicker
-        defaultValue={[
-          dayjs("2015/01/01", dateFormat),
-          dayjs("2015/01/01", dateFormat),
-        ]}
-        format={dateFormat}
-      />
-    </Space>
+    <ConfigProvider
+      locale={locale}
+      theme={{
+        components: {
+          DatePicker: {
+            activeBg: "#f2f2f2",
+          },
+        },
+      }}
+    >
+      <div className="h-12 p-3 bg-[#f2f2f2] rounded-lg justify-start items-center gap-2 inline-flex">
+        <div className="w-6 h-6 relative">
+          <Image
+            src="/images/icons/calendar.svg"
+            alt="calenderIcon"
+            width={24}
+            height={24}
+          />
+        </div>
+        <RangePicker
+          className="custom-range-picker"
+          format={dateFormat}
+          placeholder={["YYYY.MM.DD", "YYYY.MM.DD"]}
+          separator="-"
+          suffixIcon={null}
+        />
+      </div>
+    </ConfigProvider>
   );
 }
