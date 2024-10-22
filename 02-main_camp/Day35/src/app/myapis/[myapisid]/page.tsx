eceb451f903747bc0799bb1816b706c980cc313d
h0ngsik <hongsik.choi.dev@gmail.com>
2024-10-22 17:22:43 +0900

    [241022] feat: - myapis-detail 페이지 구현 - myapis-detail 페이지에서 상세보기 할 데이터 불러오기 및 적용 구현

--- 02-main_camp/Day35/src/app/myapis/[myapisid]/page.tsx
+++ 02-main_camp/Day35/src/app/myapis/[myapisid]/page.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+"use client";
+
+import FirebaseAPI, { CollectionList } from "@/commons/apis/firebase";
+import { DocumentData } from "firebase/firestore";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function MyPlanDetailPage() {
+  const params = useParams();
+  const [plan, setPlan] = useState<DocumentData>({});
+
+  const { fetchDocument } = FirebaseAPI();
+
+  const getPlan = async () => {
+    const result = await fetchDocument(
+      CollectionList.plan,
+      String(params.myapisid)
+    );
+    console.log(result);
+    setPlan(result ?? {});
+  };
+
+  useEffect(() => {
+    getPlan();
+  }, []);
+
   return (
     <div>
       MyPlanDetailPage
       <div>
-        <div>일정명</div>
-        <div>날짜</div>
-        <div>출발지</div>
-        <div>목적지</div>
-        <div>동행자</div>
-        <div>타임라인</div>
+        <div>일정명: {plan.title}</div>
+        <div>출발일: {plan.startDate}</div>
+        <div>도착일: {plan.endDate}</div>
+        <div>출발지: {plan.departureLocation}</div>
+        <div>목적지: {plan.destination}</div>
+        <div>동행자: {plan.companions}</div>
+        <div>타임라인: {plan.timeline}</div>
       </div>
     </div>
   );
