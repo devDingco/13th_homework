fde7aca34b01f9388f1e59b65d51cbcf266bb384
h0ngsik <hongsik.choi.dev@gmail.com>
2024-10-22 16:15:09 +0900

    [241022] refactor: - myapis-write 컴포넌트 생성 - /myapis/new 페이지에 myapis-wirte 컴포넌트 조립

---
+++ 02-main_camp/Day35/src/app/myapis/[myapisid]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MyPlanDetailPage() {
+  return (
+    <div>
+      MyPlanDetailPage
+      <div>
+        <div>일정명</div>
+        <div>날짜</div>
+        <div>출발지</div>
+        <div>목적지</div>
+        <div>동행자</div>
+        <div>타임라인</div>
+      </div>
+    </div>
+  );
+}
