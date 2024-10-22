bf64e346af58d6d97a2e23ed911dc6fe3811d9c0
h0ngsik <hongsik.choi.dev@gmail.com>
2024-10-22 15:21:05 +0900

    [241022] refactor: - Firestore를 다른 곳에서 import 할 수 있도록 수정

--- 02-main_camp/Day35/src/commons/apis/firebase/index.ts
+++ 02-main_camp/Day35/src/commons/apis/firebase/index.ts
@@ -1,35 +1,62 @@
-import { firebaseApp } from "@/commons/apis/firebase/config";
-import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
+import { db } from "@/commons/apis/firebase/config";
+import {
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 export enum CollectionList {
   plan = "plan",
 }
 
 export default function FirebaseAPI() {
-  const fetchCollection = (name: CollectionList) => {
-    const data = collection(getFirestore(firebaseApp), name);
-    return data;
-  };
-
-  const createDocument = async (name: CollectionList, data: object) => {
-    const collection = fetchCollection(name);
-    await addDoc(collection, {
-      data,
-    });
-    alert("일정을 생성합니다.");
+  const createDocument = async (
+    name: CollectionList,
+    index: number,
+    data: object
+  ) => {
+    try {
+      await setDoc(doc(db, name, String(index)), {
+        ...data,
+      });
+      console.log(`${name} 문서 "생성" 성공`);
+    } catch (error) {
+      console.log(`${name} 문서 "생성" 실패: `, error);
+    }
   };
 
   const fetchDocument = async (name: CollectionList) => {
-    const collection = fetchCollection(name);
-    const documents = await getDocs(collection);
-    const data = documents.docs.map((el) => el.data());
-    console.log(data);
-    console.log("일정 문서를 가져옵니다.");
-    return data;
+    try {
+      const documents = await getDocs(collection(db, name));
+      const data = documents.docs.map((el) => el.data());
+      console.log(`${name} 문서 "가져오기" 성공`);
+      return data;
+    } catch (error) {
+      console.log(`${name} 문서 "가져오기" 실패: `, error);
+    }
+  };
+
+  const updateDocument = async (
+    name: CollectionList,
+    index: number,
+    updateValue: object
+  ) => {
+    try {
+      const docRef = doc(db, name, String(index));
+      await updateDoc(docRef, {
+        updateValue,
+      });
+      console.log("문서 업데이트를 성공했습니다.");
+    } catch (error) {
+      console.error("문서 업데이트를 실패했습니다.:", error);
+    }
   };
 
   return {
     createDocument,
     fetchDocument,
+    updateDocument,
   };
 }
