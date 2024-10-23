99447edf12f8670f0bffb38342a64caad4c318eb
jomira0220 <whalfk220@gmail.com>
2024-10-23 09:51:17 +0900

    로그인 페이지 구현 관련 함수 추가 및 리팩토링에 따른 정리

--- next_과제/src/app/(login)/login/hook.ts
+++ next_과제/src/app/(login)/login/hook.ts
@@ -1,9 +1,33 @@
-"use client";
 import { useForm } from "react-hook-form";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export const useLoginPage = () => {
-  const { control } = useForm({
+  // const session: any = await getServerSession(authOptions) // server-side
+  const { data: session } = useSession(); // client-side
+  console.log("세션", session);
+  const router = useRouter();
+
+  const { control, getValues } = useForm({
     mode: "onChange",
   });
-  return { control };
+
+  const signInSubmit = async () => {
+    const { email, password } = getValues();
+    // 0. 이메일과 비밀번호가 입력되었는지 확인
+    if (!email || !password) {
+      return alert("이메일과 비밀번호를 입력해 주세요.");
+    }
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+    if (result?.error) {
+      alert(result.error);
+    }
+  };
+
+  return { control, signInSubmit, router };
 };
