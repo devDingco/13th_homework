5227fd54bddeed04aa732d84685b3f85e6819617
h0ngsik <hongsik.choi.dev@gmail.com>
2024-10-24 17:04:15 +0900

    [241024] feat: - 게시글 이미지 업로드 시 이미지 검증 로직 구현

--- 02-main_camp/Day36/src/app/_components/boards/Boards-write/hook.tsx
+++ 02-main_camp/Day36/src/app/_components/boards/Boards-write/hook.tsx
@@ -181,6 +181,8 @@ export const useBoardWrite = (isEdit?: boolean, data?: FetchBoardQuery) => {
 
   const onChangeFile = async (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
+    if (file === undefined) return;
+    if (!checkValidationImage(file)) return;
     console.log(file);
     const result = await uploadFile({
       variables: {
@@ -196,6 +198,23 @@ export const useBoardWrite = (isEdit?: boolean, data?: FetchBoardQuery) => {
         images: newImages,
       };
     });
+  };
+
+  const checkValidationImage = (file: File) => {
+    if (file.size > 5 * 1024 * 1024) {
+      alert("파일 용량이 너무 큽니다.(제한: 5MB)");
+      return false;
+    }
+
+    if (
+      !file.type.includes("jpeg") &&
+      !file.type.includes("png") &&
+      !file.type.includes("svg")
+    ) {
+      alert("jpeg 또는 png 파일만 업로드 가능합니다.");
+      return false;
+    }
+    return true;
   };
 
   const onClickImage = (event: MouseEvent<HTMLDivElement>) => {
