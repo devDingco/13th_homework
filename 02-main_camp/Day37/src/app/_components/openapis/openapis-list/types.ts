9b5f27bed226d355370f79e64a4eb51f145869b2
h0ngsik <hongsik.choi.dev@gmail.com>
2024-10-24 17:12:03 +0900

    [241024] create: - init Day37

---
+++ 02-main_camp/Day37/src/app/_components/openapis/openapis-list/types.ts
@@ -0,0 +1,24 @@
+interface IFestival {
+  addr1: string;
+  addr2: string;
+  booktour: string;
+  cat1: string;
+  cat2: string;
+  cat3: string;
+  contentid: string;
+  contenttypeid: string;
+  createdtime: string;
+  eventstartdate: string;
+  eventenddate: string;
+  firstimage: string;
+  firstimage2: string;
+  cpyrhtDivCd: string;
+  mapx: string;
+  mapy: string;
+  mlevel: string;
+  modifiedtime: string;
+  areacode: string;
+  sigungucode: string;
+  tel: string;
+  title: string;
+}
