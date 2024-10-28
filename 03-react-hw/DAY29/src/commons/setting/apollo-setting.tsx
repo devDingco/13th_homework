59b1db4a7cd0ea88dd01c473531e6e5fab2118c4
Shin-E2 <sinhyejee322@gmail.com>
2024-10-28 17:58:23 +0900

    [241028] 회원가입, 로그인

--- 03-react-hw/DAY29/src/commons/setting/apollo-setting.tsx
+++ 03-react-hw/DAY29/src/commons/setting/apollo-setting.tsx
@@ -7,18 +7,26 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+import { useAccessTokenStore } from "../stores/accessToken";
 
+const GLOBAL_STATE = new InMemoryCache();
 interface IApolloSetting {
   children: React.ReactNode;
 }
+
 export default function ApolloSetting(props: IApolloSetting) {
+  const { accessToken } = useAccessTokenStore();
+
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
   });
 
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    cache: new InMemoryCache(),
+    cache: GLOBAL_STATE,
   });
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
