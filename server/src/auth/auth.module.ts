75b8105f3d6d8d05bd95d4612857818df79db44f
지승 <rjs8833@gmail.com>
2024-10-29 09:26:48 +0900

    [241029] server register jwt setting

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,13 +1,29 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { BoardModule } from 'src/board/board.module';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserRepository } from './repository/user.repository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User], 'PostgreSQL'), BoardModule],
+    imports: [
+        TypeOrmModule.forFeature([User], 'PostgreSQL'),
+        BoardModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+                signOptions: {
+                    expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+                },
+            }),
+        }),
+    ],
     controllers: [AuthController],
     providers: [AuthService, UserRepository],
 })
