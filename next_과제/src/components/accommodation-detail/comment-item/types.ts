022900a9267aa7284df5a9bcb02bdeb884dc7ca7
jomira0220 <whalfk220@gmail.com>
2024-10-29 22:22:06 +0900

    파일명 수정 및 변경에 따른 삭제 처리

--- next_과제/src/components/accommodation-detail/comment-item/types.ts
+++
@@ -1,7 +0,0 @@
-import { FetchBoardCommentsQuery } from "@/commons/graphql/graphql";
-
-export interface IcommentItemBox {
-  commentData: FetchBoardCommentsQuery["fetchBoardComments"][0];
-  starCountBox: boolean;
-  reply: boolean;
-}
