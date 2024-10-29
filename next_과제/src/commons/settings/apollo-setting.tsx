12fe0202cb1b43c53befb27d0943f4a96c77a84a
jomira0220 <whalfk220@gmail.com>
2024-10-29 10:07:48 +0900

    엑세스 토큰 설정 관련 셋팅 수정

--- next_과제/src/commons/settings/apollo-setting.tsx
+++ next_과제/src/commons/settings/apollo-setting.tsx
@@ -7,18 +7,32 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+import { useAccessTokenStore } from "@/commons/stores/access-token";
+import { useEffect } from "react";
 
+const cache = new InMemoryCache();
 interface IApolloUploadSetting {
   children: React.ReactNode;
 }
 export default function ApolloUploadSetting(props: IApolloUploadSetting) {
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      useAccessTokenStore.getState().setAccessToken(accessToken);
+    }
+  }, []);
+
+  const { accessToken } = useAccessTokenStore();
   const uploadLink = createUploadLink({
-    uri: "http://main-practice.codebootcamp.co.kr/graphql",
+    uri: `${process.env.NEXT_PUBLIC_SERVER_URI}/graphql`,
+    headers: {
+      authorization: `Bearer ${accessToken}`,
+    },
   });
 
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    cache: new InMemoryCache(),
+    cache: cache,
   });
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
