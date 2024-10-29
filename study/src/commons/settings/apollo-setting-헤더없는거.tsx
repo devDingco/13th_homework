af06cf5863ebea5d47ebb5404772c76bd0fd111e
jomira0220 <whalfk220@gmail.com>
2024-10-29 10:00:06 +0900

    수업내용 파일 업로드

--- study/src/commons/settings/apollo-setting-헤더없는거.tsx
+++ study/src/commons/settings/apollo-setting-헤더없는거.tsx
@@ -11,29 +11,18 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
-import { useAccessTokenStore } from "@/commons/stores/22-01-access-token-store";
-
-const GLOBAL_STATE = new InMemoryCache();
 
 interface IApolloUploadSetting {
   children: React.ReactNode;
 }
-export default function ApolloHeaderSetting(props: IApolloUploadSetting) {
-  const { accessToken } = useAccessTokenStore();
-
+export default function ApolloUploadSetting(props: IApolloUploadSetting) {
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
   });
 
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    // cache: new InMemoryCache(),
-    // ★ useAccessToken이 변경되면서 리랜더링 되므로 기존 캐시가 날아가는 문제가 발생
-    // 그러므로 전역으로 캐시를 만들어서 사용한다. 위에 GLOBAL_STATE 전역 변수 선언
-    cache: GLOBAL_STATE,
+    cache: new InMemoryCache(),
   });
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
