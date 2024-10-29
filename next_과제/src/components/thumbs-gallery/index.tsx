0a0c0e10cce0a6924ad55e0d2614e95f15c07a16
jomira0220 <whalfk220@gmail.com>
2024-10-29 22:28:15 +0900

    갤러리 노출 관련 수정

--- next_과제/src/components/thumbs-gallery/index.tsx
+++ next_과제/src/components/thumbs-gallery/index.tsx
@@ -14,52 +14,56 @@ import "./styles.scss";
 
 import { FreeMode, Thumbs } from "swiper/modules";
 
-export default function ThumbsGallery() {
+export default function ThumbsGallery({ images }: { images: string[] }) {
   const [thumbsSwiper, setThumbsSwiper] = useState<Swiper | null>(null);
 
-  const slides = [
-    "/images/mainSlide1.jpg",
-    "/images/mainSlide2.jpg",
-    "/images/mainSlide3.jpg",
-    "/images/beach.jpg",
-  ];
-  //
-
-  return (
-    <div id="thumbsGallery">
-      <Swiper
-        loop={true}
-        spaceBetween={10}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Thumbs]}
-        className="mySwiper2"
-      >
-        {slides &&
-          slides.map((slideImg, idx) => (
+  if (images.length > 0) {
+    return (
+      <div id="thumbsGallery">
+        <Swiper
+          loop={true}
+          spaceBetween={10}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Thumbs]}
+          className="mySwiper2"
+        >
+          {images.map((slideImg, idx) => (
             <SwiperSlide key={slideImg + idx}>
-              <Image src={slideImg} alt="slideImg" width="1000" height="1000" />
+              <Image
+                src={`${process.env.NEXT_PUBLIC_IMAGE_HOST_NAME}${slideImg}`}
+                alt="slideImg"
+                width="1000"
+                height="1000"
+              />
             </SwiperSlide>
           ))}
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        loop={true}
-        spaceBetween={10}
-        slidesPerView="auto"
-        freeMode={true}
-        // centeredSlides={true}
-        watchSlidesProgress={true}
-        direction="vertical"
-        modules={[FreeMode, Thumbs]}
-        className="mySwiper"
-      >
-        {slides &&
-          slides.map((slideImg, idx) => (
+        </Swiper>
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          loop={true}
+          spaceBetween={10}
+          slidesPerView="auto"
+          freeMode={true}
+          // centeredSlides={true}
+          watchSlidesProgress={true}
+          direction="vertical"
+          modules={[FreeMode, Thumbs]}
+          className="mySwiper"
+        >
+          {images.map((slideImg, idx) => (
             <SwiperSlide key={slideImg + idx}>
-              <Image src={slideImg} alt="slideImg" width="100" height="100" />
+              <Image
+                src={`${process.env.NEXT_PUBLIC_IMAGE_HOST_NAME}${slideImg}`}
+                alt="slideImg"
+                width="1000"
+                height="1000"
+              />
             </SwiperSlide>
           ))}
-      </Swiper>
-    </div>
-  );
+        </Swiper>
+      </div>
+    );
+  } else {
+    return <div>등록된 이미지가 없습니다.</div>;
+  }
 }
