cfccbb71eaae434838501c4171190fd39c886003
jomira0220 <whalfk220@gmail.com>
2024-10-29 10:09:24 +0900

    우편번호 검색 컴포넌트 수정 - 위도 경도 확인하는 함수 추가

--- next_과제/src/components/post-search-pop-btn/index.tsx
+++ next_과제/src/components/post-search-pop-btn/index.tsx
@@ -1,24 +1,43 @@
-import { useDaumPostcodePopup } from "react-daum-postcode";
-import { Button } from "antd";
+import DaumPostcodeEmbed from "react-daum-postcode";
+import { Button, Modal } from "antd";
+import { useState } from "react";
 
 type PostSearchPopProps = {
-  setaddress: (field: string, value: string) => void;
-  setzonecode: (field: string, value: string) => void;
-  btnstyle?: string;
   disabled?: boolean;
+
+  setaddress: (_: string, value: string) => void;
+  setzonecode: (_: string, value: string) => void;
+  addressKeyName: string;
+  addressPostKeyName: string;
+
+  setLat?: (_: string, value: string) => void;
+  setLng?: (_: string, value: string) => void;
+  addressLatKeyName?: string;
+  addressLngKeyName?: string;
 };
 
-const PostSearchPopBtn = (props: PostSearchPopProps) => {
-  const { setaddress, setzonecode, btnstyle, disabled = false } = props;
-  const open = useDaumPostcodePopup(process.env.DAUM_POSTCODE_API_KEY);
+interface dataType {
+  address: string;
+  addressType: string;
+  bname: string;
+  buildingName: string;
+  zonecode: string;
+}
 
-  interface dataType {
-    address: string;
-    addressType: string;
-    bname: string;
-    buildingName: string;
-    zonecode: string;
-  }
+const PostSearchPopBtn = (props: PostSearchPopProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const {
+    setaddress,
+    setzonecode,
+    setLat,
+    setLng,
+    disabled = false,
+    addressKeyName,
+    addressPostKeyName,
+    addressLatKeyName,
+    addressLngKeyName,
+  } = props;
 
   const handleComplete = (data: dataType) => {
     let fullAddress = data.address;
@@ -36,27 +55,81 @@ const PostSearchPopBtn = (props: PostSearchPopProps) => {
       fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
     }
 
-    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
-    setaddress("writeAddress", fullAddress);
-    setzonecode("writeAddressPost", zoneCode);
+    // console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+    setaddress(addressKeyName, fullAddress);
+    setzonecode(addressPostKeyName, zoneCode);
+
+    // 주소 -> 좌표 변환 요청
+    fetchCoordinates(fullAddress);
+
+    setIsModalOpen(false);
   };
 
-  const handleClick = () => {
-    open({ onComplete: handleComplete });
+  const fetchCoordinates = async (address: string) => {
+    try {
+      const response = await fetch(
+        `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURIComponent(
+          address
+        )}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+            // Referer: "http://localhost:3000", //! 이부분 실제 배포시 카카오 개발자 콘솔과 함께 수정해야함
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("네트워크 응답에 문제가 있습니다.");
+      }
+
+      const data = await response.json();
+      if (data.documents.length > 0) {
+        const { y, x } = data.documents[0];
+        setLat && addressLatKeyName && setLat(addressLatKeyName, y);
+        setLng && addressLngKeyName && setLng(addressLngKeyName, x);
+      } else {
+        console.error("좌표를 찾을 수 없습니다.");
+      }
+    } catch (error) {
+      console.error("좌표 변환 오류:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(true);
   };
 
   return (
-    <Button
-      disabled={disabled}
-      type="default"
-      size="large"
-      color="default"
-      variant="solid"
-      className={btnstyle ? btnstyle : "btn btn-outline"}
-      onClick={() => handleClick()}
-    >
-      우편번호 검색
-    </Button>
+    <>
+      {isModalOpen && (
+        <Modal
+          title="우편번호 검색"
+          onOk={handleOk}
+          open={isModalOpen}
+          onCancel={handleCancel}
+          footer={null}
+        >
+          <DaumPostcodeEmbed onComplete={handleComplete} {...props} />
+        </Modal>
+      )}
+
+      <Button
+        disabled={disabled}
+        type="default"
+        size="large"
+        color="default"
+        variant="solid"
+        onClick={() => handleOk()}
+      >
+        우편번호 검색
+      </Button>
+    </>
   );
 };
 
