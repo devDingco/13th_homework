35aacb0723b7e86946df6e93c90f111843938950
yheseul <kangyheseul@naver.com>
2024-10-29 18:46:21 +0900

    feat: complete publishing of login landing page

--- trip-talk/src/app/page.tsx
+++ trip-talk/src/app/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import Input from "./_components/Input/Input";
+import Button from "./_components/Button/Button";
+import useLogin from "../commons/hooks/useLogin";
+
 export default function Home() {
-  return <div>Next.js first page</div>;
+  const { handleInputChange, onClickLogin, onClickSignUp } = useLogin();
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-col w-80 items-center gap-6 self-stretch px-5 pt-16">
+        <Image src="/pngs/logo.png" alt="logo" width={120} height={74.533} />
+        <div className="self-stretch text-black text-center text-lg not-italic font-semibold leading-6">
+          트립트립에 오신걸 환영합니다.
+        </div>
+        <div className="self-stretch text-[color:var(--gray-800,#333)] text-center text-sm not-italic font-medium leading-5">
+          트립트립에 로그인 하세요.
+        </div>
+
+        <Input
+          id="email"
+          required={false}
+          onChange={handleInputChange}
+          isLabel={false}
+        />
+        <Input
+          id="password"
+          required={false}
+          onChange={handleInputChange}
+          isLabel={false}
+        />
+        <Button id="login" color="blue" width="100%" onClick={onClickLogin} />
+        <Button
+          id="singUp"
+          color="white"
+          width="100%"
+          onClick={onClickSignUp}
+        />
+      </div>
+
+      <Image
+        src="/pngs/login-signup.png"
+        alt="loginSignUp"
+        width={900}
+        height={600}
+      />
+    </div>
+  );
 }
