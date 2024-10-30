95aaf67952ba5421a5638c00da47a852ab197773
yheseul <kangyheseul@naver.com>
2024-10-30 19:03:58 +0900

    feat: apply access token in Apollo setting for login

--- trip-talk/src/commons/settings/apollo-setting.tsx
+++ trip-talk/src/commons/settings/apollo-setting.tsx
@@ -7,18 +7,31 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+import { useAccessTokenStore } from "../stores/useAccessTokenStore";
+import { useEffect } from "react";
+
+const GLOBAL_STATE = new InMemoryCache();
 
 interface IApolloUploadSetting {
   children: React.ReactNode;
 }
 export default function ApolloUploadSetting(props: IApolloUploadSetting) {
+  const { accessToken, setAccessToken } = useAccessTokenStore();
+
+  useEffect(() => {
+    setAccessToken(localStorage.getItem("accessToken") ?? "")
+  }, [])
+
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
   });
 
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    cache: new InMemoryCache(),
+    cache: GLOBAL_STATE,
   });
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
