b8532c88fcf5989cfe97168418b984a9c3dbd099
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-04 02:06:19 +0900

    [241104] design: - 관리자인 경우 답변하기 아이콘 및 레이블 띄우기 - 답변하기 클릭 시 문의 작성할 수 있도록 구현 - 아이콘 이미지 사이즈 변경

--- 02-main_camp/Day39/src/commons/ui/comment/comment/index.tsx
+++ 02-main_camp/Day39/src/commons/ui/comment/comment/index.tsx
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
-import { Close, Edit, Profile } from "../../icon";
+import { Close, Edit, Profile, ReplyIcon } from "../../icon";
+import ReplyWriting from "../../reply/writing";
 
 interface ICommentProps {
+  isManaged?: boolean;
   nickname: string;
   contents: string;
   date: string;
 }
 
-export default function Comment({ nickname, contents, date }: ICommentProps) {
+export default function Comment({
+  isManaged = true,
+  nickname,
+  contents,
+  date,
+}: ICommentProps) {
+  const [isReplyOpened, setIsReplyOpened] = useState(false);
+
+  const onClickReply = () => {
+    setIsReplyOpened((prev) => !prev);
+  };
+
+  const onClickCancel = () => {
+    setIsReplyOpened((prev) => !prev);
+  };
+
   return (
     <div className={styles.comment__container}>
-      <div className={styles.header__container}>
-        <div className={styles.user__info__container}>
-          <Profile />
-          {nickname}
+      <div className={styles.user__container}>
+        <div className={styles.header__container}>
+          <div className={styles.user__info__container}>
+            <Profile />
+            {nickname}
+          </div>
+          {isManaged === false && (
+            <div className={styles.sideMenu__container}>
+              <Edit width={1.25} height={1.25} />
+              <Close width={1.25} height={1.25} />
+            </div>
+          )}
         </div>
-        <div className={styles.sideMenu__container}>
-          <Edit width={20} height={20} />
-          <Close width={20} height={20} />
-        </div>
+        <div className={styles.contents__container}>{contents}</div>
+        <div className={styles.date__container}>{date}</div>
       </div>
-      <div className={styles.contents__container}>{contents}</div>
-      <div className={styles.date__container}>{date}</div>
+
+      {isManaged && isReplyOpened === false && (
+        <div className={styles.reply__container} onClick={onClickReply}>
+          <ReplyIcon />
+          <span className={styles.reply__label}>답변 하기</span>
+        </div>
+      )}
+      {isReplyOpened && <ReplyWriting handleCancel={onClickCancel} />}
     </div>
   );
 }
