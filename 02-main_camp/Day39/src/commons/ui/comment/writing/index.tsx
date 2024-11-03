c396890478f837d68e3a5faf2799fea3a81d31bc
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-04 02:07:21 +0900

    [241104] design: - 레이블 숨김 여부 속성 추가 - 취소 버튼 숨김 여부 속성 추가 - 취소 버튼 동작 속성 추가

--- 02-main_camp/Day39/src/commons/ui/comment/writing/index.tsx
+++ 02-main_camp/Day39/src/commons/ui/comment/writing/index.tsx
@@ -4,25 +4,40 @@ import { TextArea } from "../../input/textarea";
 import styles from "./styles.module.css";
 
 interface ICommentWritingProps {
-  label: string;
+  hiddenLabel?: boolean;
+  hiddenCancel?: boolean;
+  label?: string;
   placeholder: string;
   buttonText: string;
+  handleCancel?: () => void;
 }
 
 export default function CommentWriting({
+  hiddenLabel = false,
+  hiddenCancel = true,
   label,
   placeholder,
   buttonText,
+  handleCancel,
 }: ICommentWritingProps) {
   return (
     <div className={styles.commentWriting__container}>
-      <div className={styles.label__container}>
-        <Chat />
-        <span className={styles.label}>{label}</span>
-      </div>
+      {hiddenLabel === false && (
+        <div className={styles.label__container}>
+          <Chat />
+          <span className={styles.label}>{label}</span>
+        </div>
+      )}
       <div className={styles.contents__container}>
         <TextArea showCount={true} placeholder={placeholder} />
-        <button className={styles.button}>{buttonText}</button>
+        <div className={styles.buttons__container}>
+          {hiddenCancel === false && (
+            <button className={styles.button__cancel} onClick={handleCancel}>
+              취소
+            </button>
+          )}
+          <button className={styles.button}>{buttonText}</button>
+        </div>
       </div>
     </div>
   );
