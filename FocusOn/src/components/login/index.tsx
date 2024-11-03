01308982656e1f438fce0b8233c05bdf0a4e526b
MingseoYa <alstj3758@naver.com>
2024-11-04 01:55:55 +0900

    회원가입 스키마 작성, 공통 컴포넌트 적용

--- FocusOn/src/components/login/index.tsx
+++ FocusOn/src/components/login/index.tsx
@@ -5,6 +5,7 @@ import InputField from "../commons/input";
 import styles from "./styles.module.css";
 import useLogin from "./hook";
 import ErrorMsg from "../commons/error";
+import Image from "next/image";
 
 export default function Login() {
   const { onChangeLoginInputs, onClickLogin, errorMessage } = useLogin();
@@ -12,8 +13,17 @@ export default function Login() {
     <div className={styles.login_page_body}>
       <div className={styles.main_box}>
         <div className={styles.main_form}>
-          <div>로고</div>
-          <div className={styles.login_title}>어쩌구에 오신걸 환영합니다</div>
+          <Image
+            src="/images/focuson.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="FocusOn 로고"
+            className={styles.logo}
+          />
+          <div className={styles.login_title}>
+            포커스온에 오신것을 환영합니다
+          </div>
           <div className={styles.input_form}>
             <InputField
               placeholder="이메일을 입력해 주세요."
