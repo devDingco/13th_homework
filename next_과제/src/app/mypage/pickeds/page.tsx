66e8f7edb3a72ee47e08b7a2ca8173648b0bf3a1
jomira0220 <whalfk220@gmail.com>
2024-11-04 09:25:16 +0900

    pickeds페이지 추가

---
+++ next_과제/src/app/mypage/pickeds/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import PickedList from "@/components/mypage/picked-list";
+import MyPageSubMenu from "@/components/mypage/mypage-menu/sub-menu";
+
+export default function MyBookMarkPage() {
+  return (
+    <>
+      <MyPageSubMenu />
+      <PickedList />
+    </>
+  );
+}
