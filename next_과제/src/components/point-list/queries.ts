a206d44cb6624493698e087ab9503b7d668a990b
jomira0220 <whalfk220@gmail.com>
2024-11-04 09:34:33 +0900

    포인트 리스트 노출 관련 변수 파일 분리 및 graphql 호출 관련 추가

--- next_과제/src/components/point-list/queries.ts
+++ next_과제/src/components/point-list/queries.ts
@@ -1,5 +1,84 @@
 import { gql } from "@apollo/client";
 
+export const FETCH_POINT_TRANSACTIONS_OF_ALL = gql`
+  query fetchPointTransactionsOfAll($search: String, $page: Int) {
+    fetchPointTransactionsOfBuying(search: $search, page: $page) {
+      _id
+      impUid
+      amount
+      balance
+      status
+      statusDetail
+      # travelproduct {
+      #   _id
+      #   name
+      #   remarks
+      #   contents
+      #   price
+      #   tags
+      #}
+      user {
+        name
+      }
+      createdAt
+      updatedAt
+      deletedAt
+    }
+    fetchPointTransactionsOfLoading(search: $search, page: $page) {
+      _id
+      impUid
+      amount
+      balance
+      status
+      statusDetail
+      # travelproduct {
+      #   _id
+      #   name
+      #   remarks
+      #   contents
+      #   price
+      #   tags
+      #}
+      user {
+        name
+      }
+      createdAt
+      updatedAt
+      deletedAt
+    }
+    fetchPointTransactionsOfSelling(search: $search, page: $page) {
+      _id
+      impUid
+      amount
+      balance
+      status
+      statusDetail
+      # travelproduct {
+      #   _id
+      #   name
+      #   remarks
+      #   contents
+      #   price
+      #   tags
+      #}
+      user {
+        name
+      }
+      createdAt
+      updatedAt
+      deletedAt
+    }
+  }
+`;
+
+export const FETCH_POINT_TRANSACTIONS_COUNT_OF_ALL = gql`
+  query fetchPointTransactionsCountOfAll {
+    fetchPointTransactionsCountOfBuying
+    fetchPointTransactionsCountOfLoading
+    fetchPointTransactionsCountOfSelling
+  }
+`;
+
 export const FETCH_POINT_TRANSACTIONS_OF_BUYING = gql`
   query fetchPointTransactionsOfBuying($search: String, $page: Int) {
     fetchPointTransactionsOfBuying(search: $search, page: $page) {
@@ -30,5 +109,71 @@ export const FETCH_POINT_TRANSACTIONS_OF_BUYING = gql`
 export const FETCH_POINT_TRANSACTIONS_COUNT_OF_BUYING = gql`
   query fetchPointTransactionsCountOfBuying {
     fetchPointTransactionsCountOfBuying
+  }
+`;
+
+export const FETCH_POINT_TRANSACTIONS_OF_LOADING = gql`
+  query fetchPointTransactionsOfLoading($search: String, $page: Int) {
+    fetchPointTransactionsOfLoading(search: $search, page: $page) {
+      _id
+      impUid
+      amount
+      balance
+      status
+      statusDetail
+      # travelproduct {
+      #   _id
+      #   name
+      #   remarks
+      #   contents
+      #   price
+      #   tags
+      #}
+      user {
+        name
+      }
+      createdAt
+      updatedAt
+      deletedAt
+    }
+  }
+`;
+
+export const FETCH_POINT_TRANSACTIONS_COUNT_OF_LOADING = gql`
+  query fetchPointTransactionsCountOfLoading {
+    fetchPointTransactionsCountOfLoading
+  }
+`;
+
+export const FETCH_POINT_TRANSACTIONS_OF_SELLING = gql`
+  query fetchPointTransactionsOfSelling($search: String, $page: Int) {
+    fetchPointTransactionsOfSelling(search: $search, page: $page) {
+      _id
+      impUid
+      amount
+      balance
+      status
+      statusDetail
+      # travelproduct {
+      #   _id
+      #   name
+      #   remarks
+      #   contents
+      #   price
+      #   tags
+      #}
+      user {
+        name
+      }
+      createdAt
+      updatedAt
+      deletedAt
+    }
+  }
+`;
+
+export const FETCH_POINT_TRANSACTIONS_COUNT_OF_SELLING = gql`
+  query fetchPointTransactionsCountOfSelling {
+    fetchPointTransactionsCountOfSelling
   }
 `;
