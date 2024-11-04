6a731257a0f8c4c46739c4923bed54b3dc784727
jomira0220 <whalfk220@gmail.com>
2024-11-04 09:25:30 +0900

    컴포넌트 경로 수정

--- next_과제/src/app/mypage/page.tsx
+++ next_과제/src/app/mypage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import ProductList from "@/components/product-list";
-import MyPageSubMenu from "@/components/mypage-menu/sub-menu";
+import MyPageSubMenu from "@/components/mypage/mypage-menu/sub-menu";
 
 export default function MyPage() {
   return (
