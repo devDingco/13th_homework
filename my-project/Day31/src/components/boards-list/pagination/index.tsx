f0e3feba0cadae14137cdb45d18cac63612911ac
moonhuicheol <moonhuicheol@gmail.com>
2024-11-05 22:09:09 +0900

    페이지네이션 구현완료

--- my-project/Day31/src/components/boards-list/pagination/index.tsx
+++ my-project/Day31/src/components/boards-list/pagination/index.tsx
@@ -2,9 +2,8 @@ import { Pagination } from "antd";
 
 // 페이지네이션을 구현할 때 필요한 것
 // 페이지 버튼을 누를때마다 게시글 불러오기(refetch)
-// 현재 페이지 값을 관리할 스테이트
+// 현재 페이지 값을 관리할 스테이트와 set스테이트
 // 총 페이지 수
-
 export default function PageButton({
   currentPage,
   refetch,
