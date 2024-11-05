5515a4260d202220f8a689517f8139997d27cece
revy7289 <95402753+revy7289@users.noreply.github.com>
2024-11-05 09:35:41 +0900

    스타일: inline-style 남은 것들 Pandacss로

--- main-app/src/app/login/page.tsx
+++ main-app/src/app/login/page.tsx
@@ -8,9 +8,10 @@ import Input from "@/components/Atoms/_Input";
 import Button from "@/components/Atoms/_Button";
 import InputField from "@/components/Molecules/_InputField";
 
-import { CSSProperties, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useLoginUser from "@/common/hooks/useLoginUser";
+import { css } from "@/common/styled-system/css";
 
 export default function LoginPage() {
     const router = useRouter();
@@ -20,37 +21,37 @@ export default function LoginPage() {
     const [isLogIn, setIsLogIn] = useState(true);
 
     return (
-        <div style={CSSLoginWrap}>
-            <div style={CSSLoginImg}>
-                <Image src={cover} alt="greeting" style={CSSImgAdjust} />
+        <div className={CSS_LoginWrap}>
+            <div className={CSS_LoginImg}>
+                <Image src={cover} alt="greeting" className={CSS_ImgAdjust} />
             </div>
 
-            <div style={CSSLineBG}></div>
+            <div className={CSS_LineBG}></div>
 
-            <div style={CSSLoginBG}>
-                <div style={CSSLoginForm}>
+            <div className={CSS_LoginBG}>
+                <div className={CSS_LoginForm}>
                     {isLogIn ? (
                         <>
                             <Image
                                 src={logoVer}
                                 alt="logo_ver"
-                                style={CSSLogoAdjust}
+                                className={CSS_LogoAdjust}
                                 onClick={() => router.push(`/`)}
                             />
 
-                            <p style={{ fontSize: "1.8rem", margin: "1rem" }}>
+                            <p className={CSS_Text}>
                                 성북 통합 구립도서관 사이트에 오신 것을 환영합니다.
                             </p>
 
                             <Input id="email_ID" onChange={onLoginChange} />
                             <Input id="password_ID" onChange={onLoginChange} />
                             {loginError && (
-                                <div style={CSSValidText}>
+                                <div className={css({ color: "var(--chroma-error)" })}>
                                     아이디 또는 비밀번호를 확인해 주세요.
                                 </div>
                             )}
 
-                            <div style={CSSButtonWrap}>
+                            <div className={CSS_ButtonWrap}>
                                 <Button label="로그인" onClick={onLoginInClick} />
                                 <Button
                                     label="회원가입"
@@ -60,8 +61,8 @@ export default function LoginPage() {
                         </>
                     ) : (
                         <>
-                            <p style={{ fontSize: "2.4rem", margin: "1rem" }}>회원 가입</p>
-                            <p style={{ fontSize: "2rem", margin: "1rem" }}>
+                            <p className={CSS_Text}>회원 가입</p>
+                            <p className={CSS_Text}>
                                 회원 가입을 위해 아래 빈 칸을 모두 채워 주세요.
                             </p>
 
@@ -74,7 +75,7 @@ export default function LoginPage() {
                                 required
                             />
 
-                            <div style={CSSButtonWrap}>
+                            <div className={CSS_ButtonWrap}>
                                 <Button label="취소" onClick={() => setIsLogIn((prev) => !prev)} />
                                 <Button label="회원가입" onClick={onSignUpClick} />
                             </div>
@@ -86,62 +87,63 @@ export default function LoginPage() {
     );
 }
 
-const CSSLoginWrap: CSSProperties = {
+const CSS_LoginWrap = css({
     display: "flex",
     width: "100vw",
     height: "100vh",
     position: "fixed",
-};
+});
 
-const CSSLoginImg: CSSProperties = {
+const CSS_LoginImg = css({
     position: "relative",
-    width: "70%",
+    width: "71%",
     overflow: "hidden",
-};
+});
 
-const CSSImgAdjust: CSSProperties = {
+const CSS_ImgAdjust = css({
     position: "absolute",
     objectFit: "cover",
     height: "100%",
-};
+});
 
-const CSSLoginBG: CSSProperties = {
-    position: "relative",
-    width: "30%",
-    backgroundColor: "#fcfcfc",
-    padding: "2rem 4rem 2rem 8rem",
-    flexShrink: "0",
-};
-
-const CSSLogoAdjust: CSSProperties = {
-    width: "20rem",
-    cursor: "pointer",
-    // filter: "brightness(0) invert(100%)",
-};
-
-const CSSLoginForm: CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "1rem",
-    height: "80%",
-};
-
-const CSSLineBG: CSSProperties = {
+const CSS_LineBG = css({
     width: "1%",
     height: "100%",
     backgroundColor: "#faa634",
     position: "absolute",
     right: "28%",
     zIndex: "1",
-};
+});
 
-const CSSButtonWrap = {
+const CSS_LoginBG = css({
+    position: "relative",
+    width: "30%",
+    backgroundColor: "#fcfcfc",
+    padding: "2rem 4rem 2rem 8rem",
+    flexShrink: "0",
+});
+
+const CSS_LoginForm = css({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "1rem",
+    height: "80%",
+});
+
+const CSS_LogoAdjust = css({
+    width: "20rem",
+    cursor: "pointer",
+    // filter: "brightness(0) invert(100%)",
+});
+
+const CSS_ButtonWrap = css({
     display: "flex",
     gap: "2rem",
-};
+});
 
-const CSSValidText = {
-    color: "#f55",
-};
+const CSS_Text = css({
+    fontSize: "2rem",
+    margin: "1rem",
+});
