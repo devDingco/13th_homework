28a655c2ef48837aea4864fd708c154e3ffcfe25
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-06 17:57:37 +0900

    [241106] design: - 인풋 검증 상태 속성 추가 - 에러 메시지 추가

--- 02-main_camp/Day41/src/app/_components/commons/input/index.tsx
+++ 02-main_camp/Day41/src/app/_components/commons/input/index.tsx
@@ -3,12 +3,16 @@
 import { HTMLInputTypeAttribute } from "react";
 import styles from "./styles.module.css";
 import { FieldValues, Path, useFormContext } from "react-hook-form";
+import classNames from "classnames";
+
+type ValidationStatus = "default" | "error";
 
 interface IInputBaseProps<T extends FieldValues> {
   type: HTMLInputTypeAttribute;
   className?: string;
   placeholder: string;
   keyName: Path<T>;
+  validationStatus?: ValidationStatus;
 }
 
 function InputBase<T extends FieldValues>({
@@ -29,7 +33,16 @@ function InputBase<T extends FieldValues>({
 }
 
 export function Input<T extends FieldValues>(props: IInputBaseProps<T>) {
-  return <InputBase className={styles.input} {...props} />;
+  const getClassNames = () => {
+    console.log(props.validationStatus);
+    if (typeof props.validationStatus === "undefined") return;
+
+    return classNames(`${styles.input}`, {
+      [styles.error__input]: props.validationStatus === "error",
+    });
+  };
+
+  return <InputBase className={getClassNames()} {...props} />;
 }
 
 interface IBaseInputHeaderProps {
@@ -61,6 +74,8 @@ interface IInputFormProps<T extends FieldValues> {
   children?: React.ReactNode;
   isRequired?: boolean;
   isHiddenHeader?: boolean;
+  validationStatus?: ValidationStatus;
+  errorMessage?: string;
 }
 
 export function InputForm<T extends FieldValues>({
@@ -71,17 +86,30 @@ export function InputForm<T extends FieldValues>({
   children,
   isRequired = false,
   isHiddenHeader = false,
+  validationStatus = "default",
+  errorMessage,
 }: IInputFormProps<T>) {
-  const props = { label, type, placeholder, isRequired, children, keyName };
+  const props = {
+    label,
+    type,
+    placeholder,
+    isRequired,
+    children,
+    keyName,
+    validationStatus,
+  };
+
   return (
     <div className={styles.form__container}>
       {!isHiddenHeader && (
         <InputHeader isRequired={isRequired}>{label}</InputHeader>
       )}
-      <div className={styles.form__Input__container}>
+      <div className={styles.form__input__container}>
         <Input {...props} />
+        {/* 인풋 오른쪽에 버튼과 같은 요소가 있는 경우 사용. */}
         {children}
       </div>
+      <span className={styles.error__message}>{errorMessage}</span>
     </div>
   );
 }
