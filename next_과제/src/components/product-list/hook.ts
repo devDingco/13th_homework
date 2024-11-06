ff52ad5dd95d292c05ce37aa6f71830eabd38b7c
jomira0220 <whalfk220@gmail.com>
2024-11-06 14:45:24 +0900

    예약 마감 상품 조회 추가

--- next_과제/src/components/product-list/hook.ts
+++ next_과제/src/components/product-list/hook.ts
@@ -1,14 +1,25 @@
 "use client";
-// import { FETCH_TRAVELS } from "./queries";
 import { FetchTravelproductsDocument } from "@/commons/graphql/graphql";
 import { useQuery } from "@apollo/client";
+import { useSearch } from "@/commons/stores/search-store";
+import { useState } from "react";
 
 export const useProductList = () => {
-  const { data: fetchTravelsData } = useQuery(FetchTravelproductsDocument, {
-    variables: { isSoldout: false, search: "", page: 2 },
-  });
+  const { search } = useSearch();
+  const [isSoldout, setIsSoldout] = useState(false);
+  const { data: fetchTravelsData, refetch } = useQuery(
+    FetchTravelproductsDocument,
+    {
+      variables: { isSoldout },
+    }
+  );
   const data = fetchTravelsData?.fetchTravelproducts;
   console.log("숙박리스트", data);
 
-  return { data };
+  const handleSearch = async () => {
+    const result = await refetch({ search });
+    console.log(result);
+  };
+
+  return { data, handleSearch, setIsSoldout, isSoldout };
 };
