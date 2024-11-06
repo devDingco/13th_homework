b901691621cbea2bc4efbda8d6787f4e7846282e
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-06 10:04:26 +0900

    [241106] design: - Login, SignUp 컴포넌트를 결합하여 LoginPage 구현

--- 02-main_camp/Day41/src/app/login/page.tsx
+++ 02-main_camp/Day41/src/app/login/page.tsx
@@ -1,33 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./styles.module.css";
-import { LoginMainImage, LogoIcon } from "@/commons/ui/icon";
-import { InputForm } from "../_components/commons/input";
+import Login from "@/commons/ui/login";
+import { LoginMainImage } from "@/commons/ui/icon";
+import SignUp from "@/commons/ui/signUp";
 
 export default function LoginPage() {
+  const [signUp, setSignUp] = useState(false);
+
+  const presentSignUpPage = () => {
+    setSignUp(true);
+  };
+
+  const dismissSignUp = () => {
+    setSignUp(false);
+  };
+
+  const signUpCompletionHandler = () => {
+    dismissSignUp();
+  };
+
   return (
     <div className={styles.loginPage__container}>
-      <div className={styles.login__container}>
-        <div className={styles.logo__container}>
-          <LogoIcon width={7.5} height={5} />
-          <span className={styles.description}>
-            트립트립에 오신걸 환영합니다.
-          </span>
-        </div>
-        <div className={styles.input__container}>
-          <span className={styles.login__description}>
-            트립트립에 로그인 하세요.
-          </span>
-          <div className={styles.inputs}>
-            <InputForm type="text" placeholder="이메일을 입력해주세요." />
-            <InputForm type="password" placeholder="비밀번호를 입력해주세요." />
-          </div>
-        </div>
-        <div className={styles.buttons__container}>
-          <button className={styles.button__login}>로그인</button>
-          <button className={styles.button__signUp}>회원가입</button>
-        </div>
+      <div className={styles.form__container}>
+        {!signUp && <Login handleSignUp={presentSignUpPage} />}
+        {signUp && (
+          <SignUp
+            handleCancel={dismissSignUp}
+            completionHandler={signUpCompletionHandler}
+          />
+        )}
       </div>
-      <LoginMainImage />
+      <div className={styles.image__wrap}>
+        <LoginMainImage />
+      </div>
     </div>
   );
 }
