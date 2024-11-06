8f703ec405b255676e9c871d81be67ea3f3611cf
jomira0220 <whalfk220@gmail.com>
2024-11-06 14:32:15 +0900

    로그인 처리 관련 함수 추가

---
+++ next_과제/src/commons/hocs/withAuth.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useLoadStore } from "@/commons/stores/load-store";
+import { useAccessTokenStore } from "@/commons/stores/access-token";
+import { useEffect, ComponentType } from "react";
+
+export const withAuth = <P extends object>(Component: ComponentType<P>) => {
+  const WrappedComponent = (props: P) => {
+    const router = useRouter();
+    const { isLoaded } = useLoadStore();
+    const { accessToken } = useAccessTokenStore();
+
+    useEffect(() => {
+      // 이미 아폴로 셋팅에서 useEffect 로 액세스 토큰을 저장하고 있기 때문에
+      // 여기서는 그 로딩이 완료되었는지만 확인해서 처리하면 된다.
+      if (!isLoaded) return;
+      if (accessToken) return;
+
+      // 로딩이 완료된 상태인데 엑세스 토큰이 없다면 로그인 페이지로 이동
+      alert("로그인 후 이용해주세요.");
+      router.push("/login");
+    }, [isLoaded]);
+
+    return isLoaded && accessToken !== "" && <Component {...props} />;
+  };
+
+  return WrappedComponent;
+};
