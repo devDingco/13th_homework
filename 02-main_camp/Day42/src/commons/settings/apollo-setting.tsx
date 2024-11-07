bcd033830c8fae052e9732fe77c9d92b0733d8d8
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-07 12:15:12 +0900

    [241107] feat: - 새로 고침 시 로컬 스토리지에서 access token을 가져와서 사용 - http 헤더를 전송할 때, access token을 포함하여 전송

--- 02-main_camp/Day42/src/commons/settings/apollo-setting.tsx
+++ 02-main_camp/Day42/src/commons/settings/apollo-setting.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useAccessTokenStore } from "@/app/_store/accessToken/store";
 import {
   ApolloClient,
   ApolloLink,
@@ -7,17 +8,28 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+import { useEffect } from "react";
+
+const GLOBAL_STATE = new InMemoryCache();
 
 interface IApolloUploadSetting {
   children: React.ReactNode;
 }
 export default function ApolloUploadSetting(props: IApolloUploadSetting) {
+  const { accessToken, setAccessToken } = useAccessTokenStore();
+
+  useEffect(() => {
+    const result = localStorage.getItem("accessToken") ?? "";
+    setAccessToken(result);
+  }, []);
+
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
+    headers: { Authorization: `Bearer ${accessToken}` },
   });
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    cache: new InMemoryCache(),
+    cache: GLOBAL_STATE,
   });
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
