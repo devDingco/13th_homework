0cc288c93a09ff78c2d66366f0286de1530dfe6a
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-07 20:15:11 +0900

    [241107] other: 여행 상품 생성 및 업데이트 쿼리 추가

--- 02-main_camp/Day43/src/commons/graphql/travelProduct.ts
+++ 02-main_camp/Day43/src/commons/graphql/travelProduct.ts
@@ -34,3 +34,27 @@ export const FETCH_TRAVEL_PRODUCT = gql`
     }
   }
 `;
+
+export const CREATE_TRAVEL_PRODUCT = gql`
+  mutation createTravelProduct(
+    $createTravelproductInput: CreateTravelproductInput!
+  ) {
+    createTravelproduct(createTravelproductInput: $createTravelproductInput) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_TRAVEL_PRODUCT = gql`
+  mutation updateTravelproduct(
+    $updateTravelproductInput: UpdateTravelproductInput!
+    $travelproductId: ID!
+  ) {
+    updateTravelproduct(
+      updateTravelproductInput: $updateTravelproductInput
+      travelproductId: $travelproductId
+    ) {
+      _id
+    }
+  }
+`;
