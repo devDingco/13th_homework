ce22be3bab09fbff9be651e36690e155a5b6794e
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-10 15:02:19 +0900

    [241110] fix: 여행 상품 스키마 변경 - 인풋으로 입력받는 값은 모두 string 타입이기 때문에 수정 - zod를 통해 입력 양식 검증 후 오류 메시지 출력 - 타입스크립트에서 객체의 키값을 string 타입의 문자열을 사용할 수 없어, 사용할 수 있도록 인덱스 시그니처 추가

--- 02-main_camp/Day43/src/app/_schema/travelProductSchema.tsx
+++ 02-main_camp/Day43/src/app/_schema/travelProductSchema.tsx
@@ -1,35 +1,30 @@
 import { z } from "zod";
 
 export interface ITravelProductSchema {
+  [key: string]: string | string[];
   name: string;
   remarks: string;
   contents: string;
-  price: number;
+  price: string;
   tags: string[];
-  travelProductAddressInput: ITravelProductAddressInput;
-  images: string[];
-}
-
-interface ITravelProductAddressInput {
   zipcode: string;
   address: string;
   addressDetail: string;
   lat: string;
   lng: string;
+  images: string[];
 }
 
 export const travelProductSchema = z.object({
-  name: z.string(),
-  remarks: z.string(),
-  contents: z.string(),
-  price: z.number(),
-  tags: z.array(z.string()),
-  travelProductAddressInput: z.object({
-    zipcode: z.string(),
-    address: z.string(),
-    addressDetail: z.string(),
-    lat: z.number(),
-    lng: z.number(),
-  }),
-  images: z.array(z.string()),
+  name: z.string().min(1, { message: "상품명을 입력해 주세요." }),
+  remarks: z.string().min(1, { message: "한 줄 요약을 입력해 주세요." }),
+  contents: z.string().min(1, { message: "설명을 입력해 주세요." }),
+  price: z.string().min(1, { message: "가격을 입력해 주세요." }),
+  tags: z.array(z.string()).optional(),
+  zipcode: z.string().optional(),
+  address: z.string().optional(),
+  addressDetail: z.string().optional(),
+  lat: z.string().optional(),
+  lng: z.string().optional(),
+  images: z.array(z.string()).optional(),
 });
