e21a6a5b62046bec539d659e22803189d267e298
jomira0220 <whalfk220@gmail.com>
2024-11-11 20:27:17 +0900

    검색 관련 validation 수정

--- next_과제/src/components/search-box/index.tsx
+++ next_과제/src/components/search-box/index.tsx
@@ -6,14 +6,12 @@ import { useSearch } from "@/commons/stores/search-store";
 import { useSearchDate } from "@/commons/stores/search-date-store";
 import "dayjs/locale/ko";
 import locale from "antd/lib/date-picker/locale/ko_KR";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import _ from "lodash";
+import { usePathname } from "next/navigation";
 
 interface IValidation {
-  startDate: string;
-  endDate: string;
-  search: string;
-  page: number;
+  [key: string]: string | number;
 }
 
 export default function SearchBox({
@@ -22,17 +20,21 @@ export default function SearchBox({
   countDataRefetch,
 }: {
   isDate?: boolean;
-  refetch: ({ startDate, endDate, search, page }: IValidation) => void;
-  countDataRefetch?: ({
-    startDate,
-    endDate,
-    search,
-    page,
-  }: IValidation) => void;
+  refetch: (validation: IValidation) => void;
+  countDataRefetch?: (validation: IValidation) => void;
 }) {
   const { RangePicker } = DatePicker;
-  const { setSearch } = useSearch();
+  const { search, setSearch } = useSearch();
   const { startDate, endDate, setStartDate, setEndDate } = useSearchDate();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (search !== "") {
+      setSearch("");
+      setStartDate("");
+      setEndDate("");
+    }
+  }, [pathname]);
 
   const inputRef = useRef<InputRef>(null);
 
