fadfcf2e6042c731b37b97978ba032cc9ccf05aa
jintae1954 <qkrwlsxo1954@gmail.com>
2024-11-11 21:31:15 +0900

    설치: react-quill

---
+++ homework-intermediate/day49/src/app/boards/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import BoardsPagination from '@/components/boards-list/pagination';
+import BoardList from '@/components/boards-list/list';
+// import { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { FetchBoardsDocument } from '@/commons/graphql/graphql';
+import BoardTitleSearch from '@/components/boards-list/search';
+import { useBoardStore } from '@/commons/stores/board-store';
+
+export default function BoardsPage() {
+	const { titleSearch, setTitleSearch, activePage, setActivePage } =
+		useBoardStore();
+	// const [activePage, setActivePage] = useState(1);
+	// const [titleSearch, setTitleSearch] = useState('');
+	const { data, refetch } = useQuery(FetchBoardsDocument);
+
+	return (
+		<div className="flex w-full max-w-7xl flex-col gap-8">
+			<BoardTitleSearch {...{ titleSearch, setTitleSearch, refetch }} />
+			<BoardList data={data} activePage={activePage} refetch={refetch} />
+			<BoardsPagination
+				data={data}
+				refetch={refetch}
+				activePage={activePage}
+				setActivePage={setActivePage}
+				titleSearch={titleSearch}
+			/>
+		</div>
+	);
+}
---
+++ homework-intermediate/day49/src/app/page.tsx
@@ -0,0 +1,9 @@
+import ProductListPage from '@/components/products-list';
+
+export default function Home() {
+	return (
+		<>
+			<ProductListPage />
+		</>
+	);
+}
