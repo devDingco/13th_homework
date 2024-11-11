707aac5cee8bce2cbf53fee23b82cdfa4a089529
jomira0220 <whalfk220@gmail.com>
2024-11-11 11:07:38 +0900

    로그인 상태 확인용 store 저장하는 부분 추가

--- next_과제/src/commons/hocs/withAuth.tsx
+++ next_과제/src/commons/hocs/withAuth.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import { useLoadStore } from "@/commons/stores/load-store";
 import { useAccessTokenStore } from "@/commons/stores/access-token";
+import { useLoginStore } from "@/commons/stores/login-store";
 import { useEffect, ComponentType } from "react";
 
 export const withAuth = <P extends object>(Component: ComponentType<P>) => {
@@ -9,12 +10,17 @@ export const withAuth = <P extends object>(Component: ComponentType<P>) => {
     const router = useRouter();
     const { isLoaded } = useLoadStore();
     const { accessToken } = useAccessTokenStore();
+    const { setIsLogged } = useLoginStore();
 
     useEffect(() => {
       // 이미 아폴로 셋팅에서 useEffect 로 액세스 토큰을 저장하고 있기 때문에
       // 여기서는 그 로딩이 완료되었는지만 확인해서 처리하면 된다.
       if (!isLoaded) return;
       if (accessToken) return;
+
+      if (isLoaded && accessToken) {
+        setIsLogged(true); // 로그인 상태로 변경
+      }
 
       // 로딩이 완료된 상태인데 엑세스 토큰이 없다면 로그인 페이지로 이동
       alert("로그인 후 이용해주세요.");
