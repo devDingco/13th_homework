bdfa4940a818e4d20ddeed950379573b867efcf7
지승 <rjs8833@gmail.com>
2024-11-11 12:23:07 +0900

    [241111] server/// .. conflict.. commit error

--- server/configs/aws-s3.config.ts
+++ server/configs/aws-s3.config.ts
@@ -1,7 +1,17 @@
-import * as AWS from 'aws-sdk';
+import * as dotenv from 'dotenv';
 
-export const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+import { S3Client } from '@aws-sdk/client-s3';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'local'}` });
+
+console.log('AWS_REGION:', process.env.AWS_REGION);
+
+const s3 = new S3Client({
     region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
 });
+
+export default s3;
--- server/configs/multer.config.ts
+++ server/configs/multer.config.ts
@@ -1,8 +1,21 @@
-import { memoryStorage } from 'multer';
+import * as multerS3 from 'multer-s3';
+
+import { ConfigService } from '@nestjs/config';
+import s3 from './aws-s3.config';
+
+const configService = new ConfigService();
 
 export const multerOptions = {
-    storage: memoryStorage(),
-    limits: {
-        fileSize: 5 * 1024 * 1024,
-    },
+    storage: multerS3({
+        s3: s3,
+        bucket: configService.get<string>('AWS_S3_BUCKET_NAME'),
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, `sub/${Date.now()}_${file.originalname}`);
+        },
+    }),
 };
