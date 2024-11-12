21dc98fb7f31e5b566cd254a35622ac7e3eab428
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-12 19:27:33 +0900

    [241112] feat: 서브 메뉴 열기, 닫기 함수 구현

--- 02-main_camp/Day43/src/commons/ui/comment/comment/hooks.tsx
+++ 02-main_camp/Day43/src/commons/ui/comment/comment/hooks.tsx
@@ -2,8 +2,18 @@ import { useState } from "react";
 
 export default function useComment() {
   const [isSubMenuOpened, setIsSubMenuOpened] = useState(false);
+
+  const openSubMenu = () => {
+    setIsSubMenuOpened(true);
+  };
+
+  const closeSubMenu = () => {
+    setIsSubMenuOpened(false);
+  };
+
   return {
     isSubMenuOpened,
-    setIsSubMenuOpened,
+    openSubMenu,
+    closeSubMenu,
   };
 }
