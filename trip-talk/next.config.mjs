3fe234677d7837e65fd0213701086f20b38eab82
yheseul <kangyheseul@naver.com>
2024-11-14 15:53:16 +0900

    fix: configure next/image to allow imgaes from storage.googleapis.com

--- trip-talk/next.config.mjs
+++ trip-talk/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
