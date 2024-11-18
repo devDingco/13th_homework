4eabf612834950edb1fbdcb677a0e937698cfc37
jomira0220 <whalfk220@gmail.com>
2024-11-18 16:04:18 +0900

    withAuth 모달 노출 관련 인자 추가

--- next_과제/src/commons/hocs/withAuth.tsx
+++ next_과제/src/commons/hocs/withAuth.tsx
@@ -4,24 +4,24 @@ import { useAccessTokenStore } from "@/commons/stores/access-token";
 import { useEffect, ComponentType } from "react";
 import { useModalStore } from "@/commons/stores/modal-store";
 
-export const withAuth = <P extends object>(Component: ComponentType<P>) => {
-  const WrappedComponent = (props: P) => {
-    const { isLoaded } = useLoadStore();
-    const { accessToken } = useAccessTokenStore();
-    const { setIsModal } = useModalStore();
+export const withAuth =
+  <P extends object>(Component: ComponentType<P>) =>
+  (isModal: boolean) => {
+    const WrappedComponent = (props: P) => {
+      const { isLoaded } = useLoadStore();
+      const { accessToken } = useAccessTokenStore();
+      const { setIsModal } = useModalStore();
 
-    useEffect(() => {
-      if (!isLoaded) return;
-      if (accessToken) return;
+      useEffect(() => {
+        if (!isLoaded) return;
+        if (accessToken) return;
 
-      // 로그인 확인 모달
-      setIsModal({
-        name: "login_confirm",
-      });
-    }, [isLoaded]);
+        // 로그인 확인 모달
+        if (isModal) setIsModal({ name: "login_confirm" });
+      }, [isLoaded]);
 
-    return accessToken !== "" ? <Component {...props} /> : null;
+      return accessToken !== "" && <Component {...props} />;
+    };
+
+    return WrappedComponent;
   };
-
-  return WrappedComponent;
-};
