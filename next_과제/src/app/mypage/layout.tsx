89a29ce5a4ce4cb702b974d32b55b7a063572ef0
jomira0220 <whalfk220@gmail.com>
2024-11-18 15:15:31 +0900

    마이페이지 withAuth추가

--- next_과제/src/app/mypage/layout.tsx
+++ next_과제/src/app/mypage/layout.tsx
@@ -3,7 +3,7 @@ import MyPageInfo from "@/components/mypage/mypage-info";
 import { withAuth } from "@/commons/hocs/withAuth";
 import React from "react";
 
-function MyPage({ children }: { children: React.ReactNode }) {
+function MyPageLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mainContent">
       <h3 className="text-3xl font-bold">마이 페이지</h3>
@@ -12,4 +12,4 @@ function MyPage({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
-export default withAuth(MyPage);
+export default withAuth(MyPageLayout);
