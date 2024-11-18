812ee9074522db62ef3189a7e5eb2f139ff49539
jomira0220 <whalfk220@gmail.com>
2024-11-18 16:05:42 +0900

    로그인 상태를 저장하는 함수 관리용 스토어 추가

--- next_과제/src/commons/stores/login-store.ts
+++ next_과제/src/commons/stores/login-store.ts
@@ -1,11 +1,12 @@
 import { create } from "zustand";
 
-interface IUseLoginStore {
-  isLogged: boolean;
-  setIsLogged: (newIsLoggen: boolean) => void;
+interface ILoginStore {
+  isLogin: boolean | "loading";
+  setIsLogin: (isLogin: boolean) => void;
 }
+
 // 로그인 상태를 저장하는 스토어
-export const useLoginStore = create<IUseLoginStore>((set) => ({
-  isLogged: false,
-  setIsLogged: (isLogged) => set(() => ({ isLogged })),
+export const useLoginStore = create<ILoginStore>((set) => ({
+  isLogin: "loading",
+  setIsLogin: (isLogin) => set(() => ({ isLogin })),
 }));
