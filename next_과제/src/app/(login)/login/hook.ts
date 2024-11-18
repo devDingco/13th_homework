05ce1166503e2be0f74426ae540594e20575e036
jomira0220 <whalfk220@gmail.com>
2024-11-18 16:02:42 +0900

    로그인 상태 처리 관련 훅 추가

--- next_과제/src/app/(login)/login/hook.ts
+++ next_과제/src/app/(login)/login/hook.ts
@@ -9,8 +9,10 @@ import {
   LogoutUserDocument,
 } from "@/commons/graphql/graphql";
 import { useAccessTokenStore } from "@/commons/stores/access-token";
+import { useLoginCheck } from "@/commons/hooks/useLoginCheck";
 
 export const useLoginPage = () => {
+  const { setIsLogin } = useLoginCheck();
   // const client = useApolloClient();
   // 로그인
   const [loginUser] = useMutation(LoginUserDocument);
@@ -53,6 +55,7 @@ export const useLoginPage = () => {
 
     // 2. 로그인 성공 시
     if (result.data?.loginUser.accessToken) {
+      setIsLogin(true); // 로그인 상태로 변경
       setAccessToken(result.data?.loginUser.accessToken); // 스테이트에 액세스 토큰 저장
       router.push("/");
     }
