dcabba3886151a04e5589aaafc28814c97b326cc
jomira0220 <whalfk220@gmail.com>
2024-11-19 10:57:16 +0900

    getBase64 함수 분리에 따른 수정 및 타입 수정

--- next_과제/src/components/product-write/hook.ts
+++ next_과제/src/components/product-write/hook.ts
@@ -7,11 +7,12 @@ import {
 } from "@/commons/graphql/graphql";
 import { useMutation, useQuery } from "@apollo/client";
 import { useState, useEffect } from "react";
-import type { UploadProps } from "antd";
-import { validationImageFile } from "@/commons/libs/validation-image-file";
+import type { UploadProps, UploadFile } from "antd";
+// import { validationImageFile } from "@/commons/libs/validation-image-file";
 import { useForm } from "react-hook-form";
 import { UploadFileDocument } from "@/commons/graphql/graphql";
 import { v4 as uuid } from "uuid";
+import { getBase64 } from "@/commons/utils/getBase64";
 
 export const useProductWrite = () => {
   const router = useRouter();
@@ -49,17 +50,16 @@ export const useProductWrite = () => {
           url: process.env.NEXT_PUBLIC_IMAGE_HOST_NAME + url,
         })) as UploadFileList[]
       );
-
-      if (data?.travelproductAddress?.lat && data?.travelproductAddress?.lng) {
-        methods.setValue(
-          "productAddressLAT",
-          String(data?.travelproductAddress?.lat) ?? ""
-        );
-        methods.setValue(
-          "productAddressLNG",
-          String(data?.travelproductAddress?.lng) ?? ""
-        );
-      }
+    }
+    if (data?.travelproductAddress?.lat && data?.travelproductAddress?.lng) {
+      methods.setValue(
+        "productAddressLAT",
+        String(data?.travelproductAddress?.lat) ?? ""
+      );
+      methods.setValue(
+        "productAddressLNG",
+        String(data?.travelproductAddress?.lng) ?? ""
+      );
     }
     if (data?.tags) {
       setTags(data?.tags);
@@ -115,9 +115,9 @@ export const useProductWrite = () => {
 
     const updateVariables = {
       updateTravelproductInput: {
-        name: productName as string,
-        remarks: productRemarks as string,
-        contents: productContents as string,
+        name: productName,
+        remarks: productRemarks,
+        contents: productContents,
         price: Number(productPrice),
         tags: tags,
         travelproductAddress: {
@@ -197,9 +197,10 @@ export const useProductWrite = () => {
     }
   };
 
+  // ! 이미지 미리보기
   const handlePreview = async (file: UploadFile) => {
     if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj as FileType);
+      file.preview = await getBase64(file.originFileObj as File);
     }
 
     setPreviewImage(file.url || (file.preview as string));
@@ -210,46 +211,6 @@ export const useProductWrite = () => {
   const handleChangeImg: UploadProps["onChange"] = async ({ fileList }) => {
     setImgFileList(fileList as UploadFileList[]);
     console.log(fileList);
-
-    // if (!file) {
-    //   return;
-    // }
-    // const temp = [...imgFileList];
-    // if (file.status !== "removed") {
-    //   // 이미지 삭제가 아닌 경우에만 검증 진행
-    //   const isValid = validationImageFile(file.originFileObj as File);
-    //   if (!isValid) {
-    //     return;
-    //   }
-    // } else {
-    //   // 이미지 삭제시에는 업로드 하지 않음
-    //   temp.pop();
-    //   setImgFileList(temp);
-    //   return;
-    // }
-    // const newFileItem: UploadFileList = {
-    //   uid: file.uid,
-    //   name: file.name,
-    //   status: "uploading",
-    //   url: "",
-    // };
-    // try {
-    //   console.log(file);
-    //   const res = await uploadFile({
-    //     variables: {
-    //       file: file.originFileObj as File,
-    //     },
-    //   });
-    //   //랜덤한 숫자를 리턴하는 함수
-    //   const randomNum = String(Math.floor(Math.random() * 100));
-    //   newFileItem.status = "done";
-    //   newFileItem.uid = randomNum || "";
-    //   newFileItem.url = res.data?.uploadFile.url || "";
-    //   setImgFileList([...imgFileList, newFileItem]);
-    // } catch (err) {
-    //   console.error(err);
-    //   newFileItem.status = "error";
-    // }
   };
 
   // ! 상품 내용 변경
@@ -281,5 +242,6 @@ export const useProductWrite = () => {
     setTags,
     tags,
     setAddress,
+    handlePreview,
   };
 };
