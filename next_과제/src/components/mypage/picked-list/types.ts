b2a7979307bcf51b3c5e163c3722cf66418afbdc
jomira0220 <whalfk220@gmail.com>
2024-11-19 10:53:03 +0900

    불필요한 타입 설정 제거

--- next_과제/src/components/mypage/picked-list/types.ts
+++ next_과제/src/components/mypage/picked-list/types.ts
@@ -1,12 +1,8 @@
 export interface DataType {
-  key: React.Key;
-  name: string;
   _id?: string;
+  name?: string;
   createdAt?: string;
   seller?: string;
   price?: number;
   dataIndex?: string;
-  width?: string;
-  align?: "center" | "left" | "right";
-  render?: (text: unknown, record: DataType) => JSX.Element;
 }
