32e483d79ce432ea445d1dc11fc59edbcc26df4f
noey <yoneee909@gmail.com>
2024-11-21 16:12:54 +0900

    day39 & day41 퍼블리싱

--- myhomework/day39/src/app/boards/page.tsx
+++ myhomework/day39/src/app/boards/page.tsx
@@ -12,26 +12,6 @@ import { useSearchComponent } from "@/components/boards-list/search/hook";
 import { useQuery } from "@apollo/client";
 
 export default function BoardListPage() {
-  // const { data, refetch } = useQuery<FetchBoardsQuery>(FetchBoardsDocument);
-  // const { data: dataBoardsCount } = useQuery(FetchBoardsCountDocument);
-  // const { keyword } = useSearchComponent();
-
-  // const lastPage = Math.ceil((dataBoardsCount?.fetchBoardsCount ?? 10) / 10);
-
-  // console.log(data);
-
-  // // data가 있을 때 첫 번째 게시물의 _id를 가져오고, 만약 data나 첫 번째 게시물이 없으면 빈 문자열 ""을 boardId에 넣는다는 의미
-  // // => 게시물이 있을 때는 그 게시물의 ID를 사용하고, 게시물이 없으면 그냥 빈 문자열을 넣어주는 안전한 방법
-  // // const boardId = data?.fetchBoards[0]?._id || "";
-
-  // return (
-  //   <>
-  //     <SearchComponent refetch={refetch} />
-  //     <BoardList isEdit={false} data={data} keyword={keyword} />
-  //     <PaginationPage refetch={refetch} lastPage={lastPage} />
-  //   </>
-  // );
-
   const { data, refetch } = useQuery<FetchBoardsQuery>(FetchBoardsDocument, {
     variables: { page: 1 }, // 초기 페이지 설정
   });
