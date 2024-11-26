f27ccdaee009224c5e20f1b5187c5b2e4cba52fc
yheseul <kangyheseul@naver.com>
2024-11-27 01:00:48 +0900

    feat: add custom colors to Tailwind config

--- hybrid_app/web/tailwind.config.ts
+++ hybrid_app/web/tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray: "#C7C7C7",
+        lightGray: "#E4E4E4",
       },
     },
   },
