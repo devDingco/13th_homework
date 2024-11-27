b2f7d90214b147fdcef3e4e5694d6f45f2bc65bb
yheseul <kangyheseul@naver.com>
2024-11-27 20:55:53 +0900

    style: add icon to address button

--- hybrid_app/web/src/commons/components/Address/index.tsx
+++ hybrid_app/web/src/commons/components/Address/index.tsx
@@ -1,10 +1,16 @@
 import { BUTTON } from "@/commons/constants/constants";
+import Image from "next/image";
 
-export default function Address () {
+export default function Address() {
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <div>플레이스 주소</div>
-    <button className="flex h-11 w-full justify-between items-center self-stretch border border-[color:var(--gray-B,#000)] px-3 py-2 rounded-lg border-solid">{BUTTON.ADDRESS}</button>
+      <button className="flex h-11 w-full justify-between items-center self-stretch border border-[color:var(--gray-B,#000)] px-3 py-2 rounded-lg border-solid">
+        {BUTTON.ADDRESS}
+        <div className="relative w-6 h-6 -scale-x-100">
+          <Image src="/pngs/left_arrow.png" alt="leftArrow" fill />
+        </div>
+      </button>
     </div>
-  )
+  );
 }
