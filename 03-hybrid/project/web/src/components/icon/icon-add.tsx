04726ee1611c5e33aba59b547b01bfebd51e81a2
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-27 17:37:38 +0900

    [241127] create: 아이콘 생성

---
+++ 03-hybrid/project/web/src/components/icon/icon-add.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function IconAdd() {
+  return (
+    <Image src="/assets/add.svg" alt="더하기 아이콘" width={24} height={24} />
+  );
+}
---
+++ 03-hybrid/project/web/src/components/icon/icon-close.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function IconClose() {
+  return (
+    <Image src="/assets/close.svg" alt="닫기 아이콘" width={16} height={16} />
+  );
+}
