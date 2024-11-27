f2f9ec1001f6433c7e0d66315815474f2e2d9fb5
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-27 17:53:03 +0900

    [241127] fix: apollo 셋팅 적용

--- 03-hybrid/project/web/src/app/layout.tsx
+++ 03-hybrid/project/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import ApolloSetting from "@/settings/setting-apollo";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ApolloSetting>{children}</ApolloSetting>
       </body>
     </html>
   );
