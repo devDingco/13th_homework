bc9d7cd9df25b5a701a2ec1965d9a360f8fa184a
yheseul <kangyheseul@naver.com>
2024-11-27 20:57:51 +0900

    feat: enable register button when name and content are entered

--- hybrid_app/web/src/app/place/new/page.tsx
+++ hybrid_app/web/src/app/place/new/page.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Address from "@/commons/components/Address";
 import Button from "@/commons/components/Button";
+import ImageUpload from "@/commons/components/ImageUpload";
 import Input from "@/commons/components/Input";
 import TextArea from "@/commons/components/TextArea";
+import usePlaceNew from "@/commons/hooks/usePlaceNew";
 
 export default function PlaceNew() {
+  const { handleChange, disabled } = usePlaceNew();
+
   return (
-    <div>
-      <Input id="이름" />
+    <div className="flex flex-col gap-5 pt-5">
+      <ImageUpload id="1" />
+      <Input id="이름" onChange={handleChange} />
       <Address />
-      <TextArea id="내용" />
-      <Button id="LOG" />
+      <TextArea id="내용" onChange={handleChange} />
+      <Button id="LOG" disabled={disabled} />
     </div>
   );
 }
--- hybrid_app/web/src/commons/components/TextArea/index.tsx
+++ hybrid_app/web/src/commons/components/TextArea/index.tsx
@@ -1,13 +1,22 @@
 import { LABEL, PLACE_HOLDER } from "@/commons/constants/constants";
+import { ChangeEventHandler } from "react";
 
-export default function TextArea({ id }: { id: string }) {
+export default function TextArea({
+  id,
+  onChange,
+}: {
+  id: string;
+  onChange: ChangeEventHandler<HTMLTextAreaElement>;
+}) {
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <div className="flex flex-row gap-1">
         <p>{LABEL(id)}</p>
         <p className="text-red-400">*</p>
       </div>
       <textarea
+        id={id}
+        onChange={onChange}
         className="flex w-full h-[148px] flex-col items-start gap-1 self-stretch border border-[color:var(--Gray-Gray-200,#D4D3D3)] px-4 py-3 rounded-lg border-solid;"
         placeholder={PLACE_HOLDER(id)}
       />
