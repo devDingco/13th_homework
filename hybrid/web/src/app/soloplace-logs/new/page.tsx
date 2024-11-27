662be58576acef90ef1653eeac14090e30609416
janghwahyun <1117jhh@gmail.com>
2024-11-27 18:12:56 +0900

    [241127] Feat: 이미지 삭제기능 + 이미지 가로 스크롤기능 구현함

--- hybrid/web/src/app/soloplace-logs/new/page.tsx
+++ hybrid/web/src/app/soloplace-logs/new/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from './styles.module.css';
 import { useRef, useState } from 'react';
 import { checkValidtionFile } from '@/commons/libraries/validation';
+import { Layout } from '@/commons/layout/02-02-layout-header-global';
 
 export default function NewPage() {
     const [images, setImages] = useState([]);
@@ -12,8 +13,10 @@ export default function NewPage() {
     const onChangeFile = (event) => {
         const files = Array.from(event.target.files);
         console.log(files);
-        const imageUrls = files.map((file) => URL.createObjectURL(file));
-        setImages(imageUrls);
+        const newImageUrls = files.map((file) => URL.createObjectURL(file));
+        const updatedImages = [...images, ...newImageUrls];
+        setImages(updatedImages);
+        sessionStorage.setItem('images', JSON.stringify(updatedImages));
 
         const isValid = checkValidtionFile(files);
         if (!isValid) return;
@@ -23,41 +26,61 @@ export default function NewPage() {
         fileRef.current.click();
     };
 
+    const handleRemoveImage = (index) => {
+        setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.layout}>
                 <div className={styles.titleBox}>{`< 플레이스 등록`}</div>
+
                 <div className={styles.imageBox}>
-                    <Image
-                        src="/images/add.png"
-                        alt="add"
-                        width={100}
-                        height={100}
-                        style={{ cursor: 'pointer', marginRight: '.75rem' }}
-                        onClick={onClickImage}
-                    />
-                    <input
-                        type="file"
-                        onChange={onChangeFile}
-                        style={{ display: 'none' }}
-                        ref={fileRef}
-                        multiple
-                    />
+                    <div className={styles.imageBoxLeft}>
+                        <Image
+                            src="/images/add.png"
+                            alt="add"
+                            width={100}
+                            height={100}
+                            layout="fixed"
+                            style={{
+                                cursor: 'pointer',
+                                marginRight: '.75rem',
+                            }}
+                            onClick={onClickImage}
+                        />
+                        <input
+                            type="file"
+                            onChange={onChangeFile}
+                            style={{ display: 'none' }}
+                            ref={fileRef}
+                        />
+                    </div>
+
                     <div className={styles.showImage}>
                         {images.map((image, index) => (
-                            <img
-                                key={index}
-                                src={image}
-                                alt={`Uploaded ${index}`}
-                                style={{
-                                    width: '100px',
-                                    height: '100px',
-                                    borderRadius: '8px',
-                                }}
-                            />
+                            <div className={styles.mapImageBox}>
+                                <img
+                                    key={index}
+                                    src={image}
+                                    alt={`Uploaded ${index}`}
+                                    style={{
+                                        width: '100px',
+                                        height: '100px',
+                                        borderRadius: '8px',
+                                    }}
+                                />
+                                <button
+                                    className={styles.cancleBtn}
+                                    onClick={() => handleRemoveImage(index)}
+                                >
+                                    X
+                                </button>
+                            </div>
                         ))}
                     </div>
                 </div>
+
                 <div className={styles.placeNameBox}>
                     <div className={styles.placeNameBoxTitle}>
                         플레이스 이름 <div className={styles.star}>*</div>
