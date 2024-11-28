f28cc7489225366a0d10887fa7c6f04209d58c3c
yheseul <kangyheseul@naver.com>
2024-11-28 20:32:01 +0900

    feat: Implement logic to hide header on detail page

--- hybrid_app/web/src/commons/layout/layout/index.tsx
+++ hybrid_app/web/src/commons/layout/layout/index.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { ReactNode } from "react";
 import LayoutHeader from "../header";
+import useLayout from "@/commons/hooks/useLayout";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const isHiddenHeader = useLayout();
+
   return (
     <div>
-      <LayoutHeader />
+      {isHiddenHeader && <LayoutHeader />}
       {children}
     </div>
   );
