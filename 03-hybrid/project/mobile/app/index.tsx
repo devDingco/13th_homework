9dae10c62987365f0f2abcec69e3075e520d511b
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-28 19:52:55 +0900

    [241128] refactor: 컴포넌트 분리 후 결합

--- 03-hybrid/project/mobile/app/index.tsx
+++ 03-hybrid/project/mobile/app/index.tsx
@@ -1,14 +1,38 @@
-import React from "react";
-import { StatusBar } from "react-native";
+"use client";
+
+import { useState } from "react";
+import { StatusBar, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 
+const UN_TRANSPARENT_HEADER_PAGES = ["/solplace-logs/new"];
+
 export default function App() {
+  const [currentPath, setCurrentPath] = useState("");
+
+  const handleNavigationStateChange = (event) => {
+    const url = new URL(event.url);
+    console.log(url.pathname);
+    setCurrentPath(url.pathname);
+  };
+
+  const isTransparentPage = !UN_TRANSPARENT_HEADER_PAGES.includes(currentPath);
+  const Wrapper = isTransparentPage ? View : SafeAreaView;
+
   return (
-    // flex: 1 = 기기 크기 전체를 사용한다.
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar translucent={false} />
-      <WebView source={{ uri: "http://10.0.2.2:3000/solplace-logs/new" }} />
-    </SafeAreaView>
+    <>
+      <Wrapper
+        style={{
+          flex: 1,
+          backgroundColor: isTransparentPage ? "transparent" : "white",
+        }}
+      >
+        <StatusBar translucent={isTransparentPage} />
+        <WebView
+          source={{ uri: "http://10.0.2.2:3000/solplace-logs/1234" }}
+          onNavigationStateChange={handleNavigationStateChange}
+        />
+      </Wrapper>
+    </>
   );
 }
