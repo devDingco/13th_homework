6c0861aabc2ccea24f1e206e13d3e4ec133154f8
jomira0220 <whalfk220@gmail.com>
2024-11-28 15:26:08 +0900

    수업 내용 파일 업로드

--- hybrid-app/class-mobile/app/index.tsx
+++ hybrid-app/class-mobile/app/index.tsx
@@ -4,10 +4,15 @@
 // import WebViewPage from "@/components/section01/01-03-webview";
 // import WebViewWithNextPage from "@/components/section01/01-04-webview-with-next";
 // import WebViewInternetFailPage from "@/components/section01/01-05-webview-internet-fail";
-// import RatioScalingPage from "@/components/section02/02-01-ratio-scaling";
 
-import LayoutHeaderGlobalPage from "@/components/section02/02-02-layout-header-global";
+// [ Section02 ]
+// import RatioScalingPage from "@/components/section02/02-01-ratio-scaling";
+// import LayoutHeaderGlobalPage from "@/components/section02/02-02-layout-header-global";
+// import LayoutHeaderLocalPage from "@/components/section02/02-03-layout-header-local";
+// import LayoutHeaderTransparentPage from "@/components/section02/02-04-layout-header-transparent";
+
+import LayoutFooterContentsShortAndLong from "@/components/section02/02-05-layout-footer-contents-short-and-long";
 
 export default function StartPage() {
-  return <LayoutHeaderGlobalPage />;
+  return <LayoutFooterContentsShortAndLong />;
 }
