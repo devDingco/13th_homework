ac019f14c6bb829b956e147cb07c4323e0721a9f
h0ngsik <hongsik.choi.dev@gmail.com>
2024-11-28 19:43:49 +0900

    [241128] refactor: 컴포넌트 분리 후 결합

--- 03-hybrid/project/web/src/app/solplace-logs/new/page.tsx
+++ 03-hybrid/project/web/src/app/solplace-logs/new/page.tsx
@@ -1,15 +1,5 @@
-import IconLeftArrow from "@/components/icon/icon-left_arrow";
 import FormSolplace from "@/components/form/form-solplace";
-import styles from "./styles.module.css";
 
 export default function SolplaceLogsNewPage() {
-  return (
-    <>
-      <div className={styles.navigation__container}>
-        <IconLeftArrow />
-        <p>플레이스 등록</p>
-      </div>
-      <FormSolplace />
-    </>
-  );
+  return <FormSolplace />;
 }
