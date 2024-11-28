db4238dd099e1d12d8c66a84376afa8bc57611f1
Shin-E2 <sinhyejee322@gmail.com>
2024-11-28 17:29:06 +0900

    [241128] 이미지 슬라이드하면 이미지 번호span 함꼐 넘어가는 부분 수정

--- 05-hybrid-app/web/src/components/solplace-logs/detail/ImageSlide.tsx
+++ 05-hybrid-app/web/src/components/solplace-logs/detail/ImageSlide.tsx
@@ -33,29 +33,33 @@ export default function ImageSlide({ images = SAMPLE_IMAGES }) {
   }, []);
 
   return (
-    <div
-      ref={containerRef}
-      className="w-full h-[480px] relative overflow-x-auto whitespace-nowrap scroll-smooth snap-x snap-mandatory"
-      // style={{ scrollSnapType: "x mandatory" }}
-    >
-      <div className="absolute inset-0 z-0">
-        {images.map((el, index) => (
-          <div
-            key={`${el}_${index}`}
-            className="inline-block w-screen snap-start"
-            style={{ scrollSnapAlign: "start" }}
-          >
-            <Image
-              src={el}
-              alt={`${index + 1}번 사진`}
-              className="w-full h-[480px] object-cover"
-              width={500}
-              height={480}
-              priority={index === 0}
-            />
-          </div>
-        ))}
+    <div className="relative">
+      {/* 이미지 슬라이드 */}
+      <div
+        ref={containerRef}
+        className="w-full h-[480px] relative overflow-x-auto whitespace-nowrap scroll-smooth snap-x snap-mandatory"
+        // style={{ scrollSnapType: "x mandatory" }}
+      >
+        <div className="absolute inset-0 z-0">
+          {images.map((el, index) => (
+            <div
+              key={`${el}_${index}`}
+              className="inline-block w-screen snap-start"
+              style={{ scrollSnapAlign: "start" }}
+            >
+              <Image
+                src={el}
+                alt={`${index + 1}번 사진`}
+                className="w-full h-[480px] object-cover"
+                width={500}
+                height={480}
+                priority={index === 0}
+              />
+            </div>
+          ))}
+        </div>
       </div>
+      {/* 현재 이미지 번호와 이미지 개수 */}
       <span className="absolute bottom-20 right-20 h-20 w-40 px-8 py-4 bg-black/60 rounded-[100px] text-[#f2f2f2] text-[11px] font-medium leading-3 flex justify-center items-center">
         {currentImgIndex}/{images.length}
       </span>
