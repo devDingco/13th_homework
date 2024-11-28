b56c0109dd562eb452dba36ecbe83cd789ebcb23
jomira0220 <whalfk220@gmail.com>
2024-11-28 15:26:23 +0900

    에뮬레이터 수정

--- hybrid_app_homework/mobile/components/index.tsx
+++ hybrid_app_homework/mobile/components/index.tsx
@@ -2,8 +2,8 @@ import { SafeAreaView, StatusBar } from "react-native";
 import { WebView } from "react-native-webview";
 
 // const computerAddress = "http://172.16.0.66:3000"; // 내 핸드폰에서 접속하기
-// const androidEmulatorAddress = "http://10.0.2.2:3000"; // 안드로이드 에뮬레이터에서 접속하기
-const iosEmulatorAddress = "http://127.0.0.1:3000"; // IOS 에뮬레이터에서 접속하기
+const androidEmulatorAddress = "http://10.0.2.2:3000"; // 안드로이드 에뮬레이터에서 접속하기
+// const iosEmulatorAddress = "http://127.0.0.1:3000"; // IOS 에뮬레이터에서 접속하기
 
 export default function SolPlaceLogsPage() {
   return (
@@ -12,7 +12,7 @@ export default function SolPlaceLogsPage() {
         <StatusBar translucent={false} />
         <WebView
           source={{
-            uri: `${iosEmulatorAddress}/solplace-logs/detail`,
+            uri: `${androidEmulatorAddress}/solplace-logs/detail`,
           }}
         />
       </SafeAreaView>
