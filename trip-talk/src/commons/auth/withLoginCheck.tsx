f82b8bb668e91272f16700004590ca42919bb39a
yheseul <kangyheseul@naver.com>
2024-11-29 17:25:58 +0900

    feat: redirect to login if unauthenticated user clicks a post
    Display post details when logged in.
    Show an alert and navigate to the login page when not logged in.

--- trip-talk/src/commons/auth/withLoginCheck.tsx
+++ trip-talk/src/commons/auth/withLoginCheck.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { useEffect } from "react";
 import { useLoadStore } from "../stores/useLoadStore";
 import { useAccessTokenStore } from "../stores/useAccessTokenStore";
 import { useRouter } from "next/navigation";
+
 const withLoginCheck =
   (Component: () => JSX.Element) =>
   <P extends object>(props: P) => {
@@ -9,8 +12,9 @@ const withLoginCheck =
     const { accessToken } = useAccessTokenStore();
     const { isSetLoaded } = useLoadStore();
     useEffect(() => {
-      if (!isSetLoaded) return;
+      if (isSetLoaded) return;
       if (accessToken) return;
+
       alert("로그인 후 다시 시도해 주세요.");
       router.push("/");
     }, [isSetLoaded]);
