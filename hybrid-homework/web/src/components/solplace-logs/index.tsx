188eb150853019e528c92b43a6f2ace152b9acd8
MingseoYa <alstj3758@naver.com>
2024-11-29 10:13:06 +0900

    솔플레이스 리스트페이지 퍼블리싱

--- hybrid-homework/web/src/components/solplace-logs/index.tsx
+++ hybrid-homework/web/src/components/solplace-logs/index.tsx
@@ -1,35 +1,51 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { PLACE_DATA } from "./constans";
 
 export default function SolplaceLogs() {
   return (
     <main className={styles.container}>
-      <div>
-        <div className={styles.card}>
-          <Image
-            src="/asset/default1.jpeg"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.image}
-            alt="cardImage"
-          />
-          <div className={styles.title}>
-            <div></div>
-            <div className={styles.address}>
-              <Image
-                className={styles.icon16}
-                src="/asset/location_icon.svg"
-                width={0}
-                height={0}
-                sizes="100vw"
-                alt="location_icon"
-              />
-              <div>서울시 용산구</div>
+      <div className={styles.grid}>
+        {PLACE_DATA.map((data) => (
+          <div className={styles.card} key={data.id}>
+            <Image
+              src={data.image}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className={styles.image}
+              alt="cardImage"
+            />
+            <div className={styles.contents}>
+              <div className={styles.info}>
+                <div className={styles.title}>{data.title}</div>
+                <div className={styles.description}>{data.description}</div>
+              </div>
+              <div className={styles.address}>
+                <Image
+                  className={styles.icon16}
+                  src="/asset/location_icon.svg"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  alt="location_icon"
+                />
+                <div className={styles.addressTitle}>{data.address}</div>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
+      <button className={styles.floatingButton}>
+        <Image
+          src="/asset/add_white.svg"
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt="add"
+          className={styles.add}
+        />
+      </button>
     </main>
   );
 }
