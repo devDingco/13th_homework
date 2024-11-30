10a4f8475d1c4bb15968d6dbc889253d99e6bb84
revy7289 <95402753+revy7289@users.noreply.github.com>
2024-11-30 16:20:28 +0900

    정리: 잘못 업로드된 코드와 파일 삭제

--- 02-react-next/homework/src/app/boards/page.tsx
+++ 02-react-next/homework/src/app/boards/page.tsx
@@ -22,15 +22,6 @@ export default function BoardsListPage() {
             <SearchUI refetch={refetch} keyword={keyword} setKeyword={setKeyword} />
             <BoardsListUI data={data} count={count} current={current} keyword={keyword} />
             <PaginationUI refetch={refetch} lastPage={lastPage} current={current} setCurrent={setCurrent} />
-
-            <<Space direction="vertical">
-    <DatePicker onChange={onChange} />
-    <DatePicker onChange={onChange} picker="week" />
-    <DatePicker onChange={onChange} picker="month" />
-    <DatePicker onChange={onChange} picker="quarter" />
-    <DatePicker onChange={onChange} picker="year" />
-  </Space>
-)
         </>
     );
 }
