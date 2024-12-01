e7497df281e1a3f3f70e323522fd29466160b9e8
yheseul <kangyheseul@naver.com>
2024-12-01 21:59:31 +0900

    feat: Add temporary navigation to PlaceList page on header button click

--- hybrid_app/web/src/commons/layout/header/index.tsx
+++ hybrid_app/web/src/commons/layout/header/index.tsx
@@ -1,11 +1,14 @@
 import Icon from "@/commons/components/Icon/page";
 import { HEADER } from "@/commons/constants/constants";
+import Link from "next/link";
 
 export default function LayoutHeader() {
   return (
-    <div className="flex flex-row gap-2">
-      <Icon src="/pngs/left_arrow.png" alt="leftArrow" />
-      <div className="text-lg font-bold">{HEADER.REGISTER}</div>
-    </div>
+    <Link href="/place">
+      <div className="flex flex-row gap-2">
+        <Icon src="/pngs/left_arrow.png" alt="leftArrow" />
+        <div className="text-lg font-bold">{HEADER.REGISTER}</div>
+      </div>
+    </Link>
   );
 }
