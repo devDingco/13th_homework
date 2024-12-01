140357cdb7ff69dd96819d303e12b0f44c748819
yheseul <kangyheseul@naver.com>
2024-12-01 21:57:18 +0900

    feat: Create PlaceList page

--- hybrid_app/web/src/commons/constants/constants.ts
+++ hybrid_app/web/src/commons/constants/constants.ts
@@ -8,10 +8,15 @@ export const BUTTON = {
   LOG: "로그 등록",
   EDIT: "수정",
   LOCATION_EDIT: "이 위치로 수정",
-  ADDRESS: "플레이스 주소 입력"
+  ADDRESS: "플레이스 주소 입력",
 };
 
 export const PLACE_HOLDER = (label: string) =>
   `플레이스 ${label}을 입력해 주세요. (1자 이상)`;
 
 export const LABEL = (label: string) => `플레이스 ${label}`;
+
+export const FOOTER = {
+  PLACE: "플레이스",
+  MY_PAGE: "내 설정",
+};
