f9d8d0a0d1674ec7e1784b632b4a18ce1fed371e
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-02 15:34:23 +0900

    [241202] add: 헤더 옵션 경로 추가 - /solplace-logs

--- 03-hybrid/project/web/src/commons/layout/constants.ts
+++ 03-hybrid/project/web/src/commons/layout/constants.ts
@@ -1,5 +1,10 @@
 export const HEADER_OPTIONS = (id?: string) => ({
   GLOBAL: {
+    "/solplace-logs": {
+      title: "플레이스",
+      hasBack: false,
+      isTransparent: false,
+    },
     "/solplace-logs/new": {
       title: "플레이스 등록",
       hasBack: true,
