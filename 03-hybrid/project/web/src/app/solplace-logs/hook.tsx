8b7b360e4826245f38153537005bfcdd267bef68
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-04 10:47:40 +0900

    [241204] feat: 솔플레이스 로그 목록 - 로그 선택 및 플로팅 액션 버튼 기능 구현 - 로그 선택 시 상세 페이지 이동 - 플로팅 액션 버튼 눌렀을 때, 작성 페이지 이동

--- 03-hybrid/project/web/src/app/solplace-logs/hook.tsx
+++ 03-hybrid/project/web/src/app/solplace-logs/hook.tsx
@@ -1,11 +1,22 @@
 import { useRouter } from "next/navigation";
+import useSampleDataStore from "../test/test-store";
+import { ISampleData } from "./page";
 
 export default function useSolplaceLogs() {
   const router = useRouter();
-  const onClickCard = () => {
+
+  const { setData } = useSampleDataStore();
+
+  const onClickCard = (data: ISampleData) => {
     router.push("/solplace-logs/123");
+    setData(data);
+  };
+
+  const onClickFloatingActionButton = () => {
+    router.push("/solplace-logs/new");
   };
   return {
     onClickCard,
+    onClickFloatingActionButton,
   };
 }
