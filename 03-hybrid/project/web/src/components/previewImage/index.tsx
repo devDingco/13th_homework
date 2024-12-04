46ee4a09ebb6f1299036decb0203be4a2f0a5cce
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-04 10:59:16 +0900

    [241204] fix: 테스트로 인한 이미지 경로 변경

--- 03-hybrid/project/web/src/components/previewImage/index.tsx
+++ 03-hybrid/project/web/src/components/previewImage/index.tsx
@@ -21,7 +21,8 @@ export default function PreviewImage({
         <Icon src="close.svg" width={0.875} height={0.875} />
       </button>
       <Image
-        src={`https://storage.googleapis.com/${src}`}
+        // src={`https://storage.googleapis.com/${src}`}
+        src={src}
         alt="미리보기 이미지"
         width={0}
         height={0}
