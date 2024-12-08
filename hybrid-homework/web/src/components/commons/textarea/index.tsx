5a89892ad054951800eb50af268b46095795a265
MingseoYa <alstj3758@naver.com>
2024-12-08 19:41:53 +0900

    공통컴포넌트-textarea 제네릭타입적용

--- hybrid-homework/web/src/components/commons/textarea/index.tsx
+++ hybrid-homework/web/src/components/commons/textarea/index.tsx
@@ -1,28 +1,55 @@
 "use client";
-import { useFormContext } from "react-hook-form";
+import { FieldValues, Path, useFormContext } from "react-hook-form";
 import styles from "./styles.module.css";
+import { TextareaHTMLAttributes } from "react";
+
+interface ITextareaBase<T> extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  className: string;
+  name: Path<T>;
+  isRequired: boolean;
+}
 
 // 1. 뼈대 만들기
-function TextareaBase(props) {
-  const { register } = useFormContext();
+function TextareaBase<T extends FieldValues>({
+  label,
+  className,
+  name,
+  isRequired,
+  ...props
+}: ITextareaBase<T>) {
+  const { register, watch } = useFormContext();
+  const value = watch(name);
+  const contentsLength = value ? value.length : 0;
+
   return (
     <div className={styles.inputField}>
       <div className={styles.label}>
-        {props.label}
-        {props.isRequired && <span className={styles.required}> *</span>}
+        {label}
+        {isRequired && <span className={styles.required}> *</span>}
       </div>
-      <textarea
-        className={props.className}
-        placeholder={props.placeholder}
-        readOnly={props.readOnly}
-        disabled={props.disabled}
-        {...register(props.name)}
-      />
+      <div className={styles.textareaBox}>
+        <textarea
+          className={className}
+          placeholder={props.placeholder}
+          readOnly={props.readOnly}
+          disabled={props.disabled}
+          {...register(name)}
+        />
+        <div className={styles.contentsLength}>{contentsLength}/100</div>
+      </div>
     </div>
   );
 }
 
+interface ITextarea<T> extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  name: Path<T>;
+  isRequired: boolean;
+}
+
+// prettier-ignore
 // 2. 인풋 찍기
-export function TextareaNormal(props) {
-  return <TextareaBase className={styles.textareaNormal} {...props} />;
+export function TextareaNormal<T extends FieldValues>({ label, isRequired, name, ...props }: ITextarea<T>) {
+  return <TextareaBase<T> className={styles.textareaNormal} label={label} isRequired={isRequired} name={name} {...props} />;
 }
--- hybrid-homework/web/src/components/domains/solplace-logs-detail-edit/input-section/contents-input/index.tsx
+++ hybrid-homework/web/src/components/domains/solplace-logs-detail-edit/input-section/contents-input/index.tsx
@@ -1,8 +1,9 @@
 import { TextareaNormal } from "@/components/commons/textarea";
+import { ISchema } from "../../form.schema";
 
 export default function SolplaceInputContents() {
   return (
-    <TextareaNormal
+    <TextareaNormal<ISchema>
       name="contents"
       label="플레이스 내용"
       placeholder="플레이스 내용을 입력해 주세요. (1자 이상)"
