eccf0664e7130e504bb76a1b22bdf88c2214d332
janghwahyun <1117jhh@gmail.com>
2024-12-11 09:31:28 +0900

    [241211] Feat: 웹앱 연결 apis 셋팅 구현함

---
+++ hybrid/mobile/apis/index.ts
@@ -0,0 +1,40 @@
+export const useApis = (webviewRef) => {
+    //
+    //
+    //
+    const onResponse = (result) => {
+        webviewRef.current?.postMessage(JSON.stringify(result));
+    };
+
+    const onRequest = (query) => {
+        switch (query) {
+            case 'fetchDeviceSystemForAppSet': {
+                onResponse({
+                    fetchDeviceSystemForAppSet: { appVersion: 'v1.0' },
+                });
+                break;
+            }
+
+            case 'fetchDeviceSystemForPlatformSet': {
+                onResponse({
+                    fetchDeviceSystemForPlatformSet: {
+                        modelName: 'iPhone 7 Plus',
+                    },
+                });
+                break;
+            }
+
+            case 'fetchDeviceLocationForLatLngSet': {
+                onResponse({
+                    fetchDeviceLocationForLatLngSet: { lat: 37, lng: 128 },
+                });
+                break;
+            }
+        }
+    };
+
+    return {
+        onResponse,
+        onRequest,
+    };
+};
